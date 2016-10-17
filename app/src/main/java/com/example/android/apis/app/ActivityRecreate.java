f6da5812fc56f7a118c39ab57c2cdac4d286f26a
markgray <mark.t.gray@gmail.com>
2016-10-17 04:25:38 -0400

    Added comments

--- app/src/main/java/com/example/android/apis/app/ActivityRecreate.java
+++ app/src/main/java/com/example/android/apis/app/ActivityRecreate.java
@@ -26,10 +26,31 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+/**
+ * Uses setTheme(theme) followed by recreate() to cycle through three different Holo themes
+ * (original version did not initialize the field that the current theme is kept in, causing
+ * it to crash on Material Light default devices.)
+ */
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class ActivityRecreate extends Activity {
-    int mCurTheme;
+    int mCurTheme; // Theme in use for this instance of the Activity
 
+    /**
+     * Called when the activity is starting. First we call through to our super's implementation of
+     * onCreate. Then if savedInstanceState is not null we retrieve the mCurTheme from that Bundle
+     * (stored under the key "theme"), and switch on its current value changing the mCurTheme from
+     * Theme_Holo_Light to Theme_Holo_Dialog to Theme_Holo and back to Theme_Holo_Light. If
+     * savedInstanceState is null (first time run) we set mCurTheme to Theme_Holo_Light. Next we
+     * set the base theme for this context to mCurTheme, then set the content view to our layout
+     * file R.layout.activity_recreate. Finally we locate the "Recreate" Button (R.id.recreate) and
+     * set its OnClickListener to OnClickListener mRecreateListener so that when the Button is
+     * clicked the Activity will be recreated.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in {@link #onSaveInstanceState}.
+     *                           <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,14 +75,22 @@ public class ActivityRecreate extends Activity {
             mCurTheme = android.R.style.Theme_Holo_Light;
         }
 
-    setTheme(mCurTheme);
+        setTheme(mCurTheme);
         setContentView(R.layout.activity_recreate);
 
         // Watch for button clicks.
-        Button button = (Button)findViewById(R.id.recreate);
+        Button button = (Button) findViewById(R.id.recreate);
         button.setOnClickListener(mRecreateListener);
     }
 
+    /**
+     * Called to retrieve per-instance state from an activity before being killed
+     * so that the state can be restored in {@link #onCreate} or
+     * {@link #onRestoreInstanceState} (the {@link Bundle} populated by this method
+     * will be passed to both).
+     *
+     * @param savedInstanceState Bundle in which to place your saved state.
+     */
     @Override
     protected void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
@@ -69,6 +98,7 @@ public class ActivityRecreate extends Activity {
     }
 
     private OnClickListener mRecreateListener = new OnClickListener() {
+        @Override
         public void onClick(View v) {
             recreate();
         }
