49c1d2d676d742e5aa1d39969723db1fc3b9118a
markgray <mark.t.gray@gmail.com>
2017-02-09 05:39:50 -0500

    Added comments

--- app/src/main/java/com/example/android/apis/app/LoaderThrottle.java
+++ app/src/main/java/com/example/android/apis/app/LoaderThrottle.java
@@ -288,7 +288,22 @@ public class LoaderThrottle extends Activity {
         }
 
         /**
-         * Handle incoming queries.
+         * Handle query incoming requests from clients.
+         *
+         * @param uri           The URI to query. This will be the full URI sent by the client; if
+         *                      the client is requesting a specific record, the URI will end in a
+         *                      record number that the implementation should parse and add to a
+         *                      WHERE or HAVING clause, specifying that _id value.
+         * @param projection    The list of columns to put into the cursor. If {@code null} all
+         *                      columns are included.
+         * @param selection     A selection criteria to apply when filtering rows. If {@code null}
+         *                      then all rows are included.
+         * @param selectionArgs You may include ?s in selection, which will be replaced by the values
+         *                      from selectionArgs, in order that they appear in the selection. The
+         *                      values will be bound as Strings.
+         * @param sortOrder     How the rows in the cursor should be sorted. If {@code null} then
+         *                      the provider is free to define the sort order.
+         * @return a Cursor or {@code null}.
          */
         @Override
         public Cursor query(@NonNull Uri uri, String[] projection, String selection,
