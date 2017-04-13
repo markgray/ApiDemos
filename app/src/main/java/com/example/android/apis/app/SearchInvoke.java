f12e6b0ae0c4d8cfb41353d7cdcd44036937bd06
markgray <mark.t.gray@gmail.com>
2017-04-13 02:47:04 -0400

    Added comments

--- app/src/main/java/com/example/android/apis/app/SearchInvoke.java
+++ app/src/main/java/com/example/android/apis/app/SearchInvoke.java
@@ -38,162 +38,193 @@ import android.widget.AdapterView.OnItemSelectedListener;
  * This activity shows a few different ways to invoke search, and inserts context-specific data for
  * use by the search activity.
  */
-public class SearchInvoke extends Activity
-{  
-        // UI elements
+public class SearchInvoke extends Activity {
+    // UI elements
     Button mStartSearch; // Button used to start search - Without a keyboard, you need to press this to get a soft keyboard to use.
     Spinner mMenuMode; // Spinner used to select between "Search Key", "Menu Item", "Type-To-Search" or "Disabled"
     EditText mQueryPrefill; // Used to enter text to prefill the search
     EditText mQueryAppData; // context specific data to include in a Bundle under the key "demo_key" it will be returned in search Intents
-    
-        // Menu mode spinner choices
-        // This list must match the list found in samples/ApiDemos/res/values/arrays.xml
+
+    // Menu mode spinner choices
+    // This list must match the list found in samples/ApiDemos/res/values/arrays.xml
     final static int MENUMODE_SEARCH_KEY = 0; // "Search Key" Uses the search key to launch searches (Needs keyboard)
     final static int MENUMODE_MENU_ITEM = 1;  // "Menu Item" Uses the menu item itself to launch searches
     final static int MENUMODE_TYPE_TO_SEARCH = 2; // "Type-To-Search" unhandled keystrokes will start an application-defined search (needs keyboard)
     final static int MENUMODE_DISABLED = 3; // "Disabled" Search is disabled
-    
-    /** 
-     * Called with the activity is first created.
+
+    /**
+     * Called when the activity is created. First we call through to our super's implementation of
+     * {@code onCreate}, then we set our content view to our layout file R.layout.search_invoke.
+     * We locate the {@code Button mStartSearch} R.id.btn_start_search, the {@code Spinner mMenuMode}
+     * R.id.spinner_menu_mode, the {@code EditText mQueryPrefill} R.id.txt_query_prefill, and the
+     * {@code EditText mQueryAppData} R.id.txt_query_appdata. Then we create from R.array.search_menuModes
+     * an ArrayAdapter<CharSequence> adapter using the system layout android.R.layout.simple_spinner_dropdown_item,
+     * set the layout resource to create the drop down views to android.R.layout.simple_spinner_dropdown_item
+     * and set the {@code SpinnerAdapter} of {@code Spinner mMenuMode} to {@code adapter}. We set the
+     * {@code OnItemSelectedListener} of {@code mMenuMode} to an anonymous class which selects the
+     * default key handling for this activity based on the Spinner item selected to DEFAULT_KEYS_SEARCH_LOCAL
+     * if MENUMODE_TYPE_TO_SEARCH is selected, and DEFAULT_KEYS_DISABLE otherwise (keyboard required
+     * to tell the difference). Finally we set the {@code OnClickListener} of {@code Button mStartSearch}
+     * to an anonymous class which calls our override of the method {@code onSearchRequested} when the
+     * Button is clicked.
      *
      * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         // Inflate our UI from its XML layout description.
         setContentView(R.layout.search_invoke);
-        
+
         // Get display items for later interaction
         mStartSearch = (Button) findViewById(R.id.btn_start_search);
         mMenuMode = (Spinner) findViewById(R.id.spinner_menu_mode);
         mQueryPrefill = (EditText) findViewById(R.id.txt_query_prefill);
         mQueryAppData = (EditText) findViewById(R.id.txt_query_appdata);
-        
+
         // Populate items
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                            this, R.array.search_menuModes, android.R.layout.simple_spinner_item);
+                this, R.array.search_menuModes, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mMenuMode.setAdapter(adapter);
-        
+
         // Create listener for the menu mode dropdown.  We use this to demonstrate control
         // of the default keys handler in every Activity.  More typically, you will simply set
         // the default key mode in your activity's onCreate() handler.
         mMenuMode.setOnItemSelectedListener(
-            new OnItemSelectedListener() {
-                @Override
-                public void onItemSelected(
-                        AdapterView<?> parent, View view, int position, long id) {
-                    if (position == MENUMODE_TYPE_TO_SEARCH) {
-                        setDefaultKeyMode(DEFAULT_KEYS_SEARCH_LOCAL);
-                    } else {
+                new OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        if (position == MENUMODE_TYPE_TO_SEARCH) {
+                            setDefaultKeyMode(DEFAULT_KEYS_SEARCH_LOCAL);
+                        } else {
+                            setDefaultKeyMode(DEFAULT_KEYS_DISABLE);
+                        }
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
                         setDefaultKeyMode(DEFAULT_KEYS_DISABLE);
                     }
-                }
-                @Override
-                public void onNothingSelected(AdapterView<?> parent) {
-                    setDefaultKeyMode(DEFAULT_KEYS_DISABLE);
-                }
-            });
-        
+                });
+
         // Attach actions to buttons
         mStartSearch.setOnClickListener(
-            new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    onSearchRequested();
-                }
-            });
+                new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onSearchRequested();
+                    }
+                });
     }
-    
-    /** 
-     * Called when your activity's options menu needs to be updated. 
+
+    /**
+     * Called when your activity's options menu needs to be updated. First we call through to our
+     * super's implementation of {@code onPrepareOptionsMenu}. Then we declare a {@code MenuItem item}.
+     * Then we remove the two {@code MenuItem}'s we may have added in a previous call. Next we
+     * switch based on the position of the currently selected item of our {@code Spinner mMenuMode}:
+     * <ul>
+     * <li>MENUMODE_SEARCH_KEY - we add the MenuItem "(Search Key)" as item ID 0</li>
+     * <li>MENUMODE_MENU_ITEM - we add the MenuItem "Search" as item ID 0, and set its alphabetic shortcut to MENU_KEY</li>
+     * <li>MENUMODE_TYPE_TO_SEARCH - we add the MenuItem "(Type-To-Search)" as item ID 0</li>
+     * <li>MENUMODE_DISABLED - we add the MenuItem "(Disabled)" as item ID 0</li>
+     * </ul>
+     * In all cases we then add the MenuItem "Clear History" as item ID 1 to {@code Menu menu} and
+     * return true to the caller.
+     *
+     * @param menu The options menu as last shown or first initialized by onCreateOptionsMenu().
+     * @return true so the menu will be displayed
      */
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
         super.onPrepareOptionsMenu(menu);
         MenuItem item;
-        
-            // first, get rid of our menus (if any)
+
+        // first, get rid of our menus (if any)
         menu.removeItem(0);
         menu.removeItem(1);
-        
-            // next, add back item(s) based on current menu mode
-        switch (mMenuMode.getSelectedItemPosition())
-        {
-        case MENUMODE_SEARCH_KEY:
-            //noinspection UnusedAssignment
-            item = menu.add( 0, 0, 0, "(Search Key)");
-            break;
-            
-        case MENUMODE_MENU_ITEM:
-            item = menu.add( 0, 0, 0, "Search");
-            item.setAlphabeticShortcut(SearchManager.MENU_KEY);
-            break;
-            
-        case MENUMODE_TYPE_TO_SEARCH:
-            //noinspection UnusedAssignment
-            item = menu.add( 0, 0, 0, "(Type-To-Search)");
-            break;
-            
-        case MENUMODE_DISABLED:
-            //noinspection UnusedAssignment
-            item = menu.add( 0, 0, 0, "(Disabled)");
-            break;
+
+        // next, add back item(s) based on current menu mode
+        switch (mMenuMode.getSelectedItemPosition()) {
+            case MENUMODE_SEARCH_KEY:
+                //noinspection UnusedAssignment
+                item = menu.add(Menu.NONE, 0, Menu.NONE, "(Search Key)");
+                break;
+
+            case MENUMODE_MENU_ITEM:
+                item = menu.add(Menu.NONE, 0, Menu.NONE, "Search");
+                item.setAlphabeticShortcut(SearchManager.MENU_KEY);
+                break;
+
+            case MENUMODE_TYPE_TO_SEARCH:
+                //noinspection UnusedAssignment
+                item = menu.add(Menu.NONE, 0, Menu.NONE, "(Type-To-Search)");
+                break;
+
+            case MENUMODE_DISABLED:
+                //noinspection UnusedAssignment
+                item = menu.add(Menu.NONE, 0, Menu.NONE, "(Disabled)");
+                break;
         }
 
         //noinspection UnusedAssignment
-        item = menu.add(0, 1, 0, "Clear History");
+        item = menu.add(Menu.NONE, 1, Menu.NONE, "Clear History");
         return true;
     }
-    
-    /** Handle the menu item selections */
+
+    /**
+     * This hook is called whenever an item in our options menu is selected.
+     *
+     * @param item The menu item that was selected.
+     * @param item
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-        case 0:
-            switch (mMenuMode.getSelectedItemPosition()) {
-            case MENUMODE_SEARCH_KEY:
-                new AlertDialog.Builder(this)
-                    .setMessage("To invoke search, dismiss this dialog and press the search key" +
-                                " (F5 on the simulator).")
-                    .setPositiveButton("OK", null)
-                    .show();
+            case 0:
+                switch (mMenuMode.getSelectedItemPosition()) {
+                    case MENUMODE_SEARCH_KEY:
+                        new AlertDialog.Builder(this)
+                                .setMessage("To invoke search, dismiss this dialog and press the search key" +
+                                        " (F5 on the simulator).")
+                                .setPositiveButton("OK", null)
+                                .show();
+                        break;
+
+                    case MENUMODE_MENU_ITEM:
+                        onSearchRequested();
+                        break;
+
+                    case MENUMODE_TYPE_TO_SEARCH:
+                        new AlertDialog.Builder(this)
+                                .setMessage("To invoke search, dismiss this dialog and start typing.")
+                                .setPositiveButton("OK", null)
+                                .show();
+                        break;
+
+                    case MENUMODE_DISABLED:
+                        new AlertDialog.Builder(this)
+                                .setMessage("You have disabled search.")
+                                .setPositiveButton("OK", null)
+                                .show();
+                        break;
+                }
                 break;
-                
-            case MENUMODE_MENU_ITEM:
-                onSearchRequested();
+            case 1:
+                clearSearchHistory();
                 break;
-                
-            case MENUMODE_TYPE_TO_SEARCH:
-                new AlertDialog.Builder(this)
-                    .setMessage("To invoke search, dismiss this dialog and start typing.")
-                    .setPositiveButton("OK", null)
-                    .show();
-                break;
-                
-            case MENUMODE_DISABLED:
-                new AlertDialog.Builder(this)
-                    .setMessage("You have disabled search.")
-                    .setPositiveButton("OK", null)
-                    .show();
-                break;
-            }
-            break;
-        case 1:
-            clearSearchHistory();
-            break;
         }
-    
-         return super.onOptionsItemSelected(item);
+
+        return super.onOptionsItemSelected(item);
     }
-    
+
     /**
      * This hook is called when the user signals the desire to start a search.
-     * 
+     * <p>
      * By overriding this hook we can insert local or context-specific data.
-     * 
+     *
      * @return Returns true if search launched, false if activity blocks it
      */
     @Override
@@ -202,13 +233,13 @@ public class SearchInvoke extends Activity
         if (mMenuMode.getSelectedItemPosition() == MENUMODE_DISABLED) {
             return false;
         }
-        
+
         // It's possible to pre-fill the query string before launching the search
         // UI.  For this demo, we simply copy it from the user input field.
         // For most applications, you can simply pass null to startSearch() to
         // open the UI with an empty query string.
         final String queryPrefill = mQueryPrefill.getText().toString();
-        
+
         // Next, set up a bundle to send context-specific search data (if any)
         // The bundle can contain any number of elements, using any number of keys;
         // For this Api Demo we copy a string from the user input field, and store
@@ -221,31 +252,31 @@ public class SearchInvoke extends Activity
             appDataBundle = new Bundle();
             appDataBundle.putString("demo_key", queryAppDataString);
         }
-        
+
         // Now call the Activity member function that invokes the Search Manager UI.
-        startSearch(queryPrefill, false, appDataBundle, false); 
-        
+        startSearch(queryPrefill, false, appDataBundle, false);
+
         // Returning true indicates that we did launch the search, instead of blocking it.
         return true;
     }
-    
+
     /**
      * Any application that implements search suggestions based on previous actions (such as
      * recent queries, page/items viewed, etc.) should provide a way for the user to clear the
      * history.  This gives the user a measure of privacy, if they do not wish for their recent
      * searches to be replayed by other users of the device (via suggestions).
-     * 
-     * This example shows how to clear the search history for apps that use 
+     * <p>
+     * This example shows how to clear the search history for apps that use
      * android.provider.SearchRecentSuggestions.  If you have developed a custom suggestions
      * provider, you'll need to provide a similar API for clearing history.
-     * 
-     * In this sample app we call this method from a "Clear History" menu item.  You could also 
+     * <p>
+     * In this sample app we call this method from a "Clear History" menu item.  You could also
      * implement the UI in your preferences, or any other logical place in your UI.
      */
     private void clearSearchHistory() {
-        SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this, 
+        SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
                 SearchSuggestionSampleProvider.AUTHORITY, SearchSuggestionSampleProvider.MODE);
         suggestions.clearHistory();
     }
-    
+
 }
