63f6680cb3e0739cebe9589f250c6743e1c3b4f5
markgray <mark.t.gray@gmail.com>
2017-06-12 04:00:51 -0400

    Added comments

--- app/src/main/java/com/example/android/apis/graphics/BitmapMesh.java
+++ app/src/main/java/com/example/android/apis/graphics/BitmapMesh.java
@@ -46,17 +46,54 @@ public class BitmapMesh extends GraphicsActivity {
      * {@code onTouchEvent} override.
      */
     private static class SampleView extends View {
+        /**
+         * Number of vertices in X dimension of Bitmap mesh.
+         */
         private static final int WIDTH = 20;
+        /**
+         * Number of vertices in Y dimension of Bitmap mesh.
+         */
         private static final int HEIGHT = 20;
+        /**
+         * Total number of vertices in Bitmap mesh
+         */
         private static final int COUNT = (WIDTH + 1) * (HEIGHT + 1);
 
+        /**
+         * {@code Bitmap} that contains our jpg to be displayed and warped.
+         */
         private final Bitmap mBitmap;
+        /**
+         * Warped Bitmap mesh array of (x,y) vertices
+         */
         private final float[] mVerts = new float[COUNT * 2];
+        /**
+         * Original un-warped Bitmap mesh array of (x,y) vertices
+         */
         private final float[] mOrig = new float[COUNT * 2];
 
+        /**
+         * {@code Matrix} used to translate the {@code Canvas canvas} to (10,10) before drawing the
+         * {@code Bitmap}
+         */
         private final Matrix mMatrix = new Matrix();
+        /**
+         * The inverse of {@code Matrix mMatrix} used to translate points received in touch events
+         * to a (0,0) origin (ie. it converts points in the {@code Canvas canvas} coordinate space
+         * to points in the {@code Matrix mMatrix} translated {@code Bitmap} coordinate space).
+         */
         private final Matrix mInverse = new Matrix();
 
+        /**
+         * Convenience method to set the (x,y) values for a vertex, it simply calculates the array
+         * indices for the x and y values of the vertex in question based on two floats per vertex
+         * and stores the parameters {@code x} and {@code y} where they belong.
+         *
+         * @param array Vertices array
+         * @param index address of vertex to set the (x,y) coordinates of
+         * @param x x coordinate
+         * @param y y coordinate
+         */
         private static void setXY(float[] array, int index, float x, float y) {
             //noinspection PointlessArithmeticExpression
             array[index * 2 + 0] = x;
@@ -69,8 +106,8 @@ public class BitmapMesh extends GraphicsActivity {
 
             mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.beach);
 
-            float w = mBitmap.getWidth();
-            float h = mBitmap.getHeight();
+            float w = mBitmap.getWidth();  // 1050 for our jpg
+            float h = mBitmap.getHeight(); // 788 for our jpg
             // construct our mesh
             int index = 0;
             for (int y = 0; y <= HEIGHT; y++) {
