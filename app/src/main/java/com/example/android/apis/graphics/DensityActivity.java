9c3cb9c936f4466c7d652568916d19771a71b901
markgray <mark.t.gray@gmail.com>
2017-06-29 03:37:28 -0400

    Added comments

--- app/src/main/java/com/example/android/apis/graphics/DensityActivity.java
+++ app/src/main/java/com/example/android/apis/graphics/DensityActivity.java
@@ -19,24 +19,23 @@ package com.example.android.apis.graphics;
 //Need the following import to get access to the app resources, since this
 //class is in a sub-package.
 
-import com.example.android.apis.R;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.os.Bundle;
-import android.graphics.BitmapFactory;
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.ScrollView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.content.Context;
+import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.example.android.apis.R;
 
 /**
  * This activity demonstrates various ways density can cause the scaling of
@@ -45,13 +44,56 @@ import android.util.Log;
  */
 @SuppressWarnings("deprecation")
 public class DensityActivity extends Activity {
-    @SuppressLint("InflateParams")
+    /**
+     * Called when the activity is starting. First we call through to our super's implementation of
+     * {@code onCreate}, then we fetch a handle to the system-level service LAYOUT_INFLATER_SERVICE
+     * to {@code LayoutInflater li}. We set our title to R.string.density_title, and the system
+     * chooses the title from 5 different strings depending on the screen density:
+     * <ul>
+     * <li>values/strings.xml "Density: Unknown Screen"</li>
+     * <li>values-hdpi/strings.xml "Density: High"</li>
+     * <li>values-ldpi/strings.xml "Density: Low"</li>
+     * <li>values-mdpi/strings.xml "Density: Medium"</li>
+     * <li>values-xhdpi/strings.xml "Density: Extra High"</li>
+     * </ul>
+     * We create an instance of {@code LinearLayout root} and set its orientation to VERTICAL. This is
+     * the {@code ViewGroup} we will add 9 rows of {@code LinearLayout layout}'s to, each row consisting
+     * of 120dpi, 160dpi and 240dpi images stored and loaded using different approaches:
+     * <ul>
+     * <li>
+     * "Pre-scaled bitmap in drawable" uses our method {@code addBitmapDrawable} to add the
+     * resource images logo120dpi.png, logo160dpi.png, and logo240dpi.png (loaded using scaling)
+     * to {@code LinearLayout layout} which we then add to {@code root} using our method
+     * {@code addChildToRoot}
+     * </li>
+     * <li>
+     * "Auto-scaled bitmap in drawable" uses our method {@code addBitmapDrawable} to add the
+     * resource images logo120dpi.png, logo160dpi.png, and logo240dpi.png (loaded without scaling)
+     * to {@code LinearLayout layout} which we then add to {@code root} using our method
+     * {@code addChildToRoot}
+     * </li>
+     * <li>
+     * "Pre-scaled resource drawable" uses our method {@code addResourceDrawable} to load the
+     * resource images logo120dpi.png, logo160dpi.png, and logo240dpi.png, loaded using
+     * {@code getDrawable} and used to set the background of a view which it adds
+     * to {@code LinearLayout layout} which we then add to {@code root} using our method
+     * {@code addChildToRoot}
+     * </li>
+     * <li>
+     *     "Inflated layout" inflates the layout file R.layout.density_image_views which creates a
+     *     {@code LinearLayout layout} containing three {@code ImageView}'s which use the resource
+     *     images logo120dpi.png, logo160dpi.png, and logo240dpi.png as their content which we then
+     *     add to {@code root} using our method {@code addChildToRoot}
+     * </li>
+     * </ul>
+     *
+     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final LayoutInflater li = (LayoutInflater) getSystemService(
-                LAYOUT_INFLATER_SERVICE);
+        final LayoutInflater li = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
 
         this.setTitle(R.string.density_title);
         LinearLayout root = new LinearLayout(this);
@@ -78,11 +120,11 @@ public class DensityActivity extends Activity {
         addLabelToRoot(root, "Pre-scaled resource drawable");
         addChildToRoot(root, layout);
 
-        layout = (LinearLayout) li.inflate(R.layout.density_image_views, null);
+        layout = (LinearLayout) li.inflate(R.layout.density_image_views, root, false);
         addLabelToRoot(root, "Inflated layout");
         addChildToRoot(root, layout);
 
-        layout = (LinearLayout) li.inflate(R.layout.density_styled_image_views, null);
+        layout = (LinearLayout) li.inflate(R.layout.density_styled_image_views, root, false);
         addLabelToRoot(root, "Inflated styled layout");
         addChildToRoot(root, layout);
 
@@ -131,11 +173,28 @@ public class DensityActivity extends Activity {
                 LinearLayout.LayoutParams.WRAP_CONTENT));
     }
 
+    /**
+     * Adds the {@code LinearLayout layout} to {@code LinearLayout root} using the layout parameters
+     * LinearLayout root, and WRAP_CONTENT
+     *
+     * @param root   {@code LinearLayout} {@code ViewGroup} which we want to add
+     *               {@code LinearLayout layout} to
+     * @param layout {@code LinearLayout} we are to add to {@code LinearLayout root}
+     */
     private void addChildToRoot(LinearLayout root, LinearLayout layout) {
         root.addView(layout, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT));
     }
 
+    /**
+     * Adds a {@code Bitmap} decoded from resource ID {@code resource} to {@code LinearLayout layout},
+     * optionally using scaling if the flag {@code boolean scale} is true.
+     *
+     * @param layout   {@code LinearLayout} we are to add the resource {@code Drawable} to
+     * @param resource resource ID of a {@code Drawable} to read and use from our resources
+     * @param scale    flag indicating whether our method {@code loadAndPrintDpi} should use scaling when
+     *                 decoding the resource image.
+     */
     private void addBitmapDrawable(LinearLayout layout, int resource, boolean scale) {
         Bitmap bitmap;
         bitmap = loadAndPrintDpi(resource, scale);
@@ -151,7 +210,16 @@ public class DensityActivity extends Activity {
         layout.addView(view);
     }
 
-    @SuppressWarnings("deprecation")
+    /**
+     * Creates a {@code View view}, loads the resource drawable with resource ID {@code resource} into
+     * a {@code Drawable d} and sets the background drawable of {@code view} to it. Sets the layout
+     * parameters of {@code view} to the intrinsic width and height of {@code d} then adds {@code view}
+     * to {@code LinearLayout layout}.
+     *
+     * @param layout   {@code LinearLayout} {@code ViewGroup} we are to add the resource image with
+     *                 resource ID {@code resource} to.
+     * @param resource resource ID of an resource image to load.
+     */
     private void addResourceDrawable(LinearLayout layout, int resource) {
         View view = new View(this);
 
@@ -187,6 +255,14 @@ public class DensityActivity extends Activity {
         layout.addView(view);
     }
 
+    /**
+     * Decodes the resource image with the resource ID {@code id} into a {@code Bitmap} which it returns,
+     * optionally applying scaling if {@code boolean scale} is true.
+     *
+     * @param id    resource ID of an image to load into the {@code Bitmap} we return
+     * @param scale if true we allow {@code decodeResource} to scale the image, false if we want it unscaled
+     * @return {@code Bitmap} decoded from resource ID {@code id} resource image
+     */
     private Bitmap loadAndPrintDpi(int id, boolean scale) {
         Bitmap bitmap;
         if (scale) {
