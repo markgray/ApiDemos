94926512a6f3ec2949af975c3077907e0a544d5a
markgray <mark.t.gray@gmail.com>
2017-06-30 04:00:02 -0400

    Added comments

--- app/src/main/java/com/example/android/apis/graphics/DensityActivity.java
+++ app/src/main/java/com/example/android/apis/graphics/DensityActivity.java
@@ -80,12 +80,46 @@ public class DensityActivity extends Activity {
      * {@code addChildToRoot}
      * </li>
      * <li>
-     *     "Inflated layout" inflates the layout file R.layout.density_image_views which creates a
-     *     {@code LinearLayout layout} containing three {@code ImageView}'s which use the resource
-     *     images logo120dpi.png, logo160dpi.png, and logo240dpi.png as their content which we then
-     *     add to {@code root} using our method {@code addChildToRoot}
+     * "Inflated layout" inflates the layout file R.layout.density_image_views which creates a
+     * {@code LinearLayout layout} containing three {@code ImageView}'s which use the resource
+     * images logo120dpi.png, logo160dpi.png, and logo240dpi.png as their content which we then
+     * add to {@code root} using our method {@code addChildToRoot}
+     * </li>
+     * <li>
+     * "Inflated styled layout" inflates the layout file R.layout.density_styled_image_views
+     * which creates a {@code LinearLayout layout} containing three {@code ImageView}'s which
+     * use style/ImageView120dpi, style/ImageView160dpi, and style/ImageView240dpi to access
+     * images stylogo120dpi.png, stylogo160dpi.png and stylogo240dpi.png as their content
+     * which we then add to {@code root} using our method {@code addChildToRoot}
+     * </li>
+     * <li>
+     * "Pre-scaled bitmap" uses our method {@code addCanvasBitmap} to load the resource images
+     * logo120dpi.png, logo160dpi.png, and logo240dpi.png, (loaded using scaling) into instances
+     * of our custom {@code View} {@code ScaledBitmapView} which it adds to {@code LinearLayout layout}
+     * which we then add to {@code root} using our method {@code addChildToRoot}
+     * </li>
+     * <li>
+     * "Auto-scaled bitmap" uses our method {@code addCanvasBitmap} to load the resource images
+     * logo120dpi.png, logo160dpi.png, and logo240dpi.png, (loaded without scaling) into instances
+     * of our custom {@code View} {@code ScaledBitmapView} which it adds to {@code LinearLayout layout}
+     * which we then add to {@code root} using our method {@code addChildToRoot}
+     * </li>
+     * <li>
+     * "No-dpi resource drawable" uses our method {@code addResourceDrawable} to load the resource
+     * images R.drawable.logonodpi120.png, R.drawable.logonodpi160.png, and R.drawable.logonodpi240.png,
+     * loaded using {@code getDrawable} and used to set the background of a view which it adds to
+     * {@code LinearLayout layout} which we then add to {@code root} using our method
+     * {@code addChildToRoot}
+     * </li>
+     * <li>
+     * "Pre-scaled 9-patch resource drawable" uses our method {@code addNinePatchResourceDrawable}
+     * to add R.drawable.smlnpatch120dpi.9.png, R.drawable.smlnpatch160dpi.9.png, and
+     * R.drawable.smlnpatch240dpi.9.png to {@code Layout layout} which we then add to {@code root}
+     * using our method {@code addChildToRoot}
      * </li>
      * </ul>
+     * Finally we set our content view to {@code LinearLayout root} wrapped in a {@code ScrollView} by
+     * our method {@code scrollWrap}.
      *
      * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use
      */
@@ -159,6 +193,15 @@ public class DensityActivity extends Activity {
         setContentView(scrollWrap(root));
     }
 
+    /**
+     * Wraps the {@code View} it is passed inside a {@code ScrollView}, which it returns. First we
+     * create {@code ScrollView scroller}, then we add our parameter {@code View view} to {@code scroller}
+     * using MATCH_PARENT for the layout parameters for both width and height. Finally we return
+     * {@code scroller} to the caller.
+     *
+     * @param view {@code View} we are to add to a {@code ScrollView} which we return
+     * @return a {@code ScrollView} containing our parameter {@code View view} as its only child
+     */
     private View scrollWrap(View view) {
         ScrollView scroller = new ScrollView(this);
         scroller.addView(view, new ScrollView.LayoutParams(ScrollView.LayoutParams.MATCH_PARENT,
@@ -166,6 +209,12 @@ public class DensityActivity extends Activity {
         return scroller;
     }
 
+    /**
+     * Adds a {@code TextView} displaying the text {@code String text} to the {@code LinearLayout root}.
+     *
+     * @param root The {@code LinearLayout} we are to add a label to
+     * @param text The text for the {@code TextView} we will add to {@code root}
+     */
     private void addLabelToRoot(LinearLayout root, String text) {
         TextView label = new TextView(this);
         label.setText(text);
@@ -175,7 +224,7 @@ public class DensityActivity extends Activity {
 
     /**
      * Adds the {@code LinearLayout layout} to {@code LinearLayout root} using the layout parameters
-     * LinearLayout root, and WRAP_CONTENT
+     * MATCH_PARENT for width, and WRAP_CONTENT for height
      *
      * @param root   {@code LinearLayout} {@code ViewGroup} which we want to add
      *               {@code LinearLayout layout} to
@@ -231,6 +280,17 @@ public class DensityActivity extends Activity {
         layout.addView(view);
     }
 
+    /**
+     * Adds a {@code ScaledBitmapView} to our parameter {@code LinearLayout layout}, created using a
+     * {@code Bitmap} loaded rom the resource image given by the resource ID {@code resource}, and
+     * optionally scaled first if {@code scale} is true.
+     *
+     * @param layout   {@code LinearLayout} we are to add the {@code Bitmap} we load from the resource
+     *                 image given by the resource ID {@code resource} to.
+     * @param resource resource ID of png image we should load into a {@code Bitmap} and add to {@code layout}
+     * @param scale    if true we want to allow {@code loadAndPrintDpi} to scale the image when loading it into
+     *                 a {@code Bitmap}, false if we want it unscaled
+     */
     private void addCanvasBitmap(LinearLayout layout, int resource, boolean scale) {
         Bitmap bitmap;
         bitmap = loadAndPrintDpi(resource, scale);
@@ -242,6 +302,15 @@ public class DensityActivity extends Activity {
         layout.addView(view);
     }
 
+    /**
+     * Loads a nine-patch png image with resource ID {@code resource} from our resources, and adds a
+     * {@code View} using it as the background drawable to the {@code LinearLayout layout}.
+     *
+     * @param layout   {@code LinearLayout} to which we will add the {@code Drawable} we load from the resource
+     *                 image with resource ID {@code resource}
+     * @param resource resource ID for a nine-patch png image to load into a {@code Drawable} and then
+     *                 use as the background for a view which we add to {@code LinearLayout layout}
+     */
     private void addNinePatchResourceDrawable(LinearLayout layout, int resource) {
         View view = new View(this);
 
@@ -275,14 +344,48 @@ public class DensityActivity extends Activity {
         return bitmap;
     }
 
+    /**
+     * Custom {@code View} which uses the scaled width and height of the {@code Bitmap} it contains
+     * given the target density of the current display metrics.
+     */
     private class ScaledBitmapView extends View {
+        /**
+         * {@code Bitmap} we were created to hold and draw when our {@code onDraw} override is called.
+         */
         private Bitmap mBitmap;
 
+        /**
+         * Simple constructor, first we call through to our super's constructor, then we save our parameter
+         * {@code Bitmap bitmap} in our field {@code Bitmap mBitmap}
+         *
+         * @param context {@code Context} to use for resources, "this" {@code DensityActivity} in our case
+         * @param bitmap  {@code Bitmap} we are to hold and Display.
+         */
         public ScaledBitmapView(Context context, Bitmap bitmap) {
             super(context);
             mBitmap = bitmap;
         }
 
+        /**
+         * Measure the view and its content to determine the measured width and the
+         * measured height. This method is invoked by {@link #measure(int, int)} and
+         * should be overridden by subclasses to provide accurate and efficient
+         * measurement of their contents.
+         * <p>
+         * First we call through to our super's implementation of {@code onMeasure}, then we set
+         * {@code DisplayMetrics metrics} to the current display metrics that are in effect for
+         * the resources associated with this view. We use {@code metrics} to determine the width
+         * and height of {@code Bitmap mBitmap} given the target density of the current display
+         * metrics, which we then pass to the method {@code setMeasuredDimension} to store as our
+         * width and height.
+         *
+         * @param widthMeasureSpec  horizontal space requirements as imposed by the parent.
+         *                          The requirements are encoded with
+         *                          {@link android.view.View.MeasureSpec}.
+         * @param heightMeasureSpec vertical space requirements as imposed by the parent.
+         *                          The requirements are encoded with
+         *                          {@link android.view.View.MeasureSpec}.
+         */
         @Override
         protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
@@ -292,6 +395,13 @@ public class DensityActivity extends Activity {
                     mBitmap.getScaledHeight(metrics));
         }
 
+        /**
+         * We implement this to do our drawing. First we call through to our super's implementation of
+         * {@code onDraw}, then we instruct {@code Canvas canvas} to draw {@code Bitmap mBitmap} at
+         * (0,0) using a null {@code Paint}.
+         *
+         * @param canvas the canvas on which the background will be drawn
+         */
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
