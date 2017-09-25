591087f7ca9a1f5a9ba7e5247e3127f0b07e11df
markgray <mark.t.gray@gmail.com>
2017-09-25 04:39:33 -0400

    Added comments

--- app/src/main/java/com/example/android/apis/graphics/spritetext/MatrixStack.java
+++ app/src/main/java/com/example/android/apis/graphics/spritetext/MatrixStack.java
@@ -21,8 +21,6 @@ import android.opengl.Matrix;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
-import static android.opengl.GLES10.glLoadIdentity;
-
 /**
  * A matrix stack, similar to OpenGL ES's internal matrix stack.
  */
@@ -89,12 +87,12 @@ public class MatrixStack {
      * as the output array using {@code mTop} as the index offset into that array, and passing our
      * parameters as the clipping planes.
      *
-     * @param left left vertical clipping plane
-     * @param right right vertical clipping plane
+     * @param left   left vertical clipping plane
+     * @param right  right vertical clipping plane
      * @param bottom bottom horizontal clipping plane
-     * @param top top horizontal clipping plane
-     * @param near near depth clipping planes
-     * @param far far depth clipping planes
+     * @param top    top horizontal clipping plane
+     * @param near   near depth clipping planes
+     * @param far    far depth clipping planes
      */
     public void glFrustumf(float left, float right, float bottom, float top, float near, float far) {
         Matrix.frustumM(mMatrix, mTop, left, right, bottom, top, near, far);
@@ -106,15 +104,169 @@ public class MatrixStack {
      * float values using our method {@code fixedToFloat}, then pass the call to our method
      * {@code glFrustumf}.
      *
-     * @param left left vertical clipping plane
-     * @param right right vertical clipping plane
+     * @param left   left vertical clipping plane
+     * @param right  right vertical clipping plane
      * @param bottom bottom horizontal clipping plane
-     * @param top top horizontal clipping plane
-     * @param near near depth clipping planes
-     * @param far far depth clipping planes
+     * @param top    top horizontal clipping plane
+     * @param near   near depth clipping planes
+     * @param far    far depth clipping planes
      */
     public void glFrustumx(int left, int right, int bottom, int top, int near, int far) {
-        glFrustumf(fixedToFloat(left),fixedToFloat(right),
+        glFrustumf(fixedToFloat(left), fixedToFloat(right),
+                fixedToFloat(bottom), fixedToFloat(top),
+                fixedToFloat(near), fixedToFloat(far));
+    }
+
+    /**
+     * Loads the top of our matrix stack with the identity matrix, we simply call the static method
+     * {@code android.opengl.Matrix.setIdentityM} to do this for us.
+     */
+    public void glLoadIdentity() {
+        Matrix.setIdentityM(mMatrix, mTop);
+    }
+
+    /**
+     * Replace the matrix at the top of our stack with the matrix {@code float[] m}.
+     *
+     * @param m      matrix to load to the top of the matrix stack
+     * @param offset offset to first source location
+     */
+    public void glLoadMatrixf(float[] m, int offset) {
+        System.arraycopy(m, offset, mMatrix, mTop, MATRIX_SIZE);
+    }
+
+    /**
+     * Replace the matrix at the top of our stack with the matrix contained in {@code FloatBuffer m}.
+     *
+     * @param m {@code FloatBuffer} containing matrix to load to the top of the matrix stack
+     */
+    public void glLoadMatrixf(FloatBuffer m) {
+        m.get(mMatrix, mTop, MATRIX_SIZE);
+    }
+
+    /**
+     * Replace the matrix at the top of our stack with the matrix {@code int[] m} converted from fixed
+     * point 16.16 format to {@code float} format.
+     *
+     * @param m      matrix to load to the top of the matrix stack
+     * @param offset offset to first source location
+     */
+    public void glLoadMatrixx(int[] m, int offset) {
+        for (int i = 0; i < MATRIX_SIZE; i++) {
+            mMatrix[mTop + i] = fixedToFloat(m[offset + i]);
+        }
+    }
+
+    /**
+     * Replace the matrix at the top of our stack with the matrix contained in {@code IntBuffer m}
+     * converted from fixed point 16.16 format to {@code float} format.
+     *
+     * @param m {@code IntBuffer} containing 16.16 format matrix to load to the top of the matrix stack
+     */
+    public void glLoadMatrixx(IntBuffer m) {
+        for (int i = 0; i < MATRIX_SIZE; i++) {
+            mMatrix[mTop + i] = fixedToFloat(m.get());
+        }
+    }
+
+    /**
+     * Replaces our top of stack matrix by the results of multiplying our top of stack matrix (lhs)
+     * by the parameter matrix {@code float[] m} (rhs). Note that when this result is then used to
+     * multiply a vector it is as if the rhs {@code m} matrix is used to multiply first, then the old
+     * lhs top of stack matrix (a result of the way matrix multiplication works which might run
+     * counter to your intuition).
+     * <p>
+     * First we copy our top of stack matrix to {@code float[] mTemp}, then we use the static method
+     * {@code android.opengl.Matrix.multiplyMM} to multiply {@code mTemp} by {@code m} placing the
+     * result in our top of stack matrix.
+     *
+     * @param m      matrix to multiply our top of stack matrix by
+     * @param offset offset to first source location in {@code float[] m}
+     */
+    public void glMultMatrixf(float[] m, int offset) {
+        System.arraycopy(mMatrix, mTop, mTemp, 0, MATRIX_SIZE);
+        Matrix.multiplyMM(mMatrix, mTop, mTemp, 0, m, offset);
+    }
+
+    /**
+     * Replaces our top of stack matrix by the results of multiplying our top of stack matrix (lhs)
+     * by the matrix contained in our parameter {@code FloatBuffer m} (rhs).
+     * <p>
+     * To do this we {@code get} the matrix into the second matrix in our {@code float[] mTemp} temp
+     * matrix storage, then call our method {@code glMultMatrixf(float[], int)} using this copy of the
+     * matrix that was contained in {@code m}.
+     *
+     * @param m {@code FloatBuffer} containing a matrix to multiply our top of stack matrix by
+     */
+    public void glMultMatrixf(FloatBuffer m) {
+        m.get(mTemp, MATRIX_SIZE, MATRIX_SIZE);
+        glMultMatrixf(mTemp, MATRIX_SIZE);
+    }
+
+    /**
+     * Replaces our top of stack matrix by the results of multiplying our top of stack matrix (lhs)
+     * by the parameter matrix {@code int[] m} (rhs). To do this we convert the 16.16 fixed point
+     * values of {@code m} to {@code float} values, storing the result in the second matrix in our
+     * {@code float[] mTemp} temp matrix storage. We then call our method {@code glMultMatrixf(float[], int)}
+     * using this converted copy of the matrix {@code m}.
+     *
+     * @param m      matrix to multiply our top of stack matrix by
+     * @param offset offset to first source location
+     */
+    public void glMultMatrixx(int[] m, int offset) {
+        for (int i = 0; i < MATRIX_SIZE; i++) {
+            mTemp[MATRIX_SIZE + i] = fixedToFloat(m[offset + i]);
+        }
+        glMultMatrixf(mTemp, MATRIX_SIZE);
+    }
+
+    /**
+     * Replaces our top of stack matrix by the results of multiplying our top of stack matrix (lhs)
+     * by the matrix contained in our parameter {@code IntBuffer m}. To do this we {@code get} and
+     * convert the 16.16 fixed point values of {@code m} to {@code float} values, storing the result
+     * in the second matrix in our {@code float[] mTemp} temp matrix storage. We then call our method
+     * {@code glMultMatrixf(float[], int)} using this converted copy of the matrix contained in
+     * {@code m}.
+     *
+     * @param m {@code IntBuffer} containing 16.16 format matrix to multiply our top of stack matrix by.
+     */
+    public void glMultMatrixx(IntBuffer m) {
+        for (int i = 0; i < MATRIX_SIZE; i++) {
+            mTemp[MATRIX_SIZE + i] = fixedToFloat(m.get());
+        }
+        glMultMatrixf(mTemp, MATRIX_SIZE);
+    }
+
+    /**
+     * Replaces our top of stack matrix with an orthographic projection matrix based on its input
+     * parameters. We simply call the static method {@code android.opengl.Matrix.orthoM} to do all
+     * our work for us.
+     *
+     * @param left   left vertical clipping plane
+     * @param right  right vertical clipping plane
+     * @param bottom bottom horizontal clipping plane
+     * @param top    top horizontal clipping plane
+     * @param near   near depth clipping plane
+     * @param far    far depth clipping plane
+     */
+    public void glOrthof(float left, float right, float bottom, float top, float near, float far) {
+        Matrix.orthoM(mMatrix, mTop, left, right, bottom, top, near, far);
+    }
+
+    /**
+     * Replaces our top of stack matrix with an orthographic projection matrix based on its input
+     * parameters. We pass our input parameters converted from fixed point 16.16 format to {@code float}
+     * to our method {@code glOrthof(float, float, float, float, float, float)}.
+     *
+     * @param left   left vertical clipping plane
+     * @param right  right vertical clipping plane
+     * @param bottom bottom horizontal clipping plane
+     * @param top    top horizontal clipping plane
+     * @param near   near depth clipping plane
+     * @param far    far depth clipping plane
+     */
+    public void glOrthox(int left, int right, int bottom, int top, int near, int far) {
+        glOrthof(fixedToFloat(left), fixedToFloat(right),
                 fixedToFloat(bottom), fixedToFloat(top),
                 fixedToFloat(near), fixedToFloat(far));
     }
@@ -122,64 +274,6 @@ public class MatrixStack {
     /**
      * 
      */
-    public void glLoadIdentity() {
-        Matrix.setIdentityM(mMatrix, mTop);
-    }
-
-    public void glLoadMatrixf(float[] m, int offset) {
-        System.arraycopy(m, offset, mMatrix, mTop, MATRIX_SIZE);
-    }
-
-    public void glLoadMatrixf(FloatBuffer m) {
-        m.get(mMatrix, mTop, MATRIX_SIZE);
-    }
-
-    public void glLoadMatrixx(int[] m, int offset) {
-        for(int i = 0; i < MATRIX_SIZE; i++) {
-            mMatrix[mTop + i] = fixedToFloat(m[offset + i]);
-        }
-    }
-
-    public void glLoadMatrixx(IntBuffer m) {
-        for(int i = 0; i < MATRIX_SIZE; i++) {
-            mMatrix[mTop + i] = fixedToFloat(m.get());
-        }
-    }
-
-    public void glMultMatrixf(float[] m, int offset) {
-        System.arraycopy(mMatrix, mTop, mTemp, 0, MATRIX_SIZE);
-        Matrix.multiplyMM(mMatrix, mTop, mTemp, 0, m, offset);
-    }
-
-    public void glMultMatrixf(FloatBuffer m) {
-        m.get(mTemp, MATRIX_SIZE, MATRIX_SIZE);
-        glMultMatrixf(mTemp, MATRIX_SIZE);
-    }
-
-    public void glMultMatrixx(int[] m, int offset) {
-        for(int i = 0; i < MATRIX_SIZE; i++) {
-            mTemp[MATRIX_SIZE + i] = fixedToFloat(m[offset + i]);
-        }
-        glMultMatrixf(mTemp, MATRIX_SIZE);
-    }
-
-    public void glMultMatrixx(IntBuffer m) {
-        for(int i = 0; i < MATRIX_SIZE; i++) {
-            mTemp[MATRIX_SIZE + i] = fixedToFloat(m.get());
-        }
-        glMultMatrixf(mTemp, MATRIX_SIZE);
-    }
-
-    public void glOrthof(float left, float right, float bottom, float top, float near, float far) {
-        Matrix.orthoM(mMatrix, mTop, left, right, bottom, top, near, far);
-    }
-
-    public void glOrthox(int left, int right, int bottom, int top, int near, int far) {
-        glOrthof(fixedToFloat(left), fixedToFloat(right),
-                fixedToFloat(bottom), fixedToFloat(top),
-                fixedToFloat(near), fixedToFloat(far));
-    }
-
     public void glPopMatrix() {
         preflight_adjust(-1);
         adjust(-1);
@@ -187,8 +281,7 @@ public class MatrixStack {
 
     public void glPushMatrix() {
         preflight_adjust(1);
-        System.arraycopy(mMatrix, mTop, mMatrix, mTop + MATRIX_SIZE,
-                MATRIX_SIZE);
+        System.arraycopy(mMatrix, mTop, mMatrix, mTop + MATRIX_SIZE, MATRIX_SIZE);
         adjust(1);
     }
 
