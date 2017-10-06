2834b731aa1e1cff82b8e0aba8d07b746e9cd554
markgray <mark.t.gray@gmail.com>
2017-10-06 05:05:45 -0400

    Added comments

--- app/src/main/java/com/example/android/apis/graphics/Patterns.java
+++ app/src/main/java/com/example/android/apis/graphics/Patterns.java
@@ -92,17 +92,72 @@ public class Patterns extends GraphicsActivity {
      * a rectangle pattern, and one movable on top of it consisting of a circle pattern.
      */
     private static class SampleView extends View {
+        /**
+         * {@code Shader} consisting of a pattern of blue rectangles on a red background.
+         */
         private final Shader mShader1;
+        /**
+         * {@code Shader} consisting of a pattern of green circles on an uncolored background.
+         */
         private final Shader mShader2;
+        /**
+         * {@code Paint} used to draw to our {@code Canvas}
+         */
         private final Paint mPaint;
+        /**
+         * {@code PaintFlagsDrawFilter} that clears the Paint.FILTER_BITMAP_FLAG and Paint.DITHER_FLAG
+         * of the {@code Paint} used to draw to the {@code Canvas} when it is set as the draw filter
+         * of that {@code Canvas}. {@code DrawFilter mDF} is set to it when an ACTION_DOWN touch event
+         * is received (and {@code mDF} is set to null on an ACTION_UP event). See {@code mDF}.
+         */
         private final DrawFilter mFastDF;
 
+        /**
+         * x coordinate of the last ACTION_DOWN event
+         */
         private float mTouchStartX;
+        /**
+         * y coordinate of the last ACTION_DOWN event
+         */
         private float mTouchStartY;
+        /**
+         * x coordinate of the last ACTION_MOVE event
+         */
         private float mTouchCurrX;
+        /**
+         * y coordinate of the last ACTION_MOVE event
+         */
         private float mTouchCurrY;
+        /**
+         * {@code DrawFilter} that is used as the draw filter of the {@code Canvas} we are drawing to.
+         * It is set to {@code DrawFilter mFastDF} on an ACTION_DOWN event and to null on an ACTION_UP
+         * event.
+         */
         private DrawFilter mDF;
 
+        /**
+         * Our constructor. First we call our super's constructor, then we enable our view to receive
+         * focus, and to receive focus in touch mode. We initialize our field {@code DrawFilter mFastDF}
+         * with a new instance of {@code PaintFlagsDrawFilter} configured to clear the {@code Paint}
+         * flags Paint.FILTER_BITMAP_FLAG and Paint.DITHER_FLAG if it is set as the draw filter of a
+         * canvas. We initialize our field {@code Shader mShader1} with a new instance of
+         * {@code BitmapShader} created using the {@code Bitmap} returned from our method {@code makeBitmap1}
+         * (a blue rectangle with a red background) and configured to repeat in both the x and y
+         * directions. We initialize our field {@code Shader mShader2} with a new instance of
+         * {@code BitmapShader} created using the {@code Bitmap} returned from our method {@code makeBitmap2}
+         * (a green circle with an uncolored background) and configured to repeat in both the x and y
+         * directions.
+         * <p>
+         * We create a new instance for {@code Matrix m}, set the matrix to rotate about (0,0) by
+         * 30 degrees, and use it to set the local matrix of {@code Shader mShader2} (rotates the
+         * pattern by 30 degrees when it is drawn).
+         * <p>
+         * Finally we allocate a new instance of {@code Paint} for our field {@code Paint mPaint},
+         * setting the Paint.FILTER_BITMAP_FLAG on it (enables bilinear sampling on scaled bitmaps).
+         *
+         * @param context {@code Context} to use to access resources, "this" when called from the
+         *                {@code onCreate} method of the {@code Patterns} activity.
+         */
         public SampleView(Context context) {
             super(context);
             setFocusable(true);
@@ -120,6 +175,21 @@ public class Patterns extends GraphicsActivity {
             mPaint = new Paint(Paint.FILTER_BITMAP_FLAG);
         }
 
+        /**
+         * We implement this to do our drawing. First we set the draw filter of our parameter
+         * {@code Canvas canvas} to our field {@code DrawFilter mDF} (this will either be the
+         * contents of our field {@code DrawFilter mFastDF} after an ACTION_DOWN event or null
+         * after an ACTION_UP event). We set the {@code Shader} object of {@code Paint mPaint} to
+         * {@code mShader1} (blue rectangles on a red background) and fill the {@code Canvas canvas}
+         * bitmap with that pattern. Then we move the {@code Canvas} by the movement in x and y implied
+         * by the last ACTION_MOVE event ({@code mTouchCurrX - mTouchStartX} in the x direction and
+         * {@code mTouchCurrY - mTouchStartY} in the y direction). We set the {@code Shader} object
+         * of {@code Paint mPaint} to {@code mShader2} (green circles on an uncolored background) and
+         * fill the {@code Canvas canvas} bitmap with that pattern. The rectangle pattern of
+         * {@code mShader1} will show through the uncolored background of {@code mShader1}.
+         *
+         * @param canvas the canvas on which the background will be drawn
+         */
         @Override
         protected void onDraw(Canvas canvas) {
             canvas.setDrawFilter(mDF);
@@ -128,12 +198,37 @@ public class Patterns extends GraphicsActivity {
             canvas.drawPaint(mPaint);
 
             canvas.translate(mTouchCurrX - mTouchStartX,
-                             mTouchCurrY - mTouchStartY);
+                    mTouchCurrY - mTouchStartY);
 
             mPaint.setShader(mShader2);
             canvas.drawPaint(mPaint);
         }
 
+        /**
+         * Implement this method to handle touch screen motion events. First we fetch the x coordinate
+         * of the {@code MotionEvent event} to {@code float x} and the y coordinate to {@code float y}.
+         * Then we switch based on the kind of action of the {@code event}:
+         * <ul>
+         * <li>
+         * ACTION_DOWN - We set our fields {@code mTouchStartX} and {@code mTouchCurrX} to x
+         * and our fields {@code mTouchStartY} and {@code mTouchCurrY}. We set our field
+         * {@code DrawFilter mDF} to the contents of {@code DrawFilter mFastDF}, and invalidate
+         * our view so that our {@code onDraw} method will be called.
+         * </li>
+         * <li>
+         * ACTION_MOVE - We set our field {@code mTouchCurrX} to x, and {@code mTouchCurrY}
+         * to y and invalidate our view so that our {@code onDraw} method will be called.
+         * </li>
+         * <li>
+         * ACTION_UP - We set our field {@code DrawFilter mDF} to null and invalidate our
+         * view so that our {@code onDraw} method will be called.
+         * </li>
+         * </ul>
+         * In all cases we return true to the called to indicate that we handled the {@code MotionEvent}.
+         *
+         * @param event The motion event.
+         * @return True if the event was handled, false otherwise. We always return true.
+         */
         @Override
         public boolean onTouchEvent(MotionEvent event) {
             float x = event.getX();
--- app/src/main/java/com/example/android/apis/graphics/Pictures.java
+++ app/src/main/java/com/example/android/apis/graphics/Pictures.java
@@ -26,18 +26,55 @@ import android.view.View;
 
 import java.io.*;
 
+/**
+ * Shows how to use the Picture class to record drawing instructions performed on the Canvas returned
+ * by Picture.beginRecording(width, height), then after calling Picture.endRecording() the Picture is
+ * turned into a Drawable by calling PictureDrawable(Picture). Then in the onDraw method it first
+ * draws the Picture using the Canvas.drawPicture(Picture) method then it stretches the Picture when
+ * drawing using the Canvas.drawPicture(Picture,RectF) method, then it draws the Drawable it created
+ * with PictureDrawable(Picture) to draw using Drawable.draw(Canvas), then it writes the Picture to
+ * a ByteArrayOutputStream and draws it by reading that ByteArrayOutputStream back in and then drawing
+ * it using Canvas.drawPicture(Picture.createFromStream(is))
+ */
 public class Pictures extends GraphicsActivity {
 
+    /**
+     * Called when the activity is starting. First we call our super's implementation of {@code onCreate}
+     * then we set our content view to a new instance of {@code SampleView}.
+     *
+     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new SampleView(this));
     }
 
+    /**
+     * Custom view demonstrating some of the features of the {@code Picture} class.
+     */
     private static class SampleView extends View {
+        /**
+         * Our {@code Picture} consisting of a pink circle (the alpha of 0x88 lets the white background
+         * lighten the red circle) with the green text "Pictures" partially obscuring it.
+         */
         private Picture mPicture;
+        /**
+         * {@code PictureDrawable} created out of {@code Picture mPicture}.
+         */
         private Drawable mDrawable;
 
+        /**
+         * Draws a pink circle (the alpha of 0x88 lets the white background lighten the red circle)
+         * with the green text "Pictures" partially obscuring it. First we allocate a new instance of
+         * {@code Paint} for {@code Paint p}, with the ANTI_ALIAS_FLAG set. We set the color of {@code p}
+         * to red with an alpha of 0x88 and use it to draw a 40 pixel radius circle on {@code Canvas canvas}
+         * with its center at (50,50). We set the color of {@code p} to GREEN, set its text size to 30,
+         * and use it to draw the text "Pictures" at location (60,60) on {@code canvas}.
+         *
+         * @param canvas {@code Canvas} to draw to, it is created by calling the {@code beginRecording}
+         *               method of our field {@code Picture mPicture}.
+         */
         static void drawSomething(Canvas canvas) {
             Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
 
@@ -49,6 +86,18 @@ public class Pictures extends GraphicsActivity {
             canvas.drawText("Pictures", 60, 60, p);
         }
 
+        /**
+         * Our constructor. First we call our super's constructor, then we enable our view to receive
+         * focus, and to receive focus in touch mode. We create a new instance of {@code Picture} for
+         * our field {@code Picture mPicture}, call its {@code beginRecording} method specifying a
+         * width of 200, and a height of 100, and pass the {@code Canvas} returned to our method
+         * {@code drawSomething}. When {@code drawSomething} is done drawing to the {@code mPicture}
+         * canvas we call the {@code endRecording} method of {@code mPicture}. Finally we create a
+         * {@code PictureDrawable} from {@code mPicture} for our field {@code Drawable mDrawable}.
+         *
+         * @param context {@code Context} to use to access resources, "this" when called from the
+         *                {@code onCreate} method of the {@code Pictures} activity.
+         */
         public SampleView(Context context) {
             super(context);
             setFocusable(true);
@@ -61,6 +110,11 @@ public class Pictures extends GraphicsActivity {
             mDrawable = new PictureDrawable(mPicture);
         }
 
+        /**
+         * We implement this to do our drawing.
+         *
+         * @param canvas the canvas on which the background will be drawn
+         */
         @SuppressLint("DrawAllocation")
         @Override
         protected void onDraw(Canvas canvas) {
