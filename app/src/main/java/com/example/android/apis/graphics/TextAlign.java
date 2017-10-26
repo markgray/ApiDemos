33588902f052374d7b8441115510cec5d6a6e4d1
markgray <mark.t.gray@gmail.com>
2017-10-26 04:29:23 -0400

    Upgrade to Studio 3.0, and comments

--- app/src/main/java/com/example/android/apis/graphics/TextAlign.java
+++ app/src/main/java/com/example/android/apis/graphics/TextAlign.java
@@ -27,48 +27,83 @@ import android.view.*;
  */
 public class TextAlign extends GraphicsActivity {
 
+    /**
+     * Called when the activity is starting. First we call through to our super's implementation of
+     * {@code onCreate}, then we set our content view to a new instance of {@code SampleView}.
+     *
+     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new SampleView(this));
     }
 
+    /**
+     * Our custom View, it simply displays our text samples.
+     */
     private static class SampleView extends View {
-        private Paint   mPaint;
-        private float   mX;
+        /**
+         * {@code Paint} we use to draw our text and some spacing lines with.
+         */
+        private Paint mPaint;
+        /**
+         * X coordinate of the center of the screen.
+         */
+        private float mX;
+        /**
+         * x and y coordinates of each character in our "Positioned" text sample.
+         */
         private float[] mPos;
 
-        private Path    mPath;
-        private Paint   mPathPaint;
+        /**
+         * {@code Path} we use for our "Along a path" text sample.
+         */
+        private Path mPath;
+        /**
+         * {@code Paint} we use to draw the {@code Path} under our "Along a path" text sample.
+         */
+        private Paint mPathPaint;
 
+        /**
+         * Distance between lines of text.
+         */
         private static final int DY = 30;
+        /**
+         * String used for the "Left" aligned text.
+         */
         private static final String TEXT_L = "Left";
+        /**
+         * String used for the "Center" aligned text.
+         */
         private static final String TEXT_C = "Center";
+        /**
+         * String used for the "Right" aligned text.
+         */
         private static final String TEXT_R = "Right";
+        /**
+         * String used for the "Positioned" text.
+         */
         private static final String POSTEXT = "Positioned";
+        /**
+         * String used for the "Along a path" text sample.
+         */
         private static final String TEXTONPATH = "Along a path";
 
-        private static void makePath(Path p) {
-            p.moveTo(10, 0);
-            p.cubicTo(100, -50, 200, 50, 300, 0);
-        }
-
-        private float[] buildTextPositions(String text, float y, Paint paint) {
-            float[] widths = new float[text.length()];
-            // initially get the widths for each char
-            int n = paint.getTextWidths(text, widths);
-            // now popuplate the array, interleaving spaces for the Y values
-            float[] pos = new float[n * 2];
-            float accumulatedX = 0;
-            for (int i = 0; i < n; i++) {
-                //noinspection PointlessArithmeticExpression
-                pos[i*2 + 0] = accumulatedX;
-                pos[i*2 + 1] = y;
-                accumulatedX += widths[i];
-            }
-            return pos;
-        }
-
+        /**
+         * Our constructor. First we call our super's constructor, and then enable our view to receive
+         * focus. We initialize our field {@code Paint mPaint} with a new instance of {@code Paint},
+         * set its anti alias flag to true, its text size to 30, and its typeface to SERIF. We initialize
+         * our field {@code float[] mPos} with the character positions that the characters in the
+         * string POSTEXT ("Positioned") when it is drawn. We allocate a new instance of {@code Path}
+         * for {@code Path mPath} and call our method {@code makePath} to fill {@code mPath} with a
+         * cubic bezier from (10,0) to (300,0). Then we allocate a new instance of {@code Paint} for
+         * {@code Paint mPathPaint}, set its anti alias flag to true, set its color to blue (with an
+         * alpha of 0x80), and set its style to STROKE.
+         *
+         * @param context {@code Content} to access resources, "this" in the {@code onCreate} method
+         *                of {@code TextAlign}.
+         */
         public SampleView(Context context) {
             super(context);
             setFocusable(true);
@@ -89,7 +124,46 @@ public class TextAlign extends GraphicsActivity {
             mPathPaint.setStyle(Paint.Style.STROKE);
         }
 
-        @Override protected void onDraw(Canvas canvas) {
+        /**
+         * Fills its parameter {@code Path p} with a cubic bezier from (10,0) to (300,0) using the
+         * control points (100,-50) and (200,50).
+         *
+         * @param p {@code Path} we are to initialize.
+         */
+        private static void makePath(Path p) {
+            p.moveTo(10, 0);
+            p.cubicTo(100, -50, 200, 50, 300, 0);
+        }
+
+        /**
+         * Creates and returns an array of x, y coordinates for drawing each individual character in
+         * the parameter {@code String text}.
+         *
+         * @param text  String whose characters we are to determine the position of.
+         * @param y     y coordinate position for all the characters.
+         * @param paint {@code Paint} to use to get the text widths of the characters in {@code text}
+         * @return An array of x,y coordinates for the position of each character in {@code text}
+         * when is is drawn.
+         */
+        @SuppressWarnings("SameParameterValue")
+        private float[] buildTextPositions(String text, float y, Paint paint) {
+            float[] widths = new float[text.length()];
+            // initially get the widths for each char
+            int n = paint.getTextWidths(text, widths);
+            // now populate the array, interleaving spaces for the Y values
+            float[] pos = new float[n * 2];
+            float accumulatedX = 0;
+            for (int i = 0; i < n; i++) {
+                //noinspection PointlessArithmeticExpression
+                pos[i * 2 + 0] = accumulatedX;
+                pos[i * 2 + 1] = y;
+                accumulatedX += widths[i];
+            }
+            return pos;
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
             canvas.drawColor(Color.WHITE);
 
             Paint p = mPaint;
@@ -100,7 +174,7 @@ public class TextAlign extends GraphicsActivity {
             // draw the normal strings
 
             p.setColor(0x80FF0000);
-            canvas.drawLine(x, y, x, y+DY*3, p);
+            canvas.drawLine(x, y, x, y + DY * 3, p);
             p.setColor(Color.BLACK);
 
             canvas.translate(0, DY);
@@ -115,15 +189,15 @@ public class TextAlign extends GraphicsActivity {
             p.setTextAlign(Paint.Align.RIGHT);
             canvas.drawText(TEXT_R, x, y, p);
 
-            canvas.translate(100, DY*2);
+            canvas.translate(100, DY * 2);
 
             // now draw the positioned strings
 
             p.setColor(0xBB00FF00);
-            for (int i = 0; i < pos.length/2; i++) {
+            for (int i = 0; i < pos.length / 2; i++) {
                 //noinspection PointlessArithmeticExpression
-                canvas.drawLine(pos[i*2+0], pos[i*2+1]-DY,
-                                pos[i*2+0], pos[i*2+1]+DY*2, p);
+                canvas.drawLine(pos[i * 2 + 0], pos[i * 2 + 1] - DY,
+                        pos[i * 2 + 0], pos[i * 2 + 1] + DY * 2, p);
             }
             p.setColor(Color.BLACK);
 
@@ -143,18 +217,18 @@ public class TextAlign extends GraphicsActivity {
 
             // now draw the text on path
 
-            canvas.translate(-100, DY*2);
+            canvas.translate(-100, DY * 2);
 
             canvas.drawPath(mPath, mPathPaint);
             p.setTextAlign(Paint.Align.LEFT);
             canvas.drawTextOnPath(TEXTONPATH, mPath, 0, 0, p);
 
-            canvas.translate(0, DY*1.5f);
+            canvas.translate(0, DY * 1.5f);
             canvas.drawPath(mPath, mPathPaint);
             p.setTextAlign(Paint.Align.CENTER);
             canvas.drawTextOnPath(TEXTONPATH, mPath, 0, 0, p);
 
-            canvas.translate(0, DY*1.5f);
+            canvas.translate(0, DY * 1.5f);
             canvas.drawPath(mPath, mPathPaint);
             p.setTextAlign(Paint.Align.RIGHT);
             canvas.drawTextOnPath(TEXTONPATH, mPath, 0, 0, p);
