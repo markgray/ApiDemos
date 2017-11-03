ff4b0802b0226a3efc359c6bd598478cc788e3da
markgray <mark.t.gray@gmail.com>
2017-11-03 04:41:17 -0400

    Added comments, scaled for different dpi

--- app/src/main/java/com/example/android/apis/graphics/Xfermodes.java
+++ app/src/main/java/com/example/android/apis/graphics/Xfermodes.java
@@ -16,6 +16,7 @@
 
 package com.example.android.apis.graphics;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
@@ -31,6 +32,11 @@ import android.graphics.Xfermode;
 import android.os.Bundle;
 import android.view.View;
 
+/**
+ * Shows the results of using different Xfermode's when drawing an overlapping square and a circle
+ * to the same Canvas. It was way too small on the newer devices (froyo OK). So I modified it to
+ * scale for dpi.
+ */
 public class Xfermodes extends GraphicsActivity {
 
     // create a bitmap with a circle, used for the "dst" image
@@ -40,7 +46,7 @@ public class Xfermodes extends GraphicsActivity {
         Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
 
         p.setColor(0xFFFFCC44);
-        c.drawOval(new RectF(0, 0, w*3/4, h*3/4), p);
+        c.drawOval(new RectF(0, 0, w * 3 / 4, h * 3 / 4), p);
         return bm;
     }
 
@@ -51,7 +57,7 @@ public class Xfermodes extends GraphicsActivity {
         Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
 
         p.setColor(0xFF66AAFF);
-        c.drawRect(w/3, h/3, w*19/20, h*19/20, p);
+        c.drawRect(w / 3, h / 3, w * 19 / 20, h * 19 / 20, p);
         return bm;
     }
 
@@ -62,6 +68,7 @@ public class Xfermodes extends GraphicsActivity {
     }
 
     private static class SampleView extends View {
+        private float SCREEN_DENSITY;
         private static final int W = 64;
         private static final int H = 64;
         private static final int ROW_MAX = 4;   // number of samples per row
@@ -71,55 +78,57 @@ public class Xfermodes extends GraphicsActivity {
         private Shader mBG;     // background checker-board pattern
 
         private static final Xfermode[] sModes = {
-            new PorterDuffXfermode(PorterDuff.Mode.CLEAR),
-            new PorterDuffXfermode(PorterDuff.Mode.SRC),
-            new PorterDuffXfermode(PorterDuff.Mode.DST),
-            new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER),
-            new PorterDuffXfermode(PorterDuff.Mode.DST_OVER),
-            new PorterDuffXfermode(PorterDuff.Mode.SRC_IN),
-            new PorterDuffXfermode(PorterDuff.Mode.DST_IN),
-            new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT),
-            new PorterDuffXfermode(PorterDuff.Mode.DST_OUT),
-            new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP),
-            new PorterDuffXfermode(PorterDuff.Mode.DST_ATOP),
-            new PorterDuffXfermode(PorterDuff.Mode.XOR),
-            new PorterDuffXfermode(PorterDuff.Mode.DARKEN),
-            new PorterDuffXfermode(PorterDuff.Mode.LIGHTEN),
-            new PorterDuffXfermode(PorterDuff.Mode.MULTIPLY),
-            new PorterDuffXfermode(PorterDuff.Mode.SCREEN)
+                new PorterDuffXfermode(PorterDuff.Mode.CLEAR),
+                new PorterDuffXfermode(PorterDuff.Mode.SRC),
+                new PorterDuffXfermode(PorterDuff.Mode.DST),
+                new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER),
+                new PorterDuffXfermode(PorterDuff.Mode.DST_OVER),
+                new PorterDuffXfermode(PorterDuff.Mode.SRC_IN),
+                new PorterDuffXfermode(PorterDuff.Mode.DST_IN),
+                new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT),
+                new PorterDuffXfermode(PorterDuff.Mode.DST_OUT),
+                new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP),
+                new PorterDuffXfermode(PorterDuff.Mode.DST_ATOP),
+                new PorterDuffXfermode(PorterDuff.Mode.XOR),
+                new PorterDuffXfermode(PorterDuff.Mode.DARKEN),
+                new PorterDuffXfermode(PorterDuff.Mode.LIGHTEN),
+                new PorterDuffXfermode(PorterDuff.Mode.MULTIPLY),
+                new PorterDuffXfermode(PorterDuff.Mode.SCREEN)
         };
 
         private static final String[] sLabels = {
-            "Clear", "Src", "Dst", "SrcOver",
-            "DstOver", "SrcIn", "DstIn", "SrcOut",
-            "DstOut", "SrcATop", "DstATop", "Xor",
-            "Darken", "Lighten", "Multiply", "Screen"
+                "Clear", "Src", "Dst", "SrcOver",
+                "DstOver", "SrcIn", "DstIn", "SrcOut",
+                "DstOut", "SrcATop", "DstATop", "Xor",
+                "Darken", "Lighten", "Multiply", "Screen"
         };
 
         public SampleView(Context context) {
             super(context);
 
+            SCREEN_DENSITY = getResources().getDisplayMetrics().density;
             mSrcB = makeSrc(W, H);
             mDstB = makeDst(W, H);
 
-            // make a ckeckerboard pattern
-            Bitmap bm = Bitmap.createBitmap(new int[] { 0xFFFFFFFF, 0xFFCCCCCC,
-                                            0xFFCCCCCC, 0xFFFFFFFF }, 2, 2,
-                                            Bitmap.Config.RGB_565);
-            mBG = new BitmapShader(bm,
-                                   Shader.TileMode.REPEAT,
-                                   Shader.TileMode.REPEAT);
+            // make a checkerboard pattern
+            Bitmap bm = Bitmap.createBitmap(new int[]{0xFFFFFFFF, 0xFFCCCCCC, 0xFFCCCCCC, 0xFFFFFFFF},
+                    2, 2, Bitmap.Config.RGB_565);
+            mBG = new BitmapShader(bm, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
             Matrix m = new Matrix();
             m.setScale(6, 6);
             mBG.setLocalMatrix(m);
         }
 
-        @Override protected void onDraw(Canvas canvas) {
+        @Override
+        protected void onDraw(Canvas canvas) {
             canvas.drawColor(Color.WHITE);
 
+            canvas.scale(SCREEN_DENSITY, SCREEN_DENSITY);
+            @SuppressLint("DrawAllocation")
             Paint labelP = new Paint(Paint.ANTI_ALIAS_FLAG);
             labelP.setTextAlign(Paint.Align.CENTER);
 
+            @SuppressLint("DrawAllocation")
             Paint paint = new Paint();
             paint.setFilterBitmap(false);
 
@@ -131,8 +140,7 @@ public class Xfermodes extends GraphicsActivity {
                 // draw the border
                 paint.setStyle(Paint.Style.STROKE);
                 paint.setShader(null);
-                canvas.drawRect(x - 0.5f, y - 0.5f,
-                                x + W + 0.5f, y + H + 0.5f, paint);
+                canvas.drawRect(x - 0.5f, y - 0.5f, x + W + 0.5f, y + H + 0.5f, paint);
 
                 // draw the checker-board pattern
                 paint.setStyle(Paint.Style.FILL);
@@ -140,12 +148,13 @@ public class Xfermodes extends GraphicsActivity {
                 canvas.drawRect(x, y, x + W, y + H, paint);
 
                 // draw the src/dst example into our offscreen bitmap
+                @SuppressLint("WrongConstant")
                 int sc = canvas.saveLayer(x, y, x + W, y + H, null,
-                                          Canvas.MATRIX_SAVE_FLAG |
-                                          Canvas.CLIP_SAVE_FLAG |
-                                          Canvas.HAS_ALPHA_LAYER_SAVE_FLAG |
-                                          Canvas.FULL_COLOR_LAYER_SAVE_FLAG |
-                                          Canvas.CLIP_TO_LAYER_SAVE_FLAG);
+                        Canvas.MATRIX_SAVE_FLAG |
+                                Canvas.CLIP_SAVE_FLAG |
+                                Canvas.HAS_ALPHA_LAYER_SAVE_FLAG |
+                                Canvas.FULL_COLOR_LAYER_SAVE_FLAG |
+                                Canvas.CLIP_TO_LAYER_SAVE_FLAG);
                 canvas.translate(x, y);
                 canvas.drawBitmap(mDstB, 0, 0, paint);
                 paint.setXfermode(sModes[i]);
@@ -154,8 +163,7 @@ public class Xfermodes extends GraphicsActivity {
                 canvas.restoreToCount(sc);
 
                 // draw the label
-                canvas.drawText(sLabels[i],
-                                x + W/2, y - labelP.getTextSize()/2, labelP);
+                canvas.drawText(sLabels[i], x + W / 2, y - labelP.getTextSize() / 2, labelP);
 
                 x += W + 10;
 
