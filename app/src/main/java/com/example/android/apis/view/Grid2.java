27f13e372a1dd9e4d1a776c1f7c157ce8291eb3f
markgray <mark.t.gray@gmail.com>
2017-12-26 04:01:02 -0500

    Added comments, scaled for density.

--- app/src/main/java/com/example/android/apis/view/Grid2.java
+++ app/src/main/java/com/example/android/apis/view/Grid2.java
@@ -28,47 +28,123 @@ import android.widget.GridView;
 import android.widget.ImageView;
 
 /**
- * A grid that displays a set of framed photos.
- *
+ * A grid that displays a set of framed photos created from resource jpg's using
+ * ImageView.setImageResource
  */
 public class Grid2 extends Activity {
-
+    /**
+     * Called when the activity is starting. First we call through to our super's implementation of
+     * {@code onCreate}, then we set our content view to our layout file R.layout.grid_2. Next we
+     * initialize our variable {@code GridView g} by finding the view with ID R.id.myGrid, and set
+     * its adapter to a new instance of {@code ImageAdapter}.
+     *
+     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.grid_2);
 
         GridView g = (GridView) findViewById(R.id.myGrid);
         g.setAdapter(new ImageAdapter(this));
     }
 
+    /**
+     * An adapter which returns {@code ImageView} objects loaded from an array of resource IDs.
+     */
+    @SuppressWarnings("WeakerAccess")
     public class ImageAdapter extends BaseAdapter {
+        /**
+         * {@code Context} we were constructed with ("this" in the {@code onCreate} method of the
+         * {@code Grid2} activity), used to access resources.
+         */
+        private Context mContext;
+        /**
+         * Logical density of the display
+         */
+        private float dp2px;
+        /**
+         * Width of a photo in pixels (45*dp2px)
+         */
+        private int w;
+        /**
+         * Height of a photo in pixels (45*dp2px)
+         */
+        private int h;
+
+        /**
+         * Our constructor. First we save our parameter {@code Context c} in our field
+         * {@code Context mContext}, then we initialize our field {@code dp2px} with the logical
+         * density of our display, initialize {@code w} to 45dp and {@code h} to 45dp.
+         *
+         * @param c {@code Context} to use to access resources
+         */
         public ImageAdapter(Context c) {
             mContext = c;
+            dp2px = c.getResources().getDisplayMetrics().density;
+            w = (int) (45 * dp2px);
+            h = (int) (45 * dp2px);
         }
 
+        /**
+         * How many items are in the data set represented by this Adapter, in our case this is the
+         * length of our {@code mThumbIds} array.
+         *
+         * @return Count of items.
+         */
         @Override
         public int getCount() {
             return mThumbIds.length;
         }
 
+        /**
+         * Get the data item associated with the specified position in the data set. We simply return
+         * the contents of {@code mThumbIds[position]} to the caller (this method does not seem to
+         * ever be called, it used to return the parameter {@code position} for some reason).
+         *
+         * @param position Position of the item whose data we want within the adapter's data set.
+         * @return The data at the specified position.
+         */
         @Override
         public Object getItem(int position) {
-            return position;
+            return mThumbIds[position];
         }
 
+        /**
+         * Get the row id associated with the specified position in the list. Our row id is the same
+         * as our parameter {@code position} so we just return that to the caller.
+         *
+         * @param position The position of the item within the adapter's data set whose row id we want.
+         * @return The id of the item at the specified position.
+         */
         @Override
         public long getItemId(int position) {
             return position;
         }
 
+        /**
+         * Get a View that displays the data at the specified position in the data set. First we
+         * declare {@code ImageView imageView}, then if our parameter {@code convertView} is null we
+         * create a new instance of {@code ImageView} for {@code imageView}, set its layout parameters
+         * to {@code w} pixels wide (45 scaled by the screen density) by {@code h} pixels high (also
+         * 45 scaled by the screen density), set it to not adjust its view bounds, set its scale type
+         * to CENTER_CROP, and set its padding to 8 pixels on each side. If {@code convertView} is
+         * not null we just set {@code imageView} to it after casting it to an {@code ImageView}.
+         * Then we use the {@code setImageResource} of {@code imageView} to have it load and decode
+         * the jpg for the resource resource ID given by {@code mThumbIds[position]}, setting it as
+         * its content. Finally we return {@code imageView} to the caller.
+         *
+         * @param position    The position of the item within the adapter's data set whose view we want.
+         * @param convertView The old view to reuse, if possible.
+         * @param parent      The parent that this view will eventually be attached to
+         * @return A View corresponding to the data at the specified position.
+         */
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             ImageView imageView;
             if (convertView == null) {
                 imageView = new ImageView(mContext);
-                imageView.setLayoutParams(new GridView.LayoutParams(45, 45));
+                imageView.setLayoutParams(new GridView.LayoutParams(w, h));
                 imageView.setAdjustViewBounds(false);
                 imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
                 imageView.setPadding(8, 8, 8, 8);
@@ -81,8 +157,9 @@ public class Grid2 extends Activity {
             return imageView;
         }
 
-        private Context mContext;
-
+        /**
+         * The array of resource IDs our {@code ImageAdapter} used as its data.
+         */
         private Integer[] mThumbIds = {
                 R.drawable.sample_thumb_0, R.drawable.sample_thumb_1,
                 R.drawable.sample_thumb_2, R.drawable.sample_thumb_3,
@@ -166,5 +243,4 @@ public class Grid2 extends Activity {
                 R.drawable.sample_thumb_6, R.drawable.sample_thumb_7,
         };
     }
-
 }
--- app/src/main/java/com/example/android/apis/view/Grid3.java
+++ app/src/main/java/com/example/android/apis/view/Grid3.java
@@ -39,27 +39,54 @@ import java.util.List;
 
 /**
  * This demo illustrates the use of CHOICE_MODE_MULTIPLE_MODAL, a.k.a. selection mode on GridView.
+ * Implements multi-selection mode on GridView - hard to select by touch though
  */
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class Grid3 extends Activity {
-
+    /**
+     * Our layout's {@code GridView}, with ID R.id.myGrid.
+     */
     GridView mGrid;
 
+    /**
+     * Called when the activity is starting. First we call through to our super's implementation of
+     * {@code onCreate}, then we set our content view to our layout file R.layout.grid_1. Next we
+     * call our method {@code loadApps} to load our field {@code List<ResolveInfo> mApps} with
+     * {@code ResolveInfo} objects for accessing information about all of the apps that the
+     * {@code PackageManager} knows about. We initialize {@code GridView mGrid} by finding the view
+     * with ID R.id.myGrid, set its adapter to a new instance of {@code AppsAdapter}, set its choice
+     * mode to CHOICE_MODE_MULTIPLE_MODAL, and set its {@code MultiChoiceModeListener} to a new
+     * instance of our class {@code MultiChoiceModeListener}.
+     *
+     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.grid_1);
 
         loadApps();
 
-        setContentView(R.layout.grid_1);
         mGrid = (GridView) findViewById(R.id.myGrid);
         mGrid.setAdapter(new AppsAdapter());
         mGrid.setChoiceMode(GridView.CHOICE_MODE_MULTIPLE_MODAL);
         mGrid.setMultiChoiceModeListener(new MultiChoiceModeListener());
     }
 
+    /**
+     * List of {@code ResolveInfo} objects for all activities that can be executed for an intent
+     * with the action MAIN, and category LAUNCHER loaded from the {@code PackageManager} by our
+     * method {@code loadApps}.
+     */
     private List<ResolveInfo> mApps;
 
+    /**
+     * Loads {@code List<ResolveInfo> mApps} with a list of all activities that can be performed for
+     * an intent with the action MAIN, and category LAUNCHER loaded using the {@code PackageManager}.
+     * First we create {@code Intent mainIntent} with the action ACTION_MAIN, and add the category
+     * CATEGORY_LAUNCHER. Then we retrieve a {@code PackageManager} instance and use it to retrieve
+     * all activities that can be performed for intent {@code mainIntent} to initialize {@code mApps}.
+     */
     private void loadApps() {
         Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
         mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
@@ -67,10 +94,56 @@ public class Grid3 extends Activity {
         mApps = getPackageManager().queryIntentActivities(mainIntent, 0);
     }
 
+    /**
+     * Our adapter which fetches app icons from the {@code ResolveInfo} for each of the applications
+     * in our {@code List<ResolveInfo> mApps}.
+     */
+    @SuppressWarnings("WeakerAccess")
     public class AppsAdapter extends BaseAdapter {
+        /**
+         * Logical density of the display
+         */
+        private float dp2px;
+        /**
+         * Width of an icon in pixels (45*dp2px)
+         */
+        private int w;
+        /**
+         * Height of an icon in pixels (45*dp2px)
+         */
+        private int h;
+
+        /**
+         * Our constructor. First we initialize our field {@code dp2px} with the logical density of
+         * our display, then we use it to scale 50dp to pixels to initialize both {@code w} and
+         * {@code h}.
+         */
         public AppsAdapter() {
+            dp2px = getResources().getDisplayMetrics().density;
+            w = (int) (50 * dp2px);
+            h = (int) (50 * dp2px);
         }
 
+        /**
+         * Get a View that displays the data at the specified position in the data set. First we
+         * declare {@code CheckableLayout l} and {@code ImageView i}. Then if our parameter
+         * {@code convertView} is null we create a new instance of {@code ImageView} for {@code i},
+         * set its scale type to FIT_CENTER and set its layout parameters to {@code w} pixels wide
+         * by {@code h} pixels high (these are both 50dp scaled by the logical density of our
+         * display). We create a new instance for {@code CheckableLayout l}, and set its layout
+         * parameters to WRAP_CONTENT for both width and height. We then add the view {@code i} to
+         * {@code l}. If {@code convertView} is not null we set {@code l} to it after casting it to
+         * an {@code CheckableLayout}, and set {@code i} to the child of {@code l} at position 0.
+         * We initialize {@code ResolveInfo info} with the data in {@code mApps} at position
+         * {@code position} and set {@code i} to a drawable of the icon associated with {@code info}
+         * that we retrieve by using that {@code ResolveInfo} to call back a {@code PackageManager}
+         * instance to load the icon from the application. Finally we return {@code l} to the caller.
+         *
+         * @param position    The position of the item within the adapter's data set whose view we want.
+         * @param convertView The old view to reuse, if possible.
+         * @param parent      The parent that this view will eventually be attached to
+         * @return A View corresponding to the data at the specified position.
+         */
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             CheckableLayout l;
@@ -79,7 +152,7 @@ public class Grid3 extends Activity {
             if (convertView == null) {
                 i = new ImageView(Grid3.this);
                 i.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                i.setLayoutParams(new ViewGroup.LayoutParams(50, 50));
+                i.setLayoutParams(new ViewGroup.LayoutParams(w, h));
                 l = new CheckableLayout(Grid3.this);
                 l.setLayoutParams(new GridView.LayoutParams(GridView.LayoutParams.WRAP_CONTENT,
                         GridView.LayoutParams.WRAP_CONTENT));
@@ -164,15 +237,15 @@ public class Grid3 extends Activity {
 
         @Override
         public void onItemCheckedStateChanged(ActionMode mode, int position, long id,
-                boolean checked) {
+                                              boolean checked) {
             int selectCount = mGrid.getCheckedItemCount();
             switch (selectCount) {
-            case 1:
-                mode.setSubtitle("One item selected");
-                break;
-            default:
-                mode.setSubtitle("" + selectCount + " items selected");
-                break;
+                case 1:
+                    mode.setSubtitle("One item selected");
+                    break;
+                default:
+                    mode.setSubtitle("" + selectCount + " items selected");
+                    break;
             }
         }
 
