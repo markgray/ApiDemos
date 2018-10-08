12e1c7800a95180687e2565b52a765edeff6c898
markgray <mark.t.gray@gmail.com>
2018-10-08 08:40:44 -0400

    Updated VideoViewDemo.java

--- app/src/main/java/com/example/android/apis/media/VideoViewDemo.java
+++ app/src/main/java/com/example/android/apis/media/VideoViewDemo.java
@@ -16,9 +16,15 @@
 
 package com.example.android.apis.media;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.ClipData;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -27,22 +33,23 @@ import com.example.android.apis.R;
 /**
  * Shows how to use the VideoView class in an application to play a video, as well as a
  * MediaController to provide control buttons for the playback.
- * TODO: Set the path variable to a streaming video URL or a local media file path.
  */
+@TargetApi(Build.VERSION_CODES.N)
 public class VideoViewDemo extends Activity {
+
     /**
      * {@code VideoView} in our layout that we use to display our video (ID R.id.surface_view)
      */
-    @SuppressWarnings("FieldCanBeLocal")
     private VideoView mVideoView;
 
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
      * {@code onCreate}, then we set our content view to our layout file R.layout.videoview. We then
      * initialize our field {@code VideoView mVideoView} by locating the {@code VideoView} in our
-     * layout with ID R.id.surface_view, set its video url to address the file videoviewdemo.mp4 in
-     * our raw resources, set its media controller to a new instance of {@code MediaController}, and
-     * finally request focus for it.
+     * layout with ID R.id.surface_view. We then call our method {@code initPlayer} to set its video
+     * url to address the file videoviewdemo.mp4 in our raw resources, set its media controller to a
+     * new instance of {@code MediaController}, and request focus for it. Finally we call the
+     * {@code setOnDragListener} of {@code mVideoView} to our field {@code OnDragListener mDragListener}.
      *
      * @param icicle we do not implement {@code onSaveInstanceState} so do not use
      */
@@ -50,12 +57,56 @@ public class VideoViewDemo extends Activity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.videoview);
-        mVideoView = (VideoView) findViewById(R.id.surface_view);
-        /*
-         * Alternatively, you can use mVideoView.setVideoPath(<path>);
-         */
-        mVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.videoviewdemo));
+        mVideoView = findViewById(R.id.surface_view);
+
+        initPlayer(Uri.parse("android.resource://" + getPackageName() +
+                "/" + R.raw.videoviewdemo));
+
+        mVideoView.setOnDragListener(mDragListener);
+    }
+
+    /**
+     * Initializes our {@code VideoView mVideoView} player. We set the video {@code Uri} that
+     * {@code VideoView mVideoView} should use to our parameter {@code Uri uri}, set its media
+     * controller to a new instance of {@code MediaController}, and request focus for it.
+     *
+     * @param uri Video {@code Uri} that our {@code VideoView mVideoView} should use.
+     */
+    private void initPlayer(Uri uri) {
+        mVideoView.setVideoURI(uri);
         mVideoView.setMediaController(new MediaController(this));
         mVideoView.requestFocus();
     }
+
+    /**
+     * {@code OnDragListener} for our {@code VideoView mVideoView} (Resource id R.id.surface_view in
+     * our layout file).
+     */
+    private View.OnDragListener mDragListener = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            if (event.getAction() != DragEvent.ACTION_DROP) {
+                return true;
+            }
+            ClipData clipData = event.getClipData();
+            if (clipData.getItemCount() != 1) {
+                return false;
+            }
+            ClipData.Item item = clipData.getItemAt(0);
+            Uri uri = item.getUri();
+            if (uri == null) {
+                return false;
+            }
+            if (requestDragAndDropPermissions(event) == null) {
+                return false;
+            }
+            initPlayer(uri);
+            mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    mVideoView.start();
+                }
+            });
+            return true;
+        }
+    };
 }
