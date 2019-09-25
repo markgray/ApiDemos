e132c2378650ba9ab82bb1d23c17d96239cc4842
markgray <mark.t.gray@gmail.com>
2019-09-25 08:13:30 -0400

    Converted Intents.java to kotlin.

--- app/src/main/java/com/example/android/apis/app/Intents.kt
+++ app/src/main/java/com/example/android/apis/app/Intents.kt
@@ -14,62 +14,63 @@
  * limitations under the License.
  */
 
-package com.example.android.apis.app;
+package com.example.android.apis.app
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
 
-import com.example.android.apis.R;
+import com.example.android.apis.R
 
 /**
  * Uses Intent.createChooser(Intent, "title") to create a "chooser intent"  from three different
- * mime types ("audio/*", "image/*", and "*&#47;*") of Intent.ACTION_GET_CONTENT Intents based on which
+ * mime types ("audio/(any), "image/(any), and "(any)/(any)") of Intent.ACTION_GET_CONTENT Intents based on which
  * button the user clicks, and it then hands the created "chooser Intent" to startActivity(Intent)
+ * Note: occurrences of slash star in comments are replace by /(any) to avoid kotlin compiler bug.
  */
-public class Intents extends Activity {
+@Suppress("UNUSED_PARAMETER")
+class Intents : Activity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
      * onCreate, then we set our content view to our layout file R.layout.intents.
      *
      * @param savedInstanceState always null since onSaveInstanceState is not overridden
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.intents);
+        setContentView(R.layout.intents)
     }
 
     /**
      * Set as the OnClickListener for the R.id.get_music ("GET MUSIC") Button in the layout using
      * the attribute android:onClick="onGetMusic". First we create an Intent intent with the
-     * action ACTION_GET_CONTENT, set the type of the Intent to "audio/*" and finally create an
+     * action ACTION_GET_CONTENT, set the type of the Intent to "audio/(any) and finally create an
      * action chooser Intent with the title "Select music" from Intent intent, and start that chooser
      * Activity using the chooser Intent.
      *
      * @param view R.id.get_music ("GET MUSIC") Button which was clicked
      */
-    public void onGetMusic(View view) {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("audio/*");
-        startActivity(Intent.createChooser(intent, "Select music"));
+    fun onGetMusic(view: View) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "audio/*"
+        startActivity(Intent.createChooser(intent, "Select music"))
     }
 
     /**
      * Set as the OnClickListener for the R.id.get_image ("GET IMAGE") Button in the layout using
      * the attribute android:onClick="onGetImage". First we create an Intent intent with the
-     * action ACTION_GET_CONTENT, set the type of the Intent to "image/*" and finally create an
+     * action ACTION_GET_CONTENT, set the type of the Intent to "image/(any) and finally create an
      * action chooser Intent with the title "Select image" from Intent intent, and start that chooser
      * Activity using the chooser Intent.
      *
      * @param view R.id.get_image ("GET IMAGE") Button which was clicked
      */
-    public void onGetImage(View view) {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivity(Intent.createChooser(intent, "Select image"));
+    fun onGetImage(view: View) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivity(Intent.createChooser(intent, "Select image"))
     }
 
     /**
@@ -81,9 +82,9 @@ public class Intents extends Activity {
      *
      * @param view R.id.get_stream ("GET STREAM") Button which was clicked
      */
-    public void onGetStream(View view) {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("*/*");
-        startActivity(Intent.createChooser(intent, "Select stream"));
+    fun onGetStream(view: View) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        startActivity(Intent.createChooser(intent, "Select stream"))
     }
 }
