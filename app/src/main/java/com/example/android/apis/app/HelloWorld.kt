21cdbf4f49d282a431be78332a8323b33263475e
markgray <mark.t.gray@gmail.com>
2019-09-25 07:53:54 -0400

    Converted HelloWorld.java to kotlin.

--- app/src/main/java/com/example/android/apis/app/HelloWorld.kt
+++ app/src/main/java/com/example/android/apis/app/HelloWorld.kt
@@ -14,41 +14,32 @@
  * limitations under the License.
  */
 
-package com.example.android.apis.app;
+package com.example.android.apis.app
 
 // Need the following import to get access to the app resources, since this
 // class is in a sub-package.
-import com.example.android.apis.R;
+import com.example.android.apis.R
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.app.Activity
+import android.os.Bundle
 
 
 /**
  * Simple example of writing an application Activity.
- * Hello World</a></h3>
-
-<p>This demonstrates the basic code needed to write a Screen activity.</p>
-
-<h4>Demo</h4>
-App/Activity/Hello World
- 
-<h4>Source files</h4>
- * <table class="LinkTable">
- *         <tr>
- *             <td >src/com.example.android.apis/app/HelloWorld.java</td>
- *             <td >The Hello World Screen implementation</td>
- *         </tr>
- *         <tr>
- *             <td >/res/any/layout/hello_world.xml</td>
- *             <td >Defines contents of the screen</td>
- *         </tr>
- * </table> 
+ * Hello World
+ *
+ * This demonstrates the basic code needed to write a Screen activity.
+ *
+ * Demo App/Activity/Hello World
+ *
+ * Source files:
+ *  - src/com.example.android.apis/app/HelloWorld.java The Hello World Screen implementation
+ *  - res/any/layout/hello_world.xml</td> Defines contents of the screen
  */
-public class HelloWorld extends Activity {
+class HelloWorld : Activity() {
     /**
      * Initialization of the Activity after it is first created.  Must at least
-     * call {@link android.app.Activity#setContentView setContentView()} to
+     * call [setContentView()][android.app.Activity.setContentView] to
      * describe what is to be displayed in the screen.
      *
      * First we call through to our super's implementation of onCreate, then we set out content
@@ -56,14 +47,13 @@ public class HelloWorld extends Activity {
      *
      * @param savedInstanceState always null since onSaveInstanceState is not overridden.
      */
-    @Override
-	protected void onCreate(Bundle savedInstanceState) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         // Be sure to call the super class.
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState)
 
         // See assets/res/any/layout/hello_world.xml for this
         // view layout definition, which is being set here as
         // the content of our screen.
-        setContentView(R.layout.hello_world);
+        setContentView(R.layout.hello_world)
     }
 }
