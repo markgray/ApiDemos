007d5e071cbbd5a93bd9afbbcfa4c025e8f6fa2b
markgray <mark.t.gray@gmail.com>
2019-09-26 08:20:19 -0400

    Converted PersistentState.java to kotlin.

--- app/src/main/java/com/example/android/apis/app/PersistentState.kt
+++ app/src/main/java/com/example/android/apis/app/PersistentState.kt
@@ -14,64 +14,64 @@
  * limitations under the License.
  */
 
-package com.example.android.apis.app;
+package com.example.android.apis.app
 
 // Need the following import to get access to the app resources, since this
 // class is in a sub-package.
 
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import com.example.android.apis.R
 
 /**
  * Simple example of using persistent preferences to retain a screen's state.
- * <p>This can be used as an alternative to the normal
- * <b>onSaveInstanceState()</b> mechanism, if you
- * wish the state to persist even after an activity is finished.</p>
  *
- * <p>Note that using this approach requires more care, since you are sharing
+ * This can be used as an alternative to the normal
+ * **onSaveInstanceState()** mechanism, if you
+ * wish the state to persist even after an activity is finished.
+ *
+ * Note that using this approach requires more care, since you are sharing
  * the persistent state potentially across multiple instances of the activity.
  * In particular, if you allow a new instance of the activity to be launched
  * directly on top of the existing instance, the state can get out of sync
- * because the new instance is resumed before the old one is paused.</p>
+ * because the new instance is resumed before the old one is paused.
  *
- * <p>For any persistent state that is not simplistic, a content
- * provider is often a better choice.</p>
+ * For any persistent state that is not simplistic, a content
+ * provider is often a better choice.
  *
- * <p>In this example we are currently saving and restoring the state of the
+ * In this example we are currently saving and restoring the state of the
  * top text editor, but not of the bottom text editor.  You can see the difference
  * by editing the two text fields, then going back from the activity and
- * starting it again.</p>
+ * starting it again.
  *
  * <h4>Demo</h4>
  * App/Activity/Save &amp; Restore State
  *
  * <h4>Source files</h4>
  * <table class="LinkTable">
- *         <tr>
- *             <td class="LinkColumn">src/com.example.android.apis/app/PersistentState.java</td>
- *             <td class="DescrColumn">The Save/Restore Screen implementation</td>
- *         </tr>
- *         <tr>
- *             <td class="LinkColumn">/res/any/layout/save_restore_state.xml</td>
- *             <td class="DescrColumn">Defines contents of the screen</td>
- *         </tr>
- * </table>
+ * <tr>
+ * <td class="LinkColumn">src/com.example.android.apis/app/PersistentState.java</td>
+ * <td class="DescrColumn">The Save/Restore Screen implementation</td>
+</tr> *
+ * <tr>
+ * <td class="LinkColumn">/res/any/layout/save_restore_state.xml</td>
+ * <td class="DescrColumn">Defines contents of the screen</td>
+</tr> *
+</table> *
  *
  */
-public class PersistentState extends Activity {
+class PersistentState : Activity() {
     /**
-     * {@code EditText} whose text we save in our {@code SharedPreferences} under the key "text"
+     * `EditText` whose text we save in our `SharedPreferences` under the key "text"
      */
-    private EditText mSaved;
+    private var mSaved: EditText? = null
 
     /**
      * Initialization of the Activity after it is first created.  Here we use
-     * {@link android.app.Activity#setContentView setContentView()} to set up
+     * [setContentView()][android.app.Activity.setContentView] to set up
      * the Activity's content to our layout file R.layout.save_restore_state,
      * set the TextView R.id.msg to the string R.string.persistent_msg, and
      * retrieve the EditText widget (R.id.saved) whose state we will persistent
@@ -79,21 +79,20 @@ public class PersistentState extends Activity {
      *
      * @param savedInstanceState always null since onSaveInstanceState is not overridden
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         // Be sure to call the super class.
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState)
 
         // See assets/res/any/layout/save_restore_state.xml for this
         // view layout definition, which is being set here as
         // the content of our screen.
-        setContentView(R.layout.save_restore_state);
+        setContentView(R.layout.save_restore_state)
 
         // Set message to be appropriate for this screen.
-        ((TextView)findViewById(R.id.msg)).setText(R.string.persistent_msg);
+        (findViewById<View>(R.id.msg) as TextView).setText(R.string.persistent_msg)
 
         // Retrieve the EditText widget whose state we will save.
-        mSaved = findViewById(R.id.saved);
+        mSaved = findViewById(R.id.saved)
     }
 
     /**
@@ -110,19 +109,18 @@ public class PersistentState extends Activity {
      * the key "selection-end" to int selectionEnd (defaulting to -1 for both). If both of these
      * are not -1, then we set the selection of "mSaved" to them.
      */
-    @Override
-    protected void onResume() {
-        super.onResume();
+    override fun onResume() {
+        super.onResume()
 
-        SharedPreferences prefs = getPreferences(0); 
-        String restoredText = prefs.getString("text", null);
+        val prefs = getPreferences(0)
+        val restoredText = prefs.getString("text", null)
         if (restoredText != null) {
-            mSaved.setText(restoredText, TextView.BufferType.EDITABLE);
+            mSaved!!.setText(restoredText, TextView.BufferType.EDITABLE)
 
-            int selectionStart = prefs.getInt("selection-start", -1);
-            int selectionEnd = prefs.getInt("selection-end", -1);
+            val selectionStart = prefs.getInt("selection-start", -1)
+            val selectionEnd = prefs.getInt("selection-end", -1)
             if (selectionStart != -1 && selectionEnd != -1) {
-                mSaved.setSelection(selectionStart, selectionEnd);
+                mSaved!!.setSelection(selectionStart, selectionEnd)
             }
         }
     }
@@ -140,14 +138,13 @@ public class PersistentState extends Activity {
      * from "editor" to the SharedPreferences object it is editing. This atomically performs
      * the requested modifications, replacing whatever is currently in the SharedPreferences.
      */
-    @Override
-    protected void onPause() {
-        super.onPause();
+    override fun onPause() {
+        super.onPause()
 
-        SharedPreferences.Editor editor = getPreferences(0).edit();
-        editor.putString("text", mSaved.getText().toString());
-        editor.putInt("selection-start", mSaved.getSelectionStart());
-        editor.putInt("selection-end", mSaved.getSelectionEnd());
-        editor.apply();
+        val editor = getPreferences(0).edit()
+        editor.putString("text", mSaved!!.text.toString())
+        editor.putInt("selection-start", mSaved!!.selectionStart)
+        editor.putInt("selection-end", mSaved!!.selectionEnd)
+        editor.apply()
     }
 }
