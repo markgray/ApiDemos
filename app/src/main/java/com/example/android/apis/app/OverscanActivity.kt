edf7d59041bae2b56aded1b9cffe539d0aa5ccc5
markgray <mark.t.gray@gmail.com>
2019-09-26 08:14:14 -0400

    Converted OverscanActivity.java to kotlin.

--- app/src/main/java/com/example/android/apis/app/OverscanActivity.kt
+++ app/src/main/java/com/example/android/apis/app/OverscanActivity.kt
@@ -14,37 +14,37 @@
  * limitations under the License.
  */
 
-package com.example.android.apis.app;
+package com.example.android.apis.app
 
 // Need the following import to get access to the app resources, since this
 // class is in a sub-package.
-import android.app.Activity;
-import android.os.Bundle;
+import android.app.Activity
+import android.os.Bundle
 
-import com.example.android.apis.R;
+import com.example.android.apis.R
 
 /**
  * <h3>Overscan Activity</h3>
- * 
- * <p>This demonstrates the how to write an activity that extends into the
- * overscan region.</p>
+ *
+ *
+ * This demonstrates the how to write an activity that extends into the
+ * overscan region.
  *
  * Uses @android:style/Theme.Holo.NoActionBar.Overscan set in AndroidManifest, and
  * android:fitsSystemWindows="true" in its layout/overscan_activity.xml to place a
  * surrounding "white box" in the overscan region of the display, and displays a
  * picture in a FrameLayout offset to avoid the overscan region.
  */
-public class OverscanActivity extends Activity {
+class OverscanActivity : Activity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation
      * of onCreate, and then we set our content view to our layout file R.layout.overscan_activity.
      * The entire demo is created by the xml.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override `onSaveInstanceState` so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.overscan_activity);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.overscan_activity)
     }
 }
