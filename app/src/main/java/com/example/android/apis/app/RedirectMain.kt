fc23a5957100ada6f9ba0a80df54b5b9a8f8a751
markgray <mark.t.gray@gmail.com>
2019-09-28 08:33:05 -0400

    Converted RedirectMain.java to kotlin.

--- app/src/main/java/com/example/android/apis/app/RedirectMain.kt
+++ app/src/main/java/com/example/android/apis/app/RedirectMain.kt
@@ -14,18 +14,15 @@
  * limitations under the License.
  */
 
-package com.example.android.apis.app;
+package com.example.android.apis.app
 
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.TextView
+import com.example.android.apis.R
 
 /**
  * Launched by a push of a Button in the Activity RedirectEnter we will immediately start
@@ -36,12 +33,38 @@ import android.widget.TextView;
  * RedirectGetter to determine what to do if the result code was RESULT_CANCELED,
  * either finish() back to RedirectEnter, or just display the old text.
  */
-public class RedirectMain extends Activity {
-    static final int INIT_TEXT_REQUEST = 0; // Request code for initial run of Activity RedirectGetter
-    static final int NEW_TEXT_REQUEST = 1;  // Request code when new text Button is clicked
+class RedirectMain : Activity() {
 
-    @SuppressWarnings("FieldCanBeLocal")
-    private String mTextPref; // String stored in shared preferences file by RedirectGetter
+    private var mTextPref: String? = null // String stored in shared preferences file by RedirectGetter
+
+    /**
+     * Called when the "Clear and exit" Button is clicked. First we open the shared preference
+     * file "RedirectData" into SharedPreferences preferences, then we create an editor for
+     * **preferences**, use it to mark the key "text" for removal, and  commit the
+     * change back from the editor to the preferences file. Finally we finish() this Activity
+     * and return to RedirectEnter.
+     *
+     * Parameter: View of the Button that was clicked
+     */
+    private val mClearListener = OnClickListener {
+        // Erase the preferences and exit!
+        val preferences = getSharedPreferences("RedirectData", 0)
+        preferences.edit().remove("text").apply()
+        finish()
+    }
+
+    /**
+     * OnClickListener for the Button "New text" (R.id.newView). We create an Intent to start the
+     * Activity RedirectGetter, then startActivityForResult that Activity using the requestCode
+     * NEW_TEXT_REQUEST (1)
+     *
+     * Parameter: View of the Button that was clicked
+     */
+    private val mNewListener = OnClickListener {
+        // Retrieve new text preferences.
+        val intent = Intent(this@RedirectMain, RedirectGetter::class.java)
+        startActivityForResult(intent, NEW_TEXT_REQUEST)
+    }
 
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
@@ -56,17 +79,16 @@ public class RedirectMain extends Activity {
      *
      * @param savedInstanceState always null since onSaveInstanceState is not called
      */
-    @Override
-	protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.redirect_main);
+        setContentView(R.layout.redirect_main)
 
         // Watch for button clicks.
-        Button clearButton = (Button)findViewById(R.id.clear);
-        clearButton.setOnClickListener(mClearListener);
-        Button newButton = (Button)findViewById(R.id.newView);
-        newButton.setOnClickListener(mNewListener);
+        val clearButton = findViewById<Button>(R.id.clear)
+        clearButton.setOnClickListener(mClearListener)
+        val newButton = findViewById<Button>(R.id.newView)
+        newButton.setOnClickListener(mNewListener)
 
         // Retrieve the current text preference.  If there is no text
         // preference set, we need to get it from the user by invoking the
@@ -74,8 +96,8 @@ public class RedirectMain extends Activity {
         // temporarily hide our own activity so it is not displayed until the
         // result is returned.
         if (!loadPrefs()) {
-            Intent intent = new Intent(this, RedirectGetter.class);
-            startActivityForResult(intent, INIT_TEXT_REQUEST);
+            val intent = Intent(this, RedirectGetter::class.java)
+            startActivityForResult(intent, INIT_TEXT_REQUEST)
         }
     }
 
@@ -83,10 +105,11 @@ public class RedirectMain extends Activity {
      * Called when an activity you launched exits, giving you the requestCode
      * you started it with, the resultCode it returned, and any additional
      * data from it.  The <var>resultCode</var> will be
-     * {@link #RESULT_CANCELED} if the activity explicitly returned that,
+     * [.RESULT_CANCELED] if the activity explicitly returned that,
      * didn't return any result, or crashed during its operation.
      *
-     * <p>You will receive this call immediately before onResume() when your
+     *
+     * You will receive this call immediately before onResume() when your
      * activity is re-starting.
      *
      * First we check to see if this result is from an INIT_TEXT_REQUEST requestCode. If is we
@@ -98,26 +121,25 @@ public class RedirectMain extends Activity {
      * entry from our shared preference file and update the text in our UI.
      *
      * @param requestCode The integer request code originally supplied to
-     *                    startActivityForResult(), allowing you to identify who this
-     *                    result came from and what you requested of it.
+     * startActivityForResult(), allowing you to identify who this
+     * result came from and what you requested of it.
      * @param resultCode The integer result code returned by the child activity
-     *                   through its setResult().
+     * through its setResult().
      * @param data An Intent, which can return result data to the caller
-     *               (various data can be attached to Intent "extras"). (Unused)
+     * (various data can be attached to Intent "extras"). (Unused)
      */
-    @Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         if (requestCode == INIT_TEXT_REQUEST) {
 
             // If the request was cancelled, then we are cancelled as well.
             if (resultCode == RESULT_CANCELED) {
-                finish();
+                finish()
 
-            // Otherwise, there now should be text...  reload the prefs,
-            // and show our UI.  (Optionally we could verify that the text
-            // is now set and exit if it isn't.)
+                // Otherwise, there now should be text...  reload the prefs,
+                // and show our UI.  (Optionally we could verify that the text
+                // is now set and exit if it isn't.)
             } else {
-                loadPrefs();
+                loadPrefs()
             }
 
         } else if (requestCode == NEW_TEXT_REQUEST) {
@@ -125,7 +147,7 @@ public class RedirectMain extends Activity {
             // In this case we are just changing the text, so if it was
             // cancelled then we can leave things as-is.
             if (resultCode != RESULT_CANCELED) {
-                loadPrefs();
+                loadPrefs()
             }
 
         }
@@ -139,61 +161,25 @@ public class RedirectMain extends Activity {
      *
      * @return true if there was text stored under "text" in the shared preference file, false otherwise
      */
-    private boolean loadPrefs() {
+    private fun loadPrefs(): Boolean {
         // Retrieve the current redirect values.
         // NOTE: because this preference is shared between multiple
         // activities, you must be careful about when you read or write
         // it in order to keep from stepping on yourself.
-        SharedPreferences preferences = getSharedPreferences("RedirectData", 0);
+        val preferences = getSharedPreferences("RedirectData", 0)
 
-        mTextPref = preferences.getString("text", null);
+        mTextPref = preferences.getString("text", null)
         if (mTextPref != null) {
-            TextView text = (TextView)findViewById(R.id.text);
-            text.setText(mTextPref);
-            return true;
+            val text = findViewById<TextView>(R.id.text)
+            text.text = mTextPref
+            return true
         }
 
-        return false;
+        return false
     }
 
-    /**
-     * OnClickListener for the Button "Clear and exit" (R.id.clear)
-     */
-    private OnClickListener mClearListener = new OnClickListener() {
-        /**
-         * Called when the "Clear and exit" Button is clicked. First we open the shared preference
-         * file "RedirectData" into SharedPreferences preferences, then we create an editor for
-         * <b>preferences</b>, use it to mark the key "text" for removal, and  commit the
-         * change back from the editor to the preferences file. Finally we finish() this Activity
-         * and return to RedirectEnter.
-         *
-         * @param v View of the Button that was clicked
-         */
-        @Override
-        public void onClick(View v) {
-            // Erase the preferences and exit!
-            SharedPreferences preferences = getSharedPreferences("RedirectData", 0);
-            preferences.edit().remove("text").commit();
-            finish();
-        }
-    };
-
-    /**
-     * OnClickListener for the Button "New text" (R.id.newView)
-     */
-    private OnClickListener mNewListener = new OnClickListener() {
-        /**
-         * Called when the "New Text" Button is clicked. We create an Intent to start the Activity
-         * RedirectGetter, then startActivityForResult that Activity using the requestCode
-         * NEW_TEXT_REQUEST (1)
-         *
-         * @param v View of the Button that was clicked
-         */
-        @Override
-        public void onClick(View v) {
-            // Retrieve new text preferences.
-            Intent intent = new Intent(RedirectMain.this, RedirectGetter.class);
-            startActivityForResult(intent, NEW_TEXT_REQUEST);
-        }
-    };
+    companion object {
+        internal const val INIT_TEXT_REQUEST = 0 // Request code for initial run of Activity RedirectGetter
+        internal const val NEW_TEXT_REQUEST = 1  // Request code when new text Button is clicked
+    }
 }
