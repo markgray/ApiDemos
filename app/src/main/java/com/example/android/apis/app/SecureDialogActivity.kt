90080c728e36c99aedb59954d8c3777fa7e5fa5d
markgray <mark.t.gray@gmail.com>
2019-10-06 08:50:44 -0400

    Converted SecureDialogActivity.java to kotlin.

--- app/src/main/java/com/example/android/apis/app/SecureDialogActivity.kt
+++ app/src/main/java/com/example/android/apis/app/SecureDialogActivity.kt
@@ -14,44 +14,45 @@
  * limitations under the License.
  */
 
-package com.example.android.apis.app;
+package com.example.android.apis.app
 
-import com.example.android.apis.R;
+import com.example.android.apis.R
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
+import android.app.Activity
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Button
 
 /**
  * <h3>Secure Dialog Activity</h3>
  *
- * <p>
+ *
+ *
  * This activity demonstrates how to create a dialog whose window is backed by
- * a secure surface using {@link WindowManager.LayoutParams#FLAG_SECURE}.
+ * a secure surface using [WindowManager.LayoutParams.FLAG_SECURE].
  * Because the surface is secure, its contents cannot be captured in screenshots
  * and will not be visible on non-secure displays even when mirrored.
- * </p><p>
+ *
+ *
  * Here are a few things you can do to experiment with secure surfaces and
  * observe their behavior.
- * <ul>
- * <li>Try taking a screenshot.  Either the system will prevent you from taking
+ *
+ *  * Try taking a screenshot.  Either the system will prevent you from taking
  * a screenshot altogether or the screenshot should not contain the contents
  * of the secure surface.
- * <li>Try mirroring the secure surface onto a non-secure display such as an
+ *  * Try mirroring the secure surface onto a non-secure display such as an
  * "Overlay Display" created using the "Simulate secondary displays" option in
  * the "Developer options" section of the Settings application.  The non-secure
  * secondary display should not show the contents of the secure surface.
- * <li>Try mirroring the secure surface onto a secure display such as an
+ *  * Try mirroring the secure surface onto a secure display such as an
  * HDMI display with HDCP enabled.  The contents of the secure surface should appear
  * on the display.
- * </ul>
- * </p>
+ *
+ *
  */
-public class SecureDialogActivity extends Activity
-        implements View.OnClickListener {
+class SecureDialogActivity : Activity(), View.OnClickListener {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
      * onCreate, then we set our content view to our layout file R.layout.secure_dialog_activity.
@@ -61,19 +62,18 @@ public class SecureDialogActivity extends Activity
      *
      * @param savedInstanceState always null since onSaveInstanceState is not overridden.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         // Be sure to call the super class.
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState)
 
         // See assets/res/any/layout/secure_dialog_activity.xml for this
         // view layout definition, which is being set here as
         // the content of our screen.
-        setContentView(R.layout.secure_dialog_activity);
+        setContentView(R.layout.secure_dialog_activity)
 
         // Handle click events on the button to show the dialog.
-        Button button = (Button)findViewById(R.id.show);
-        button.setOnClickListener(this);
+        val button = findViewById<View>(R.id.show) as Button
+        button.setOnClickListener(this)
     }
 
     /**
@@ -85,20 +85,19 @@ public class SecureDialogActivity extends Activity
      *
      * @param v View of the "Show secure dialog" Button (R.id.show)
      */
-    @Override
-    public void onClick(View v) {
+    override fun onClick(v: View) {
         // Create a dialog.
-        AlertDialog dialog = new AlertDialog.Builder(this)
+        val dialog = AlertDialog.Builder(this)
                 .setPositiveButton(android.R.string.ok, null)
                 .setMessage(R.string.secure_dialog_dialog_text)
-                .create();
+                .create()
 
         // Make the dialog secure.  This must be done at the time the dialog is
         // created.  It cannot be changed after the dialog has been shown.
-        dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
-                WindowManager.LayoutParams.FLAG_SECURE);
+        dialog.window!!.setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE)
 
         // Show the dialog.
-        dialog.show();
+        dialog.show()
     }
 }
