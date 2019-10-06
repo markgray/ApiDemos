71d90e27cae852fedf02b25787ac2a4def91574d
markgray <mark.t.gray@gmail.com>
2019-10-06 09:01:23 -0400

    Edited comments.

--- app/src/main/java/com/example/android/apis/app/SecureDialogActivity.kt
+++ app/src/main/java/com/example/android/apis/app/SecureDialogActivity.kt
@@ -26,38 +26,32 @@ import android.view.WindowManager
 import android.widget.Button
 
 /**
- * <h3>Secure Dialog Activity</h3>
- *
- *
+ * Secure Dialog Activity
  *
  * This activity demonstrates how to create a dialog whose window is backed by
  * a secure surface using [WindowManager.LayoutParams.FLAG_SECURE].
  * Because the surface is secure, its contents cannot be captured in screenshots
  * and will not be visible on non-secure displays even when mirrored.
  *
- *
  * Here are a few things you can do to experiment with secure surfaces and
- * observe their behavior.
- *
- *  * Try taking a screenshot.  Either the system will prevent you from taking
+ * observe their behavior:
+ *  - Try taking a screenshot. Either the system will prevent you from taking
  * a screenshot altogether or the screenshot should not contain the contents
  * of the secure surface.
- *  * Try mirroring the secure surface onto a non-secure display such as an
+ *  - Try mirroring the secure surface onto a non-secure display such as an
  * "Overlay Display" created using the "Simulate secondary displays" option in
- * the "Developer options" section of the Settings application.  The non-secure
+ * the "Developer options" section of the Settings application. The non-secure
  * secondary display should not show the contents of the secure surface.
- *  * Try mirroring the secure surface onto a secure display such as an
- * HDMI display with HDCP enabled.  The contents of the secure surface should appear
+ *  - Try mirroring the secure surface onto a secure display such as an HDMI
+ * display with HDCP enabled. The contents of the secure surface should appear
  * on the display.
- *
- *
  */
 class SecureDialogActivity : Activity(), View.OnClickListener {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * onCreate, then we set our content view to our layout file R.layout.secure_dialog_activity.
-     * Finally we locate the "Show secure dialog" Button (R.id.show) and set its OnClickListener
-     * to "this" (we implement View.OnClickListener so our onClick method will be called when
+     * `onCreate`, then we set our content view to our layout file R.layout.secure_dialog_activity.
+     * Finally we locate the "Show secure dialog" Button (R.id.show) and set its `OnClickListener`
+     * to "this" (we implement [View.OnClickListener] so our onClick method will be called when
      * the Button is clicked).
      *
      * @param savedInstanceState always null since onSaveInstanceState is not overridden.
@@ -72,16 +66,17 @@ class SecureDialogActivity : Activity(), View.OnClickListener {
         setContentView(R.layout.secure_dialog_activity)
 
         // Handle click events on the button to show the dialog.
-        val button = findViewById<View>(R.id.show) as Button
+        val button = findViewById<Button>(R.id.show)
         button.setOnClickListener(this)
     }
 
     /**
-     * Called when the button to show the dialog is clicked. First we create AlertDialog dialog using
-     * AlertDialog.Builder, then we set the WindowManager.LayoutParams.FLAG_SECURE flag of the dialog
-     * window which causes the system to treat the content of the window as secure, preventing it from
-     * appearing in screenshots or from being viewed on non-secure displays. Finally we call show to
-     * show the dialog.
+     * Called when the button to show the dialog is clicked. First we create the [AlertDialog]
+     * `val dialog` using an instance of [AlertDialog.Builder] to set its positive button to
+     * read "OK", and its message to read "I am a secure dialog!", then we set the FLAG_SECURE flag
+     * of the `dialog` window which causes the system to treat the content of the window as secure,
+     * preventing it from appearing in screenshots or from being viewed on non-secure displays.
+     * Finally we call show to show the dialog.
      *
      * @param v View of the "Show secure dialog" Button (R.id.show)
      */
