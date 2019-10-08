950dafa1f4ea2ef8de65e644aacb376c839e0062
markgray <mark.t.gray@gmail.com>
2019-10-08 08:09:34 -0400

    Converted SecureWindowActivity.java to kotlin.

--- app/src/main/java/com/example/android/apis/app/SecureWindowActivity.kt
+++ app/src/main/java/com/example/android/apis/app/SecureWindowActivity.kt
@@ -14,62 +14,58 @@
  * limitations under the License.
  */
 
-package com.example.android.apis.app;
+package com.example.android.apis.app
 
-import com.example.android.apis.R;
+import com.example.android.apis.R
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.WindowManager;
+import android.app.Activity
+import android.os.Bundle
+import android.view.WindowManager
 
 /**
- * <h3>Secure Window Activity</h3>
+ * Secure Window Activity
  *
- * <p>
- * This activity demonstrates how to create an activity whose window is backed by
- * a secure surface using {@link WindowManager.LayoutParams#FLAG_SECURE}.
- * Because the surface is secure, its contents cannot be captured in screenshots
- * and will not be visible on non-secure displays even when mirrored.
- * </p><p>
+ * This activity demonstrates how to create an activity whose window is backed by a secure surface
+ * using [WindowManager.LayoutParams.FLAG_SECURE]. Because the surface is secure, its contents cannot
+ * be captured in screenshots and will not be visible on non-secure displays even when mirrored.
+ *
  * Here are a few things you can do to experiment with secure surfaces and
  * observe their behavior.
- * <ul>
- * <li>Try taking a screenshot.  Either the system will prevent you from taking
+ *  - Try taking a screenshot.  Either the system will prevent you from taking
  * a screenshot altogether or the screenshot should not contain the contents
  * of the secure surface.
- * <li>Try mirroring the secure surface onto a non-secure display such as an
+ *  - Try mirroring the secure surface onto a non-secure display such as an
  * "Overlay Display" created using the "Simulate secondary displays" option in
  * the "Developer options" section of the Settings application.  The non-secure
  * secondary display should not show the contents of the secure surface.
- * <li>Try mirroring the secure surface onto a secure display such as an
+ *  - Try mirroring the secure surface onto a secure display such as an
  * HDMI display with HDCP enabled.  The contents of the secure surface should appear
  * on the display.
- * </ul>
- * </p>
+ *
+ *
  */
-public class SecureWindowActivity extends Activity {
+class SecureWindowActivity : Activity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * onCreate, then we set our content view to our layout file R.layout.secure_window_activity.
-     * Finally we set the WindowManager.LayoutParams.FLAG_SECURE flag of the window causing the
-     * system treat the content of the window as secure, preventing it from appearing in screenshots
+     * `onCreate`, then we set our content view to our layout file R.layout.secure_window_activity.
+     * Finally we set the [WindowManager.LayoutParams.FLAG_SECURE] flag of the window causing the
+     * system to treat the content of the window as secure, preventing it from appearing in screenshots
      * or from being viewed on non-secure displays.
      *
      * @param savedInstanceState always null since onSaveInstanceState is not overridden.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         // Be sure to call the super class.
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState)
 
         // See assets/res/any/layout/secure_window_activity.xml for this
         // view layout definition, which is being set here as
         // the content of our screen.
-        setContentView(R.layout.secure_window_activity);
+        setContentView(R.layout.secure_window_activity)
 
         // Make the window secure.  This must be done at the time the activity
         // is created.  It cannot be changed later.
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
-                WindowManager.LayoutParams.FLAG_SECURE);
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE)
     }
 }
