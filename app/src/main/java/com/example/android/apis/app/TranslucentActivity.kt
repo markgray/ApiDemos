ce6e541ef00eaed613068eea05295b31980546fa
markgray <mark.t.gray@gmail.com>
2019-10-09 08:43:01 -0400

    Converted TranslucentActivity.java to kotlin.

--- app/src/main/java/com/example/android/apis/app/TranslucentActivity.kt
+++ app/src/main/java/com/example/android/apis/app/TranslucentActivity.kt
@@ -14,42 +14,41 @@
  * limitations under the License.
  */
 
-package com.example.android.apis.app;
+package com.example.android.apis.app
 
 // Need the following import to get access to the app resources, since this
 // class is in a sub-package.
 
-import com.example.android.apis.R;
+import com.example.android.apis.R
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.app.Activity
+import android.os.Bundle
 
 /**
- * <h3>Translucent Activity</h3>
- * <p>
- * <p>This demonstrates the how to write an activity that is translucent,
- * allowing windows underneath to show through.</p>
+ * Translucent Activity
+ *
+ * This demonstrates the how to write an activity that is translucent,
+ * allowing windows underneath to show through.
  *
  * Sets the theme on the window in AndroidManifest to @style/Theme.Translucent
  * which is derived in styles.xml from android:style/Theme.Translucent, thereby
  * causing the window to be composited over whatever is behind it.
  */
-public class TranslucentActivity extends Activity {
+class TranslucentActivity : Activity() {
     /**
      * Initialization of the Activity after it is first created. First we call through
-     * to our super's     * implementation of onCreate, then we set our content view to
+     * to our super's implementation of `onCreate`, then we set our content view to
      * our layout file R.layout.translucent_background.
      *
      * @param savedInstanceState always null since onSaveInstanceState is not overridden
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         // Be sure to call the super class.
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState)
 
         // See assets/res/any/layout/translucent_background.xml for this
         // view layout definition, which is being set here as
         // the content of our screen.
-        setContentView(R.layout.translucent_background);
+        setContentView(R.layout.translucent_background)
     }
 }
