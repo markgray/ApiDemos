cbeb6d891e388ca28dc2e2c2f04effc9ce616a6b
markgray <mark.t.gray@gmail.com>
2019-10-10 08:17:02 -0400

    Converted WallpaperActivity.java to kotlin.

--- app/src/main/java/com/example/android/apis/app/WallpaperActivity.kt
+++ app/src/main/java/com/example/android/apis/app/WallpaperActivity.kt
@@ -14,20 +14,19 @@
  * limitations under the License.
  */
 
-package com.example.android.apis.app;
+package com.example.android.apis.app
 
 // Need the following import to get access to the app resources, since this
 // class is in a sub-package.
-import com.example.android.apis.R;
+import com.example.android.apis.R
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.app.Activity
+import android.os.Bundle
 
 /**
- * <h3>Wallpaper Activity</h3>
- * 
- * <p>This demonstrates the how to write an activity that has the system
- * wallpaper behind it.</p>
+ * Wallpaper Activity
+ *
+ * This demonstrates the how to write an activity that has the system wallpaper behind it.
  *
  * This is an app which has the system wallpaper behind it. It does this by setting the theme
  * on the window in AndroidManifest to Theme.Wallpaper, which inherits from the system theme
@@ -35,21 +34,20 @@ import android.os.Bundle;
  *
  * Our style/Theme.WallPaper also defines the element: <item name="android:colorForeground">#fff</item>
  */
-public class WallpaperActivity extends Activity {
+class WallpaperActivity : Activity() {
     /**
-     * Called when the activity is starting. First we call through to our super's implementation
-     * of onCreate, then we set our content view to our layout file R.layout.translucent_background.
+     * Called when the activity is starting. First we call through to our super's implementation of
+     * `onCreate`, then we set our content view to our layout file R.layout.translucent_background.
      *
      * @param savedInstanceState always null since onSaveInstanceState is not overridden.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         // Be sure to call the super class.
-        super.onCreate(savedInstanceState);
-        
+        super.onCreate(savedInstanceState)
+
         // See assets/res/any/layout/translucent_background.xml for this
         // view layout definition, which is being set here as
         // the content of our screen.
-        setContentView(R.layout.translucent_background);
+        setContentView(R.layout.translucent_background)
     }
 }
