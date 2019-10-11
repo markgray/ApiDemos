ba3f05f19a05bafede248e0944efedf6cb7464b3
markgray <mark.t.gray@gmail.com>
2019-10-11 08:21:48 -0400

    Converted OneShotAlarm.java to kotlin.

--- app/src/main/java/com/example/android/apis/app/OneShotAlarm.kt
+++ app/src/main/java/com/example/android/apis/app/OneShotAlarm.kt
@@ -14,60 +14,57 @@
  * limitations under the License.
  */
 
-package com.example.android.apis.app;
+package com.example.android.apis.app
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.BroadcastReceiver;
-import android.content.IntentFilter;
-import android.content.ServiceConnection;
-import android.widget.Toast;
-
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
 // Need the following import to get access to the app resources, since this
 // class is in a sub-package.
-import com.example.android.apis.R;
+import com.example.android.apis.R
 
 /**
- * This is an example of implement an {@link BroadcastReceiver} for an alarm that
+ * This is an example of implement an [BroadcastReceiver] for an alarm that
  * should occur once.
- * <p>
- * When the alarm goes off, we show a <i>Toast</i>, a quick message.
+ *
+ *
+ * When the alarm goes off, we show a *Toast*, a quick message.
  *
  * This class is flagged as a BroadcastReceiver in AndroidManifest.xml using the element:
  *
- *    <receiver
- *        android:name=".app.OneShotAlarm"
- *        android:process=":remote" />
+ * <receiver android:name=".app.OneShotAlarm" android:process=":remote"></receiver>
  */
-public class OneShotAlarm extends BroadcastReceiver {
+class OneShotAlarm : BroadcastReceiver() {
     /**
      * This method is called when the BroadcastReceiver is receiving an Intent
      * broadcast.  During this time you can use the other methods on
      * BroadcastReceiver to view/modify the current result values.  This method
      * is always called within the main thread of its process, unless you
      * explicitly asked for it to be scheduled on a different thread using
-     * {@link android.content.Context#registerReceiver(BroadcastReceiver,
-     * IntentFilter, String, android.os.Handler)}. When it runs on the main
+     * [android.content.Context.registerReceiver]. When it runs on the main
      * thread you should never perform long-running operations in it (there
      * is a timeout of 10 seconds that the system allows before considering
      * the receiver to be blocked and a candidate to be killed). You cannot l
      * aunch a popup dialog in your implementation of onReceive().
      *
-     * <p><b>If this BroadcastReceiver was launched through a &lt;receiver&gt; tag,
+     *
+     * **If this BroadcastReceiver was launched through a &lt;receiver&gt; tag,
      * then the object is no longer alive after returning from this
-     * function.</b>  This means you should not perform any operations that
+     * function.**  This means you should not perform any operations that
      * return a result to you asynchronously -- in particular, for interacting
      * with services, you should use
-     * {@link Context#startService(Intent)} instead of
-     * {@link Context#bindService(Intent, ServiceConnection, int)}.  If you wish
+     * [Context.startService] instead of
+     * [Context.bindService].  If you wish
      * to interact with a service that is already running, you can use
-     * {@link #peekService}.
+     * [.peekService].
      *
-     * <p>The Intent filters used in {@link android.content.Context#registerReceiver}
-     * and in application manifests are <em>not</em> guaranteed to be exclusive. They
+     *
+     * The Intent filters used in [android.content.Context.registerReceiver]
+     * and in application manifests are *not* guaranteed to be exclusive. They
      * are hints to the operating system about how to find suitable recipients. It is
      * possible for senders to force delivery to specific recipients, bypassing filter
-     * resolution.  For this reason, {@link #onReceive(Context, Intent) onReceive()}
+     * resolution.  For this reason, `onReceive(Context, Intent) onReceive()`
      * implementations should respond only to known actions, ignoring any unexpected
      * Intents that they may receive.
      *
@@ -76,9 +73,8 @@ public class OneShotAlarm extends BroadcastReceiver {
      * @param context The Context in which the receiver is running.
      * @param intent The Intent being received.
      */
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, R.string.one_shot_received, Toast.LENGTH_SHORT).show();
+    override fun onReceive(context: Context, intent: Intent) {
+        Toast.makeText(context, R.string.one_shot_received, Toast.LENGTH_SHORT).show()
     }
 }
 
