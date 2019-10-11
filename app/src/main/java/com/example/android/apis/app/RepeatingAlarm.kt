84cae6ff32fc5f23a009dadc0019aa9c7be0066f
markgray <mark.t.gray@gmail.com>
2019-10-11 08:28:41 -0400

    Converted RepeatingAlarm.java to kotlin.

--- app/src/main/java/com/example/android/apis/app/RepeatingAlarm.kt
+++ app/src/main/java/com/example/android/apis/app/RepeatingAlarm.kt
@@ -14,35 +14,33 @@
  * limitations under the License.
  */
 
-package com.example.android.apis.app;
+package com.example.android.apis.app
 
 // Need the following import to get access to the app resources, since this
 // class is in a sub-package.
+import com.example.android.apis.R
 
-import com.example.android.apis.R;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.BroadcastReceiver;
-import android.widget.Toast;
+import android.content.Context
+import android.content.Intent
+import android.content.BroadcastReceiver
+import android.widget.Toast
 
 /**
- * This is an example of implement an {@link BroadcastReceiver} for an alarm that
- * should occur once.
+ * This is an example of implement an [BroadcastReceiver] for an alarm that
+ * should occur repeatedly.
  */
-public class RepeatingAlarm extends BroadcastReceiver {
+class RepeatingAlarm : BroadcastReceiver() {
     /**
      * This method is called when the BroadcastReceiver is receiving an Intent
      * broadcast. We simply show a Toast stating:
      *
-     *     The repeating alarm has gone off
+     * "The repeating alarm has gone off"
      *
      * @param context The Context in which the receiver is running.
      * @param intent The Intent being received.
      */
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, R.string.repeating_received, Toast.LENGTH_SHORT).show();
+    override fun onReceive(context: Context, intent: Intent) {
+        Toast.makeText(context, R.string.repeating_received, Toast.LENGTH_SHORT).show()
     }
 }
 
