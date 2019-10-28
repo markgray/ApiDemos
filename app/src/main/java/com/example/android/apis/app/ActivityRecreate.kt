68f7e96acc6ce9aa98b335f4a91c916729766910
markgray <mark.t.gray@gmail.com>
2019-10-28 06:12:19 -0400

    Migrated ActivityRecreate to AppCompatActivity.

--- app/src/main/java/com/example/android/apis/app/ActivityRecreate.kt
+++ app/src/main/java/com/example/android/apis/app/ActivityRecreate.kt
@@ -17,25 +17,28 @@
 package com.example.android.apis.app
 
 import android.annotation.TargetApi
-import android.app.Activity
 import android.os.Build
 import android.os.Bundle
 import android.view.View.OnClickListener
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.android.apis.R
 
 /**
- * Uses setTheme(theme) followed by recreate() to cycle through three different Holo themes
+ * Uses `setTheme(theme)` followed by `recreate()` to cycle through three different Holo themes
  * (original version did not initialize the field that the current theme is kept in, causing
  * it to crash on Material Light default devices.)
  */
 @Suppress("MemberVisibilityCanBePrivate")
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-class ActivityRecreate : Activity() {
-    internal var mCurTheme: Int = 0 // Theme in use for this instance of the Activity
+class ActivityRecreate : AppCompatActivity() {
+    /**
+     * Theme in use for this instance of the Activity
+     */
+    internal var mCurTheme: Int = 0
 
     /**
-     * OnClickListener used the our "Recreate" Button (R.id.recreate) in our layout.
+     * [OnClickListener] used for the "Recreate" [Button] (R.id.recreate) in our layout.
      * Called when the view has been clicked. We simply call Activity.recreate() to cause this
      * Activity to be recreated with a new instance. This results in essentially the same flow
      * as when the Activity is created due to a configuration change -- the current instance
@@ -49,18 +52,18 @@ class ActivityRecreate : Activity() {
 
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * onCreate. Then if savedInstanceState is not null we retrieve the mCurTheme from that Bundle
-     * (stored under the key "theme"), and switch on its current value changing the mCurTheme from
-     * Theme_Holo_Light to Theme_Holo_Dialog to Theme_Holo and back to Theme_Holo_Light. If
-     * savedInstanceState is null (first time run) we set mCurTheme to Theme_Holo_Light. Next we
-     * set the base theme for this context to mCurTheme, then set the content view to our layout
-     * file R.layout.activity_recreate. Finally we locate the "Recreate" Button (R.id.recreate) and
-     * set its OnClickListener to OnClickListener mRecreateListener so that when the Button is
-     * clicked the Activity will be recreated.
+     * `onCreate`. Then if [savedInstanceState] is not *null* we retrieve the [mCurTheme] from that
+     * [Bundle] (stored under the key "theme"), and switch on its current value changing the
+     * [mCurTheme] from Theme_Holo_Light to Theme_Holo_Dialog to Theme_Holo and back to
+     * Theme_Holo_Light. If [savedInstanceState] is null (first time run) we set [mCurTheme] to
+     * Theme_Holo_Light. Next we set the base theme for this context to [mCurTheme], then set the
+     * content view to our layout file R.layout.activity_recreate. Finally we locate the "Recreate"
+     * [Button] (R.id.recreate) and set its [OnClickListener] to our [OnClickListener] field
+     * [mRecreateListener] so that when the [Button] is clicked the Activity will be recreated.
      *
      * @param savedInstanceState If the activity is being re-initialized after
      * previously being shut down then this Bundle contains the data it most
-     * recently supplied in [.onSaveInstanceState].
+     * recently supplied in [onSaveInstanceState].
      * ***Note: Otherwise it is null.***
      */
     @Suppress("DEPRECATION")
@@ -91,13 +94,14 @@ class ActivityRecreate : Activity() {
 
     /**
      * Called to retrieve per-instance state from an activity before being killed
-     * so that the state can be restored in [.onCreate] or
-     * [.onRestoreInstanceState] (the [Bundle] populated by this method
+     * so that the state can be restored in [onCreate] or
+     * [onRestoreInstanceState] (the [Bundle] populated by this method
      * will be passed to both).
      *
-     * First we call through to our super's implementation of onSaveInstanceState, then we insert
-     * the current value of mCurTheme using the key "theme" in the Bundle savedInstanceState passed
-     * us. It will be used in our onCreate method when the Activity is recreated.
+     * First we call through to our super's implementation of `onSaveInstanceState`, then we insert
+     * the current value of [mCurTheme] using the key "theme" in the [Bundle] parameter
+     * [savedInstanceState] passed us. It will be used in our [onCreate] method when the Activity
+     * is recreated.
      *
      * @param savedInstanceState Bundle in which to place your saved state.
      */
