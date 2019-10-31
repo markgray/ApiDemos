e3af27b022447085ed5752a7a825eaed7dd2ee9f
markgray <mark.t.gray@gmail.com>
2019-10-31 05:21:38 -0400

    Edited comments, cleaned up code a bit.

--- app/src/main/java/com/example/android/apis/app/FragmentDialog.kt
+++ app/src/main/java/com/example/android/apis/app/FragmentDialog.kt
@@ -41,31 +41,31 @@ import com.example.android.apis.R
 @Suppress("MemberVisibilityCanBePrivate")
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 class FragmentDialog : AppCompatActivity() {
-    internal var mStackLevel = 0 // Level used to choose style of dialog (and stack level)
+    /**
+     * Level used to choose style of dialog (and stack level)
+     */
+    internal var mStackLevel = 0
 
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * onCreate, then we set our content view to our layout file R.layout.fragment_dialog. Next we
-     * locate the TextView in our layout (R.id.text) and set its text to the instructions for this
+     * `onCreate`, then we set our content view to our layout file R.layout.fragment_dialog. Next we
+     * locate the [TextView] in our layout (R.id.text) and set its text to the instructions for this
      * demo (R.string.dialog_fragment_example_instructions):
      *
+     *     Example of displaying dialogs with a DialogFragment.
+     *     Press the show button below to see the first dialog;
+     *     pressing successive show buttons will display other
+     *     dialog styles as a stack, with dismissing or back
+     *     going to the previous dialog.
      *
-     * Example of displaying dialogs with a DialogFragment.
-     * Press the show button below to see the first dialog;
-     * pressing successive show buttons will display other
-     * dialog styles as a stack, with dismissing or back
-     * going to the previous dialog.
+     * We initialize our [Button] variable `val button` by finding the view in our layout with ID
+     * R.id.show ("SHOW") and set its `OnClickListener` to a lambda which calls our method [showDialog]
+     * when the [Button] is clicked. Finally if [savedInstanceState] is not null (we are being recreated
+     * after an orientation change or other reason) we retrieve the value of our field [mStackLevel]
+     * that our callback [onSaveInstanceState] saved under the key "level".
      *
-     *
-     * We locate Button button in our layout (R.id.show "SHOW") and set its OnClickListener to an
-     * anonymous class which calls our method showDialog() when the Button is clicked. Finally if
-     * savedInstanceState is not null (we are being recreated after an orientation change or other
-     * reason) we retrieve the value of our field mStackLevel that our callback onSaveInstanceState
-     * saved using the key "level".
-     *
-     * @param savedInstanceState if Activity has been recreated after an orientation change this will
-     * have the value for mStackLevel saved by onSaveInstanceState using
-     * the key "level"
+     * @param savedInstanceState if the Activity has been recreated after an orientation change this
+     * will have the value for [mStackLevel] saved by [onSaveInstanceState] under the key "level"
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -92,16 +92,15 @@ class FragmentDialog : AppCompatActivity() {
     }
 
     /**
-     * Called to retrieve per-instance state from an activity before being killed
-     * so that the state can be restored in [.onCreate] or
-     * [.onRestoreInstanceState] (the [Bundle] populated by this method
-     * will be passed to both).
+     * Called to retrieve per-instance state from an activity before being killed so that the state
+     * can be restored in [onCreate] or [onRestoreInstanceState] (the [Bundle] populated by this
+     * method will be passed to both).
      *
+     * First we call through to our super's implementation of [onSaveInstanceState], then we store
+     * the value of our field [mStackLevel] in the mapping of our [Bundle] parameter [outState]
+     * under the key "level".
      *
-     * First we call through to our super's implementation of onCreate, then we store the value of
-     * our field mStackLevel in the mapping of our parameter Bundle outState under the key "level".
-     *
-     * @param outState Bundle in which to place your saved state.
+     * @param outState [Bundle] in which to place our saved state.
      */
     public override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -109,14 +108,15 @@ class FragmentDialog : AppCompatActivity() {
     }
 
     /**
-     * Creates and show's a new instance of MyDialogFragment using the next style and theme. First we
-     * increment the value of our field mStackLevel and if it is greater than 8 we set it back to 1.
-     * Then we use the FragmentManager for interacting with fragments associated with this activity
-     * to begin a FragmentTransaction ft, and then we add this transaction to the back stack. We
-     * search the FragmentManager for an existing Fragment that is already using Tag "dialog" and if
-     * there is one we use FragmentTransaction ft to remove it. Next we create a new instance of
-     * MyDialogFragment with the style and theme corresponding to the value of mStackLevel,
-     * (DialogFragment newFragment) and finally we show this DialogFragment.
+     * Creates and show's a new instance of [MyDialogFragment] using the next style and theme. First
+     * we increment the value of our field [mStackLevel] and if it is greater than 8 we set it back
+     * to 1. Then we use the support `FragmentManager` for interacting with fragments associated with
+     * this activity to begin a `FragmentTransaction` which we save in our variable `val ft`, and
+     * then we add this transaction to the back stack. We have the `FragmentManager` search for an
+     * existing `Fragment` that is already using Tag "dialog" and if there is one we use `ft` to
+     * remove it. Next we create a new instance of [MyDialogFragment] with the style and theme
+     * corresponding to the value of mStackLevel to initialize our [DialogFragment] variable
+     * `val newFragment` and finally we show `newFragment`.
      */
     internal fun showDialog() {
         mStackLevel++
@@ -138,20 +138,22 @@ class FragmentDialog : AppCompatActivity() {
     }
 
     /**
-     * This DialogFragment displays a dialog with its style and theme configured by its arguments
+     * This [DialogFragment] displays a dialog with its style and theme configured by its arguments
      * when created, using 8 styles and five themes.
      */
     class MyDialogFragment : DialogFragment() {
-        internal var mNum: Int = 0 // Which combination of styles and themes are being used for the dialog
+        /**
+         * Which combination of styles and themes are being used for the dialog
+         */
+        internal var mNum: Int = 0
 
         /**
          * Called to do initial creation of a fragment. First we call through to our super's
-         * implementation of onCreate, then we fetch the value of our field **int mNum** which
-         * is contained in our arguments under the key "num". Next we set our variables
-         * **int style** and **int theme** based on the value of **mNum**, defaulting to
-         * **STYLE_NORMAL** for **style** and 0 for **theme** (causes the system to pick
-         * an appropriate theme (based on the style)). Finally we use **style** and **theme**
-         * to set the attributes for our dialog.
+         * implementation of `onCreate`, then we fetch the value of our [Int] field [mNum] which
+         * is contained in our arguments under the key "num". Next we set our [Int] variables
+         * `var style` and `var theme` based on the value of **mNum**, defaulting to `STYLE_NORMAL`
+         * for `style` and 0 for `theme` (causes the system to pick an appropriate theme (based on
+         * the `style`)). Finally we use `style` and `theme` to set the attributes for our dialog.
          *
          * @param savedInstanceState we do not override onSaveInstanceState so do not use
          */
@@ -186,22 +188,22 @@ class FragmentDialog : AppCompatActivity() {
 
         /**
          * Called to have the fragment instantiate its user interface view. First we inflate our
-         * layout file R.layout.fragment_dialog into **View v**, locate the **TextView** in
-         * our layout R.id.text and set its text to a **String dialogLabel** which we create to
-         * describe the style and theme we are using for the dialog. Next we locate the R.id.show
-         * ("Show") **Button button** in our layout and set its OnClickListener to an anonymous
-         * class which will call our method **showDialog()** to show the next styled and themed
-         * **MyDialogFragment** that follows this one. Finally we return **View v** to our
-         * caller.
+         * layout file R.layout.fragment_dialog into our [View] variable `val v`, initialize our
+         * variable `val tv` by finding the [TextView] in our layout with ID R.id.text and set its
+         * text to a [String] variable `val dialogLabel` which we create to describe the style and
+         * theme we are using for the dialog. Next we locate the R.id.show ("Show") [Button] to
+         * initialize our variable `val button` and set its `OnClickListener` to a lambda which will
+         * call our method [showDialog] to show the next styled and themed [MyDialogFragment] that
+         * follows this one. Finally we return `v` to our caller.
          *
-         * @param inflater           The LayoutInflater object that can be used to inflate
-         * any views in the fragment,
-         * @param container          If non-null, this is the parent view that the fragment's
-         * UI should be attached to.  The fragment should not add the view itself,
-         * but this can be used to generate the LayoutParams of the view.
+         * @param inflater The [LayoutInflater] object that can be used to inflate any views
+         * in the fragment,
+         * @param container If non-null, this is the parent view that the fragment's UI will be
+         * attached to. The fragment should not add the view itself, but this can be used to
+         * generate the `LayoutParams` of the view.
          * @param savedInstanceState If non-null, this fragment is being re-constructed
          * from a previous saved state as given here.
-         * @return Return the View for the fragment's UI.
+         * @return Return the [View] for the fragment's UI.
          */
         override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
             val v = inflater.inflate(R.layout.fragment_dialog, container, false)
@@ -227,17 +229,21 @@ class FragmentDialog : AppCompatActivity() {
             return v
         }
 
+        /**
+         * Our static factory method.
+         */
         companion object {
 
             /**
-             * Create a new instance of MyDialogFragment, providing "num" as an argument. First we create
-             * a new instance of MyDialogFragment f, then we create a **Bundle args** and add
-             * our parameter num to its mapping using "num" as the key, and set **args** as the
-             * arguments for **f**. Finally we return **f** to the caller.
+             * Create a new instance of [MyDialogFragment], providing our parameter [num] as the
+             * argument. First we create a new instance of [MyDialogFragment] to initialize our
+             * variable `val f`, then we create a [Bundle] to initialize our variable `val args`
+             * and add our parameter [num] to its mapping using "num" as the key, and set `args`
+             * as the arguments for `f`. Finally we return `f` to the caller.
              *
              * @param num number of the style and theme combination to use
-             * @return a MyDialogFragment with arguments set to include **num** stored using the
-             * key "num"
+             * @return a [MyDialogFragment] with arguments set to include [num] stored under the
+             * key "num".
              */
             internal fun newInstance(num: Int): MyDialogFragment {
                 val f = MyDialogFragment()
@@ -252,29 +258,32 @@ class FragmentDialog : AppCompatActivity() {
         }
     }
 
+    /**
+     * Our static method.
+     */
     companion object {
 
         /**
-         * This method converts the value of mStackLevel currently being used to a String describing the
-         * the style and theme to display to the user. We simply use a switch statement to choose the
-         * String based on the value of num passed us 1-8, returning "STYLE_NORMAL" if outside those
-         * values.
+         * This method converts the value of [mStackLevel] currently being used to a [String]
+         * describing the style and theme to display to the user. We simply use a when statement to
+         * choose the [String] based on the value of [num] passed us 1-8, returning "STYLE_NORMAL"
+         * if [num] is outside the range of values we use.
          *
          * @param num mStackLevel for current style and theme
          * @return String describing style and theme
          */
         internal fun getNameForNum(num: Int): String {
-            when (num) {
-                1 -> return "STYLE_NO_TITLE"
-                2 -> return "STYLE_NO_FRAME"
-                3 -> return "STYLE_NO_INPUT (this window can't receive input, so " + "you will need to press the bottom show button)"
-                4 -> return "STYLE_NORMAL with dark fullscreen theme"
-                5 -> return "STYLE_NORMAL with light theme"
-                6 -> return "STYLE_NO_TITLE with light theme"
-                7 -> return "STYLE_NO_FRAME with light theme"
-                8 -> return "STYLE_NORMAL with light fullscreen theme"
+            return when (num) {
+                1 -> "STYLE_NO_TITLE"
+                2 -> "STYLE_NO_FRAME"
+                3 -> "STYLE_NO_INPUT (this window can't receive input, so " + "you will need to press the bottom show button)"
+                4 -> "STYLE_NORMAL with dark fullscreen theme"
+                5 -> "STYLE_NORMAL with light theme"
+                6 -> "STYLE_NO_TITLE with light theme"
+                7 -> "STYLE_NO_FRAME with light theme"
+                8 -> "STYLE_NORMAL with light fullscreen theme"
+                else -> "STYLE_NORMAL"
             }
-            return "STYLE_NORMAL"
         }
     }
 
