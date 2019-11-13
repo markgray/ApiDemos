e5de0d35b1132587551e560812ebeaf2597a4402
markgray <mark.t.gray@gmail.com>
2019-11-13 06:00:43 -0500

    Converted 2 activities to kotlin:
    IncomingMessageInterstitial.java
    IncomingMessageView.java

---
+++ app/src/main/java/com/example/android/apis/app/IncomingMessageInterstitial.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.apis.app
+
+import android.annotation.TargetApi
+import android.os.Build
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
+
+/**
+ * This is an activity that provides an interstitial UI for the notification
+ * that is posted by [IncomingMessage].  It allows the user to switch
+ * to the app in its appropriate state if they want.
+ */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+class IncomingMessageInterstitial : AppCompatActivity() {
+    /**
+     * Called when the activity is starting. First we call through to our super's implementation of
+     * `onCreate`, then we set our content view to our layout file R.layout.incoming_message_interstitial.
+     * We then locate `Button button` with ID R.id.notify_app in our layout and set its `OnClickListener`
+     * to an anonymous class which will call our method `switchToApp()` when the Button is clicked.
+     *
+     * @param savedInstanceState we do not override `onSaveInstanceState` so do not use
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.incoming_message_interstitial)
+
+        val button = findViewById<Button>(R.id.notify_app)
+        /**
+         * Called when the R.id.notify_app Button ("Switch to app") is clicked, we simply call
+         * our method `switchToApp()` to build an appropriate back stack and switch to
+         * the Activity `IncomingMessageView`.
+         *
+         * Parameter: View of the Button that was clicked
+         */
+        button.setOnClickListener {
+                switchToApp()
+            }
+    }
+
+
+    /**
+     * Perform a switch to the app.  A new activity stack is started, replacing whatever is currently
+     * running, and this activity is finished. First we retrieve `CharSequence from` which was
+     * stored as an extra in the Intent which launched us under the key KEY_FROM, and `CharSequence msg`
+     * stored under the key KEY_MESSAGE. We then use the method `IncomingMessage.makeMessageIntentStack`
+     * to build a back stack for `Intent[] stack` which includes an Intent to launch
+     * `IncomingMessageView`, start the activities in `stack`, and close this Activity by
+     * calling `finish()`.
+     */
+    @Suppress("MemberVisibilityCanBePrivate")
+    internal fun switchToApp() {
+        // We will launch the app showing what the user picked.  In this simple
+        // example, it is just what the notification gave us.
+        val from = intent.getCharSequenceExtra(IncomingMessageView.KEY_FROM)
+        val msg = intent.getCharSequenceExtra(IncomingMessageView.KEY_MESSAGE)
+        // Build the new activity stack, launch it, and finish this UI.
+
+        val stack = IncomingMessage.makeMessageIntentStack(this, from!!, msg!!)
+        startActivities(stack)
+        finish()
+    }
+
+}
---
+++ app/src/main/java/com/example/android/apis/app/IncomingMessageView.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.apis.app
+
+import com.example.android.apis.R
+
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * This activity is run as the click activity for [IncomingMessage], and also
+ * [IncomingMessageInterstitial]. When it comes up, it also clears the notification, because
+ * the "message" has been "read."
+ */
+class IncomingMessageView : AppCompatActivity() {
+
+    /**
+     * Called when the activity is starting. First we call through to our super's implementation of
+     * `onCreate`, then we set our content view to our layout file R.layout.incoming_message_view.
+     * We locate the `TextView` in the layout with ID R.id.from and set its text to the String
+     * stored as an extra in the Intent that launched us under the key KEY_FROM and the text of
+     * R.id.message to that stored under the key KEY_MESSAGE. We fetch a handle to the system service
+     * NOTIFICATION_SERVICE for `NotificationManager nm` and use it to cancel the notification
+     * which was originally posted by `IncomingMessage`.
+     *
+     * @param savedInstanceState we do not override `onSaveInstanceState` so do not use.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.incoming_message_view)
+
+        // Fill in the message content.
+        (findViewById<View>(R.id.from) as TextView).text = intent.getCharSequenceExtra(KEY_FROM)
+        (findViewById<View>(R.id.message) as TextView).text = intent.getCharSequenceExtra(KEY_MESSAGE)
+
+        // look up the notification manager service
+        val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // cancel the notification that we started in IncomingMessage
+
+        nm.cancel(R.string.imcoming_message_ticker_text)
+    }
+
+    companion object {
+        /**
+         * Extra that can be supplied to Intent: who the message is from.
+         */
+        const val KEY_FROM = "from"
+        /**
+         * Extra that can be supplied to Intent: the message that was sent.
+         */
+        const val KEY_MESSAGE = "message"
+    }
+}
+
