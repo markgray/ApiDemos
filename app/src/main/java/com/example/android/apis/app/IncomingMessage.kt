9120221eff98f960a62620c6a0eca5f3420f73b6
markgray <mark.t.gray@gmail.com>
2019-11-13 06:56:17 -0500

    Edited comments.

--- app/src/main/java/com/example/android/apis/app/IncomingMessage.kt
+++ app/src/main/java/com/example/android/apis/app/IncomingMessage.kt
@@ -36,10 +36,10 @@ import java.util.*
 
 /**
  * UI for posting an example notification. Allows you to choose between a regular app notification,
- * and an interstitial notification and shows how to launch `IncomingMessageView` from either
- * type of notification. The interstitial choice launches `IncomingMessageInterstitial` which
+ * and an interstitial notification and shows how to launch [IncomingMessageView] from either
+ * type of notification. The interstitial choice launches [IncomingMessageInterstitial] which
  * on clicking the "Switch to App" button uses the same function as the non-interstitial notification
- * to launch `IncomingMessageView`.
+ * to launch [IncomingMessageView].
  */
 @Suppress("MemberVisibilityCanBePrivate")
 @TargetApi(Build.VERSION_CODES.O)
@@ -47,13 +47,13 @@ class IncomingMessage : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
      * `onCreate`, then we set our content view to our layout file R.layout.incoming_message.
-     * We locate the `Button` R.id.notify_app to initialize `Button button`, then set
-     * its `OnClickListener` to an anonymous class which calls the method `showAppNotification()`
-     * when the `Button` is clicked. Then we locate the `Button` R.id.notify_interstitial
-     * and set its `OnClickListener` to an anonymous class which calls the method
-     * `showInterstitialNotification()`.
+     * We locate the [Button] with ID R.id.notify_app to initialize our variable `val button`,
+     * then set its `OnClickListener` to a lambda which calls our method [showAppNotification]
+     * when the [Button] is clicked. Then we set `button` to the [Button] with the ID
+     * R.id.notify_interstitial and set its `OnClickListener` to a lambda which calls the method
+     * [showInterstitialNotification].
      *
-     * @param savedInstanceState we do not override `onSaveInstanceState` so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,8 +68,8 @@ class IncomingMessage : AppCompatActivity() {
          * Parameter: `View` of the `Button` that was clicked
          */
         button.setOnClickListener {
-                showAppNotification()
-            }
+            showAppNotification()
+        }
 
         button = findViewById(R.id.notify_interstitial)
         /**
@@ -83,55 +83,36 @@ class IncomingMessage : AppCompatActivity() {
         }
     }
 
-
     /**
      * Shows a notification in the status bar, consisting of our icon and associated expanded entry.
-     * We are called when the `Button` R.id.notify_app ("Show App Notification") is clicked.
+     * We are called when the [Button] with ID R.id.notify_app ("Show App Notification") is clicked.
      * First we fetch a handle to the system-level service NOTIFICATION_SERVICE and save it in our
-     * variable `NotificationManager nm`. We initialize `NotificationChannel chan1` with
-     * a new instance whose id and user visible name are both PRIMARY_CHANNEL ("default"), and whose
-     * importance is IMPORTANCE_DEFAULT (shows everywhere, makes noise, but does not visually intrude).
-     * We set the notification light color of `chan1` to GREEN, and set its lock screen visibility
-     * to VISIBILITY_PRIVATE (shows this notification on all lockscreens, but conceal sensitive or
-     * private information on secure lockscreens). We then have `nm` create notification channel
-     * `chan1`. We create a fake message to receive consisting of a `CharSequence from`
-     * and a random `CharSequence message`. Next we create a `PendingIntent contentIntent`
-     * which will be launched when our notification is clicked. It consists of a back stack of Intents
-     * created by our method `makeMessageIntentStack` and the flag FLAG_CANCEL_CURRENT (Flag
-     * indicating that if the described PendingIntent already exists, the current one should be canceled
-     * before generating a new one).
-     *
-     *
-     * We initialize the String `String tickerText` by using our resource string
+     * [NotificationManager] variable `val nm`. We initialize our [NotificationChannel] variable
+     * `val chan1` with a new instance whose id and user visible name are both PRIMARY_CHANNEL
+     * ("default"), and whose importance is IMPORTANCE_DEFAULT (shows everywhere, makes noise, but
+     * does not visually intrude). We set the notification light color of `chan1` to GREEN, and set
+     * its lock screen visibility to VISIBILITY_PRIVATE (shows this notification on all lockscreens,
+     * but conceals sensitive or private information on secure lockscreens). We then have `nm`
+     * create notification channel `chan1`. We create a fake message to receive consisting of a
+     * [String] `val from` of "Joe",  and a random [CharSequence] `val message`. Next we create a
+     * [PendingIntent] to initialize our variable `val contentIntent` which will be launched when
+     * our notification is clicked. It consists of a back stack of [Intent]'s created by our method
+     * [makeMessageIntentStack] and the flag FLAG_CANCEL_CURRENT (Flag indicating that if the
+     * described [PendingIntent] already exists, the current one should be canceled before generating
+     * a new one). We initialize the [String] variable `val tickerText` by using our resource string
      * R.string.incoming_message_ticker_text as the format for our `message`. We create a
-     * `Notification.Builder notifBuilder` for `NotificationChannel` PRIMARY_CHANNEL and
+     * [Notification.Builder] `val notifBuilder` for the [NotificationChannel] PRIMARY_CHANNEL and
      * chain together methods which:
-     *
-     *  *
-     * Set the small icon to R.drawable.stat_sample
-     *
-     *  *
-     * Set the "ticker" text which is sent to accessibility services to `tickerText`
-     *
-     *  *
-     * Add a timestamp pertaining to the notification to be the current time in milliseconds
-     *
-     *  *
-     * Set the first line of text in the platform notification template to be `from`
-     *
-     *  *
-     * Set the second line of text in the platform notification template to be `message`
-     *
-     *  *
-     * Supply `contentIntent` as the PendingIntent to be sent when the notification is clicked
-     *
+     *  * Set the small icon to R.drawable.stat_sample
+     *  * Set the "ticker" text which is sent to accessibility services to `tickerText`
+     *  * Add a timestamp pertaining to the notification to be the current time in milliseconds
+     *  * Set the first line of text in the platform notification template to be `from`
+     *  * Set the second line of text in the platform notification template to be `message`
+     *  * Supply `contentIntent` as the [PendingIntent] to be sent when the notification is clicked
      *
      * We set which notification properties will be inherited from system defaults to be DEFAULT_ALL
-     * (all the default values).
-     *
-     *
-     * Finally we use `NotificationManager nm` to post the notification built from
-     * `notifBuilder` in the status bar.
+     * (all the default values). Finally we use [NotificationManager] `nm` to post the notification
+     * built from `notifBuilder` in the status bar.
      */
     internal fun showAppNotification() {
         // look up the notification manager service
@@ -186,50 +167,36 @@ class IncomingMessage : AppCompatActivity() {
     }
 
     /**
-     * Builds a notification which will launch the `IncomingMessageInterstitial` activity when
+     * Builds a notification which will launch the [IncomingMessageInterstitial] activity when
      * it is clicked on. First we fetch a handle to the system wide service NOTIFICATION_SERVICE to
-     * initialize `NotificationManager nm`. We initialize `NotificationChannel chan1` with
-     * a new instance whose id and user visible name are both PRIMARY_CHANNEL ("default"), and whose
-     * importance is IMPORTANCE_DEFAULT (shows everywhere, makes noise, but does not visually intrude).
-     * We set the notification light color of `chan1` to GREEN, and set its lock screen visibility
-     * to VISIBILITY_PRIVATE (shows this notification on all lockscreens, but conceal sensitive or
-     * private information on secure lockscreens). We then have `nm` create notification channel
-     * `chan1`. We create a fake message to receive consisting of a `CharSequence from`
-     * and a random `CharSequence message`. Next we create a `Intent intent` which will
-     * launch the Activity `IncomingMessageInterstitial` when our notification is clicked. We
-     * store our `from` as an extra in the Intent using the key KEY_FROM, and `message`
-     * as an extra using the key KEY_MESSAGE. We set the flags of the Intent for FLAG_ACTIVITY_NEW_TASK
-     * and FLAG_ACTIVITY_CLEAR_TASK. Next we create a new instance of `PendingIntent contentIntent`
-     * which will launch the `intent` we just created and configured, we initialize the String
-     * `String tickerText` by using our resource string R.string.incoming_message_ticker_text
-     * as the format for our `message`. We create a `Notification.Builder notifBuilder`
-     * for `NotificationChannel` PRIMARY_CHANNEL and chain together methods which:
-     *
-     *  *
-     * Set the small icon to R.drawable.stat_sample
-     *
-     *  *
-     * Set the "ticker" text which is sent to accessibility services to `tickerText`
-     *
-     *  *
-     * Add a timestamp pertaining to the notification to be the current time in milliseconds
-     *
-     *  *
-     * Set the first line of text in the platform notification template to be `from`
-     *
-     *  *
-     * Set the second line of text in the platform notification template to be `message`
-     *
-     *  *
-     * Supply `contentIntent` as the PendingIntent to be sent when the notification is clicked
-     *
+     * initialize our [NotificationManager] variable `val nm`. We initialize [NotificationChannel]
+     * variable `val chan1` with a new instance whose id and user visible name are both
+     * PRIMARY_CHANNEL ("default"), and whose importance is IMPORTANCE_DEFAULT (shows everywhere,
+     * makes noise, but does not visually intrude). We set the notification light color of `chan1`
+     * to GREEN, and set its lock screen visibility to VISIBILITY_PRIVATE (shows this notification
+     * on all lockscreens, but conceals sensitive or private information on secure lockscreens). We
+     * then have `nm` create notification channel `chan1`. We create a fake message to receive
+     * consisting of a [String] `val from` and a randomly chosen [CharSequence] `val message`. Next
+     * we create an [Intent] to initialize our variable `val intent` which will launch the Activity
+     * [IncomingMessageInterstitial] when our notification is clicked. We store our `from` as an
+     * extra in the [Intent] using the key KEY_FROM, and `message` as an extra using the key
+     * KEY_MESSAGE. We set the flags of the Intent for FLAG_ACTIVITY_NEW_TASK and
+     * FLAG_ACTIVITY_CLEAR_TASK. Next we create a new instance of [PendingIntent] to initialize our
+     * variable `val contentIntent` which will launch the `intent` we just created and configured,
+     * we initialize the [String] variable `val tickerText` by using our resource string
+     * R.string.incoming_message_ticker_text as the format for our `message`. We create a
+     * [Notification.Builder] to initialize our variable `val notifBuilder` for [NotificationChannel]
+     * PRIMARY_CHANNEL and chain together methods which:
+     *  * Set the small icon to R.drawable.stat_sample
+     *  * Set the "ticker" text which is sent to accessibility services to `tickerText`
+     *  * Add a timestamp pertaining to the notification to be the current time in milliseconds
+     *  * Set the first line of text in the platform notification template to be `from`
+     *  * Set the second line of text in the platform notification template to be `message`
+     *  * Supply `contentIntent` as the PendingIntent to be sent when the notification is clicked
      *
      * We set which notification properties will be inherited from system defaults to be DEFAULT_ALL
-     * (all the default values).
-     *
-     *
-     * Finally we use `NotificationManager nm` to post the notification built from
-     * `notifBuilder` in the status bar.
+     * (all the default values). Finally we use [NotificationManager] `nm` to post the notification
+     * built from `notifBuilder` in the status bar.
      */
     internal fun showInterstitialNotification() {
         // look up the notification manager service
@@ -286,44 +253,43 @@ class IncomingMessage : AppCompatActivity() {
         nm.notify(R.string.imcoming_message_ticker_text, notifBuilder.build())
     }
 
+    /**
+     * Our static constant and static method.
+     */
     companion object {
         /**
          * The id of the primary notification channel
          */
         const val PRIMARY_CHANNEL = "default"
 
-
         /**
          * This method creates an array of Intent objects representing the activity stack for the
          * incoming message details state that the application should be in when launching it from
          * a notification. It is used to Supply a PendingIntent to be sent when the notification is
-         * clicked, both in `IncomingMessage.showAppNotification` as well as in
-         * `IncomingMessageInterstitial.switchToApp`.
+         * clicked, both in [IncomingMessage.showAppNotification] as well as in
+         * [IncomingMessageInterstitial.switchToApp].
          *
-         *
-         * First we create an array to hold 4 Intents `Intent[] intents`. The 0'th `Intent`
-         * in `intents` is set to an `Intent` that can be used to re-launch the root activity
-         * (`ApiDemos`) in its base state. The next `Intent` in `intents` is set to an
-         * `Intent` to launch `ApiDemos` with extra data specifying that `ApiDemos`
+         * First we create an array to hold 4 [Intent]'s `val intents`. The 0'th [Intent]
+         * in `intents` is set to an [Intent] that can be used to re-launch the root activity
+         * (`ApiDemos`) in its base state. The next [Intent] in `intents` is set to an
+         * [Intent] to launch `ApiDemos` with extra data specifying that `ApiDemos`
          * should use "App" as the path when displaying its `ListView` of choices, and
-         * `intents[2]` is set to an `Intent` to launch `ApiDemos` with extra data
+         * `intents[2]` is set to an [Intent] to launch `ApiDemos` with extra data
          * specifying that `ApiDemos` should use "App/Notification" as the path when displaying its
          * `ListView` of choices.
          *
+         * The last [Intent] in `intents` is set to an [Intent] which will launch the `Activity`
+         * [IncomingMessageView], with our [CharSequence] parameter [from] stored as an extra under
+         * the key KEY_FROM ("from"), and our [CharSequence] parameter [msg] stored as an extra
+         * under the key KEY_MESSAGE ("message"). Finally we return `intents` to the callers who use
+         * it as the argument to [startActivities].
          *
-         * The last `Intent` in `intents` is set to an `Intent` which will launch the
-         * `Activity IncomingMessageView`, with our parameter `CharSequence from` stored as
-         * an extra under the key KEY_FROM ("from"), and our parameter `CharSequence msg` stored
-         * as an extra under the key KEY_MESSAGE ("message"). Finally we return `intents` to the
-         * callers who use it as the argument to `startActivities(stack)`.
-         *
-         *
-         * Note: `startActivities(stack)`, will Launch multiple new activities. This is generally
-         * the same as calling startActivity(Intent) for the first Intent in the array, that activity
-         * during its creation calling startActivity(Intent) for the second entry, etc. Note that unlike
-         * that approach, generally none of the activities except the last in the array will be created
-         * at this point, but rather will be created when the user first visits them (due to pressing
-         * back from the activity on top).
+         * Note: [startActivities], will Launch multiple new activities. This is generally the same
+         * as calling `startActivity(Intent)` for the first [Intent] in the array, that activity
+         * during its creation calling `startActivity(Intent)` for the second entry, etc. Note that
+         * unlike that approach, generally none of the activities except the last in the array will
+         * be created at this point, but rather will be created when the user first visits them (due
+         * to pressing back from the activity on top).
          *
          * @param context "this" when called from either `IncomingMessage` or `IncomingMessageInterstitial`,
          * it is the `Context` of the `Activity` it is called from.
@@ -364,5 +330,4 @@ class IncomingMessage : AppCompatActivity() {
             return intents
         }
     }
-
 }
