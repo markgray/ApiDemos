96ba7151eaf439d7acd260335947672472265557
markgray <mark.t.gray@gmail.com>
2019-11-24 07:44:58 -0500

    Edited comments

--- app/src/main/java/com/example/android/apis/content/ReadAsset.kt
+++ app/src/main/java/com/example/android/apis/content/ReadAsset.kt
@@ -25,27 +25,26 @@ import com.example.android.apis.R
 
 import java.io.IOException
 
-// Need the following import to get access to the app resources, since this
-// class is in a sub-package.
 /**
- * Shows how to read a data file contained in the app's apk "assets" directory using getAssets() to
- * get an AssetManager and AssetManager.open(String filename) to open the "file" as an InputStream.
+ * Shows how to read a data file contained in the app's apk "assets" directory using [getAssets] to
+ * get an `AssetManager` and then how to use that `AssetManager`'s method `open(String filename)`
+ * to open the "file" as an `InputStream`.
  */
 class ReadAsset : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
      * `onCreate`, then we set our content view to our layout file R.layout.read_asset.
-     *
-     * Wrapped in a try block intended to catch IOException we open `InputStream is` by
-     * obtaining an AssetManager instance for the application's package and using it to `open`
-     * the asset "read_asset.txt" using ACCESS_STREAMING mode (the `AssetManager.open` method
-     * provides access to files that have been bundled with an application as assets -- that is,
-     * files placed in to the "assets" directory). We set `int size` to an estimate of the
-     * number of bytes that can be read (or skipped over) the input stream `is` without
-     * blocking by the next invocation of a method for this input stream. We use `size` to
-     * allocate `byte[] buffer`, read the entire contents of `is` into `buffer`
-     * and then close `is`. We convert `buffer` to `String text`, locate the
-     * `TextView tv` in our layout with ID R.id.text, and set its text to `text`.
+     * Wrapped in a try block intended to catch [IOException] and rethrow it as a [RuntimeException]
+     * we open an `InputStream` for `val inputStream` by obtaining an `AssetManager` instance for
+     * the application's package and using it to `open` the asset "read_asset.txt" using the mode
+     * ACCESS_STREAMING (the `AssetManager.open` method provides access to files that have been
+     * bundled with an application as assets -- that is, files placed in to the "assets" directory).
+     * We set [Int] variable `val size` to an estimate of the number of bytes that can be read (or
+     * skipped over) the input stream `inputStream` without blocking by the next invocation of a
+     * method for this input stream. We use `size` to allocate a [ByteArray] for `var buffer`, read
+     * the entire contents of `inputStream` into `buffer` and then close `inputStream`. We convert
+     * `buffer` to [String] variable `val text`, locate the [TextView] in our layout with ID R.id.text
+     * to initialize variable `val tv` and set its text to `text`.
      *
      * @param savedInstanceState we do not override `onSaveInstanceState` so do not use.
      */
@@ -53,21 +52,32 @@ class ReadAsset : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.read_asset)
         try {
-            val `is` = assets.open("read_asset.txt")
-            // We guarantee that the available method returns the total
-// size of the asset...  of course, this does mean that a single
-// asset can't be more than 2 gigs.
-            val size = `is`.available()
-            // Read the entire asset into a local byte buffer.
+            val inputStream = assets.open("read_asset.txt")
+            /**
+             * We guarantee that the available method returns the total
+             * size of the asset...  of course, this does mean that a single
+             * asset can't be more than 2 gigs.
+             */
+            val size = inputStream.available()
+            /**
+             * Read the entire asset into a local byte buffer.
+             */
             val buffer = ByteArray(size)
-            `is`.read(buffer)
-            `is`.close()
-            // Convert the buffer into a string.
+            inputStream.read(buffer)
+            inputStream.close()
+            /**
+             * Convert the buffer into a string.
+             */
             val text = String(buffer)
-            // Finally stick the string into the text view.
+            /**
+             * Finally stick the string into the text view.
+             */
             val tv = findViewById<TextView>(R.id.text)
             tv.text = text
-        } catch (e: IOException) { // Should never happen!
+        } catch (e: IOException) {
+            /**
+             * Should never happen!
+             */
             throw RuntimeException(e)
         }
     }
