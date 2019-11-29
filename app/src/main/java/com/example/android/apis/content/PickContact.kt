84b10051a1d9d62cae89a200d50b1d9c93afd28c
markgray <mark.t.gray@gmail.com>
2019-11-29 07:07:37 -0500

    Edited comments

--- app/src/main/java/com/example/android/apis/content/PickContact.kt
+++ app/src/main/java/com/example/android/apis/content/PickContact.kt
@@ -40,50 +40,41 @@ class PickContact : AppCompatActivity() {
      */
     var mToast: Toast? = null
     /**
-     * `ResultDisplayer` instance that launched contact data base query, it is set to "this"
-     * in the `onClick` override of `ResultDisplayer` when the Button using "this"
-     * `ResultDisplayer` as its `OnClickListener` is clicked, and used in our
-     * `onActivityResult` when the `Intent` launched returns a result in order to access
-     * the field `ResultDisplayer.mMsg` to use in the toast.
+     * [ResultDisplayer] instance that launched contact data base query, it is set to *this* in the
+     * `onClick` override of [ResultDisplayer] when the Button using *this* [ResultDisplayer] as its
+     * `OnClickListener` is clicked, and used in our [onActivityResult] when the [Intent] launched
+     * returns a result in order to access the field `ResultDisplayer.mMsg` to use in the toast.
      */
     var mPendingResult: ResultDisplayer? = null
 
     /**
-     * This is a custom `OnClickListener` which is used for each of the `Button`'s in our
-     * UI. It uses its constructor to specify a string to be used as part of the toast which will be
-     * shown by `onActivityResult`, as well as the MIME data type used in the `Intent`
-     * which launches the contact data base query.
+     * This is a custom `OnClickListener` which is used for each of the `Button`'s in our UI. It
+     * uses its constructor to specify a string to be used as part of the toast which will be shown
+     * by `onActivityResult`, as well as the MIME data type used in the [Intent] which launches the
+     * contact data base query.
      */
-    inner class ResultDisplayer
-    /**
-     * Constructor used for each Button in our UI, we save the parameter `String msg` in our
-     * field `String mMsg` and `String mimeType` in `String mMimeType`
-     *
-     * Parameter: msg      String to use as part of the toast displayed when the result of the contact
-     * database query is returned to `onActivityResult`.
-     * Parameter: mimeType MIME data type to use for the `Intent` launching the contact
-     * database query.
-     */(
+    inner class ResultDisplayer(
             /**
-             * String to use as part of the toast displayed when the result of the contact database query
-             * is returned to `onActivityResult`.
+             * String to use as part of the toast displayed when the result of the contact database
+             * query is returned to [onActivityResult].
              */
             var mMsg: String,
             /**
-             * MIME data type to use for the `Intent` launching the contact database query.
+             * MIME data type to use for the [Intent] launching the contact database query.
              */
-            var mMimeType: String) : View.OnClickListener {
+            var mMimeType: String
+    ) : View.OnClickListener {
 
         /**
-         * Called when the Button is clicked that uses this instance of `ResultDisplayer` as
-         * its `OnClickListener`. First we create `Intent intent` with the action
+         * Called when the Button is clicked that uses *this* instance of [ResultDisplayer] as its
+         * `OnClickListener`. First we create [Intent] variable `val intent` with the action
          * ACTION_GET_CONTENT (Allows the user to select a particular kind of data and return it).
          * We set an explicit MIME data type for `intent` to be the contents of our field
-         * `String mMimeType` (which is set for this instance in our constructor), set our
-         * Activity's field `ResultDisplayer mPendingResult` to this so that we can be found
-         * by `onActivityResult` when a result for a query is returned, and finally we use
-         * `intent` to launch an activity for which we would like a result when it finishes.
-         * (Said result will be returned to `onActivityResult` in the sweet by-and-by).
+         * [String] field [mMimeType] (which is set for this instance in our constructor), set our
+         * Activity's [ResultDisplayer] field [mPendingResult] to this so that we can be found by
+         * [onActivityResult] when a result for a query is returned, and finally we use `intent` to
+         * launch an activity for which we would like a result when it finishes. (Said result will
+         * be returned to [onActivityResult] in the sweet by-and-by).
          *
          * @param v View of Button that was clicked
          */
@@ -100,7 +91,7 @@ class PickContact : AppCompatActivity() {
      * Called when the activity is starting. First we call through to our super's implementation of
      * `onCreate`, then we set our content view to our layout file R.layout.pick_contact. We
      * locate each of the four Buttons in our UI and set their `OnClickListener` to an
-     * instance of `ResultDisplayer` using the parameters of the constructor to customize the
+     * instance of [ResultDisplayer] using the parameters of the constructor to customize the
      * behavior when a particular Button is clicked as follows:
      *
      *  * R.id.pick_contact "Selected contact" "vnd.android.cursor.item/contact"
@@ -108,50 +99,53 @@ class PickContact : AppCompatActivity() {
      *  * R.id.pick_phone "Selected phone" "vnd.android.cursor.item/phone_v2"
      *  * R.id.pick_address "Selected address" "vnd.android.cursor.item/postal-address_v2"
      *
-     *
-     * @param savedInstanceState we do not override `onSaveInstanceState` so do not use
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pick_contact)
         // Watch for button clicks.
-        findViewById<View>(R.id.pick_contact).setOnClickListener(ResultDisplayer("Selected contact", CONTACT))
-        findViewById<View>(R.id.pick_person).setOnClickListener(ResultDisplayer("Selected person", PERSON))
-        findViewById<View>(R.id.pick_phone).setOnClickListener(ResultDisplayer("Selected phone", PHONE))
-        findViewById<View>(R.id.pick_address).setOnClickListener(ResultDisplayer("Selected address", POSTAL))
+        findViewById<View>(R.id.pick_contact).setOnClickListener(
+                ResultDisplayer("Selected contact", CONTACT)
+        )
+        findViewById<View>(R.id.pick_person).setOnClickListener(
+                ResultDisplayer("Selected person", PERSON)
+        )
+        findViewById<View>(R.id.pick_phone).setOnClickListener(
+                ResultDisplayer("Selected phone", PHONE)
+        )
+        findViewById<View>(R.id.pick_address).setOnClickListener(
+                ResultDisplayer("Selected address", POSTAL)
+        )
     }
 
     /**
-     * Called when an activity you launched exits, giving you the requestCode you started it with,
-     * the resultCode it returned, and any additional data from it.
+     * Called when an activity you launched exits, giving you the [requestCode] you started it with,
+     * the [resultCode] it returned, and any additional data from it.
      *
+     * If our [Intent] parameter [data] is non-*null*, we retrieve the `Uri` data this intent is
+     * operating on to initialize variable `val uri` . If `uri` is non-*null* we allocate [Cursor]
+     * variable `var c`, setting it to *null*, then wrapped in a try block we use a `ContentResolver`
+     * instance for our application's package to query `uri` for the column containing the unique ID
+     * for a row, saving the [Cursor] returned in `c`. If `c` is now non-*null* and we can successfully
+     * move to the first row of `c` we retrieve the `int` value of column 0 of that row to initialize
+     * variable `val id`. If there has been a toast already toasted using our [Toast] field [mToast]
+     * we cancel it.
      *
-     * If our parameter `Intent data` is non-null, we retrieve the `Uri uri` data this
-     * intent is operating on. If `uri` is non-null we allocate `Cursor c`, setting it
-     * to null, then wrapped in a try block we use a ContentResolver instance for our application's
-     * package to query `uri` for the column containing the unique ID for a row, saving the
-     * `Cursor` returned in `Cursor c`. If `c` is now non-null and we can
-     * successfully move to the first row of `c` we retrieve the `int` value of column
-     * 0 of that row to `int id`. If there has been a toast already toasted using our field
-     * `Toast mToast` we cancel it.
+     * We now create [String] variable `val txt` using our [ResultDisplayer] field [mPendingResult]
+     * to retrieve the value of the [String] field `mMsg` of the [ResultDisplayer] instance whose
+     * result we are receiving after its `Button` was clicked, concatenated with the string value
+     * of `uri` and the value of `id`. We set [Toast] field [mToast] to a toast that uses `txt` as
+     * its text and show it.
      *
+     * The finally block of our *try* then closes the [Cursor] `c` if it is not null.
      *
-     * We now create `String txt` using our field `ResultDisplayer mPendingResult` to
-     * retrieve the value of the field `String mMsg` of the `ResultDisplayer` instance
-     * whose result we are receiving after its Button was clicked, concatenated with the string value
-     * of `uri` and the value of `id`. We set `Toast mToast` to a toast that uses
-     * `txt` as its text and show it.
-     *
-     *
-     * The finally block of our try then closes the `Cursor c` if it is not null.
-     *
-     * @param requestCode The integer request code originally supplied to
-     * startActivityForResult(), allowing you to identify who this
-     * result came from.
-     * @param resultCode  The integer result code returned by the child activity
-     * through its setResult().
-     * @param data        An Intent, which can return result data to the caller
-     * (various data can be attached as Intent "extras").
+     * @param requestCode The integer request code originally supplied to [startActivityForResult],
+     * allowing you to identify who this result came from.
+     * @param resultCode  The integer result code returned by the child activity through its
+     * `setResult()` method.
+     * @param data An [Intent], which can return result data to the caller (various data can be
+     * attached as [Intent] "extras").
      */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -177,6 +171,9 @@ class PickContact : AppCompatActivity() {
         }
     }
 
+    /**
+     * Our static constants
+     */
     companion object {
         /**
          * The MIME type of a CONTENT_URI subdirectory of a single person.
