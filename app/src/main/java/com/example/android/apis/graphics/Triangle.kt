78ead1c8369ad1d1ff296d9884fbddc95cb91867
markgray <mark.t.gray@gmail.com>
2020-01-31 08:42:52 -0500

    Edited comments

--- app/src/main/java/com/example/android/apis/graphics/Triangle.kt
+++ app/src/main/java/com/example/android/apis/graphics/Triangle.kt
@@ -27,17 +27,17 @@ class Triangle {
 
     /**
      * Draws our triangle. First we specify the orientation of front-facing polygons to be GL_CCW
-     * (selects counterclockwise polygons as front-facing). We next define `mFVertexBuffer` to
-     * be our array of vertex data, with 3 coordinates per vertex, using GL_FLOAT as its data type,
-     * and a stride of 0. We enable the server-side GL capability GL_TEXTURE_2D (two-dimensional
-     * texturing is performed), then define `mTexBuffer` to be our array of texture coordinates,
-     * with 2 coordinates per point, using GL_FLOAT as its data type, and a stride of 0.
+     * (selects counterclockwise polygons as front-facing). We next define our [FloatBuffer] field
+     * [mFVertexBuffer] to be our array of vertex data, with 3 coordinates per vertex, using GL_FLOAT
+     * as its data type, and a stride of 0. We enable the server-side GL capability GL_TEXTURE_2D
+     * (two-dimensional texturing is performed), then define our [FloatBuffer] field [mTexBuffer]
+     * to be our array of texture coordinates, with 2 coordinates per point, using GL_FLOAT as its
+     * data type, and a stride of 0.
      *
-     *
-     * Finally we call `glDrawElements` to render primitives from array data specifying
-     * `ShortBuffer mIndexBuffer` as our indices array, GL_UNSIGNED_SHORT as its data type,
-     * and GL_TRIANGLE_STRIP as the type of primitive to render (Every group of 3 adjacent vertices
-     * forms a triangle - we have only one triangle).
+     * Finally we call the [GL10.glDrawElements] method of [gl] to render primitives from array data
+     * specifying our [ShortBuffer] field [mIndexBuffer] as our indices array, GL_UNSIGNED_SHORT as
+     * its data type, and GL_TRIANGLE_STRIP as the type of primitive to render (Every group of 3
+     * adjacent vertices forms a triangle - we have only one triangle).
      *
      * @param gl the GL interface.
      */
@@ -65,24 +65,25 @@ class Triangle {
      * We allocate `ByteBuffer ibb` on the native heap, set its byte order to native byte order
      * and initialize `ShortBuffer mIndexBuffer` with a view of `tbb` as a short buffer.
      *
-     *
      * We define the contents of `float[] coords` to be the coordinates of a unit-sided
      * equilateral triangle centered on the origin. Then we load the 9 entries in `coords[]`
      * multiplied by 2.0 into `FloatBuffer mFVertexBuffer`, and the (x,y) values only
      * multiplied by 2.0 and offset by 0.5 into `FloatBuffer mTexBuffer`. We load
      * `ShortBuffer mIndexBuffer` with the three indices 0, 1, and 2.
      *
-     *
      * Finally we position `mFVertexBuffer`, `mTexBuffer`, and `mIndexBuffer` to
      * their beginning entry ready for use.
      */
-    init { // Buffers to be passed to gl*Pointer() functions
-// must be direct, i.e., they must be placed on the
-// native heap where the garbage collector cannot
-// move them.
-//
-// Buffers with multi-byte data types (e.g., short, int, float)
-// must have their byte order set to native order
+    init {
+        /**
+         * Buffers to be passed to gl*Pointer() functions
+         *  must be direct, i.e., they must be placed on the
+         *  native heap where the garbage collector cannot
+         *  move them.
+         *
+         * Buffers with multi-byte data types (e.g., short, int, float)
+         * must have their byte order set to native order
+         */
         val vbb = ByteBuffer.allocateDirect(VERTS * 3 * 4)
         vbb.order(ByteOrder.nativeOrder())
         mFVertexBuffer = vbb.asFloatBuffer()
