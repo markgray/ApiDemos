a2079870b6c6d39bea5a1eb8f864a296e89a0d44
markgray <mark.t.gray@gmail.com>
2020-02-21 05:36:14 -0500

    Converted MatrixGrabber.java to kotlin

---
+++ app/src/main/java/com/example/android/apis/graphics/spritetext/MatrixGrabber.kt
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.apis.graphics.spritetext
+
+import javax.microedition.khronos.opengles.GL10
+
+/**
+ * Class which fetches and saves the current model view and projection view matrices in its fields.
+ */
+internal class MatrixGrabber {
+    /**
+     * Our copy of the model view matrix
+     */
+    @JvmField
+    var mModelView: FloatArray = FloatArray(16)
+    /**
+     * Our copy of the projection view matrix
+     */
+    @JvmField
+    var mProjection: FloatArray = FloatArray(16)
+
+    /**
+     * Record the current modelView and projection matrix state. Has the side effect of setting the
+     * current matrix state to GL_MODELVIEW -- UNUSED
+     *
+     * @param gl the [GL10] interface
+     */
+    @Suppress("unused")
+    fun getCurrentState(gl: GL10) {
+        getCurrentProjection(gl)
+        getCurrentModelView(gl)
+    }
+
+    /**
+     * Record the current modelView matrix state. Has the side effect of setting the current matrix
+     * state to GL_MODELVIEW. We simply call our method `getMatrix` to read the current model
+     * view matrix into our field `float[] mModelView`
+     *
+     * @param gl the gl interface
+     */
+    fun getCurrentModelView(gl: GL10) {
+        getMatrix(gl, GL10.GL_MODELVIEW, mModelView)
+    }
+
+    /**
+     * Record the current projection matrix state. Has the side effect of setting the current matrix
+     * state to GL_PROJECTION. We simply call our method `getMatrix` to read the current
+     * projection matrix into our field `float[] mProjection`.
+     *
+     * @param gl the gl interface
+     */
+    fun getCurrentProjection(gl: GL10) {
+        getMatrix(gl, GL10.GL_PROJECTION, mProjection)
+    }
+
+    /**
+     * Sets the current matrix to its parameter `int mode`, and reads that matrix into its
+     * parameter `float[] mat`. To do this we cast our parameter `GL10 gl` to
+     * `MatrixTrackingGL gl2`, use it to set the current matrix to `int mode`, and
+     * then use the `gl2` method `getMatrix` to copy that matrix into our parameter
+     * `mat`.
+     *
+     * @param gl   the gl interface
+     * @param mode matrix we are interested in, either GL_MODELVIEW, or GL_PROJECTION
+     * @param mat  `float[]` array to hold the matrix requested
+     */
+    private fun getMatrix(gl: GL10, mode: Int, mat: FloatArray) {
+        val gl2 = gl as MatrixTrackingGL
+        gl2.glMatrixMode(mode)
+        gl2.getMatrix(mat, 0)
+    }
+
+}
