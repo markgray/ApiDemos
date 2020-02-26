d0455d56ba10926de90f3c4dea34009bf9e84df4
markgray <mark.t.gray@gmail.com>
2020-02-26 06:31:56 -0500

    Edited comments

--- app/src/main/java/com/example/android/apis/graphics/Pictures.kt
+++ app/src/main/java/com/example/android/apis/graphics/Pictures.kt
@@ -24,23 +24,25 @@ import android.os.Bundle
 import android.view.View
 
 /**
- * Shows how to use the Picture class to record drawing instructions performed on the Canvas returned
- * by Picture.beginRecording(width, height), then after calling Picture.endRecording() the Picture is
- * turned into a Drawable by calling PictureDrawable(Picture). Then in the onDraw method it first
- * draws the Picture using the Canvas.drawPicture(Picture) method then it stretches the Picture when
- * drawing using the Canvas.drawPicture(Picture,RectF) method, then it draws the Drawable it created
- * with PictureDrawable(Picture) to draw using Drawable.draw(Canvas). Before API 29 it would then
- * write the Picture to a ByteArrayOutputStream and draw it by reading that ByteArrayOutputStream
- * back in and drawing it using Canvas.drawPicture(Picture.createFromStream(is)) but API 29 removed
- * the long deprecated Picture.writeToStream and Picture.createFromStream. The preferred method now
- * is to draw the Picture into a Bitmap.
+ * Shows how to use the [Picture] class to record drawing instructions performed on the [Canvas]
+ * returned by [Picture.beginRecording], then after calling [Picture.endRecording] the Picture is
+ * turned into a [Drawable] by calling [PictureDrawable] with that [Picture]. Then in the
+ * [SampleView.onDraw] method we first draw the [Picture] using the [Canvas.drawPicture] method
+ * then we stretch the [Picture] by drawing using the `Canvas.drawPicture(Picture,RectF)` method,
+ * then we draw the [Drawable] we created using [PictureDrawable] on that [Picture] to draw using
+ * [Drawable.draw] on the [Canvas].
+ *
+ * Before API 29 it would then write the Picture to a `ByteArrayOutputStream` and draw it by reading
+ * that `ByteArrayOutputStream` back in and drawing it using the [Canvas.drawPicture] method that
+ * used `Picture.createFromStream` but API 29 removed the long deprecated `Picture.writeToStream` and
+ * `Picture.createFromStream`. The preferred method now is to draw the [Picture] into a [Bitmap].
  */
 class Pictures : GraphicsActivity() {
     /**
      * Called when the activity is starting. First we call our super's implementation of `onCreate`
-     * then we set our content view to a new instance of `SampleView`.
+     * then we set our content view to a new instance of [SampleView].
      *
-     * @param savedInstanceState we do not override `onSaveInstanceState` so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,44 +50,40 @@ class Pictures : GraphicsActivity() {
     }
 
     /**
-     * Custom view demonstrating some of the features of the `Picture` class.
+     * Custom view demonstrating some of the features of the [Picture] class.
      */
     private class SampleView(context: Context?) : View(context) {
         /**
-         * Our `Picture` consisting of a pink circle (the alpha of 0x88 lets the white background
+         * Our [Picture] consisting of a pink circle (the alpha of 0x88 lets the white background
          * lighten the red circle) with the green text "Pictures" partially obscuring it.
          */
         private val mPicture: Picture
         /**
-         * `PictureDrawable` created out of `Picture mPicture`.
+         * [PictureDrawable] created out of [Picture] field [mPicture].
          */
         private val mDrawable: Drawable
 
         /**
-         * We implement this to do our drawing. First we set the entire `Canvas canvas` to the
-         * color WHITE. Then we draw our `Picture mPicture` 3 different ways:
+         * We implement this to do our drawing. First we set the entire [Canvas] parameter [canvas]
+         * to the color WHITE. Then we draw our [Picture] field [mPicture] 3 different ways:
          *
-         *  *
-         * Using the `canvas` method `drawPicture`
+         *  * Using the [Canvas.drawPicture] method of [canvas]
          *
-         *  *
-         * Using the `canvas` method `drawPicture` and specifying a `RectF`
-         * whose top left corner is at (0,100) and whose bottom right corner is at
-         * (`getWidth()`,200) where `getWidth` returns the width of our view. This
-         * has the effect of moving the `Picture` down 100 pixels and stretching it to
-         * fill our view.
+         *  * Using the [Canvas.drawPicture] method of [canvas] and specifying a [RectF]
+         *  whose top left corner is at (0,100) and whose bottom right corner is at
+         *  (`getWidth()`,200) where [getWidth] returns the width of our view. This
+         *  has the effect of moving the [Picture] down 100 pixels and stretching it to
+         *  fill our view.
          *
-         *  *
-         * Setting the bounds of our `Drawable mDrawable` version of `mPicture` to
-         * left of 0, top of 200, right of `getWidth` and bottom of 300, then using the
-         * `draw` method of `mDrawable` to draw to the `Canvas canvas`
+         *  * Setting the bounds of our [Drawable] field [mDrawable] version of [mPicture] to
+         *  left of 0, top of 200, right of `getWidth` and bottom of 300, then using the
+         *  [Drawable.draw] method of [mDrawable] to draw to the [Canvas] parameter [canvas]
          *
+         * Notice the two different ways the [Picture] is located on the canvas, using
+         * [RectF] to move it down by 100 pixels, and using [Drawable.setBounds] to move
+         * it down by 200 pixels.
          *
-         * Notice the three different ways the `Picture` is located on the canvas, using
-         * `RectF` to move it down by 100 pixels, using `setBounds` to move it down by
-         * 200 pixels, and using `translate` to move it down by 300 pixels.
-         *
-         * @param canvas the canvas on which the background will be drawn
+         * @param canvas the [Canvas] on which the background will be drawn
          */
         @SuppressLint("DrawAllocation")
         override fun onDraw(canvas: Canvas) {
@@ -94,6 +92,7 @@ class Pictures : GraphicsActivity() {
             canvas.drawPicture(mPicture, RectF(0f, 100f, width.toFloat(), 200f))
             mDrawable.setBounds(0, 200, width, 300)
             mDrawable.draw(canvas)
+
             /* ***** ***** ******* writeToStream and createFromStream have been removed in API 29
  *          ByteArrayOutputStream os = new ByteArrayOutputStream();
  *          //noinspection deprecation
@@ -107,15 +106,16 @@ class Pictures : GraphicsActivity() {
 
         companion object {
             /**
-             * Draws a pink circle (the alpha of 0x88 lets the white background lighten the red circle)
-             * with the green text "Pictures" partially obscuring it. First we allocate a new instance of
-             * `Paint` for `Paint p`, with the ANTI_ALIAS_FLAG set. We set the color of `p`
-             * to red with an alpha of 0x88 and use it to draw a 40 pixel radius circle on `Canvas canvas`
-             * with its center at (50,50). We set the color of `p` to GREEN, set its text size to 30,
-             * and use it to draw the text "Pictures" at location (60,60) on `canvas`.
+             * Draws a pink circle (the alpha of 0x88 lets the white background lighten the red
+             * circle) with the green text "Pictures" partially obscuring it. First we allocate
+             * a new instance of [Paint] for [Paint] variable `val p`, with the ANTI_ALIAS_FLAG
+             * set. We set the color of `p` to red with an alpha of 0x88 and use it to draw a 40
+             * pixel radius circle on [Canvas] parameter [canvas] with its center at (50,50). We
+             * set the color of `p` to GREEN, set its text size to 30, and use it to draw the text
+             * "Pictures" at location (60,60) on [canvas].
              *
-             * @param canvas `Canvas` to draw to, it is created by calling the `beginRecording`
-             * method of our field `Picture mPicture`.
+             * @param canvas [Canvas] to draw to, it is created by calling the `beginRecording`
+             * method of our [Picture] field [mPicture].
              */
             fun drawSomething(canvas: Canvas) {
                 val p = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -128,16 +128,12 @@ class Pictures : GraphicsActivity() {
         }
 
         /**
-         * Our constructor. First we call our super's constructor, then we enable our view to receive
-         * focus, and to receive focus in touch mode. We create a new instance of `Picture` for
-         * our field `Picture mPicture`, call its `beginRecording` method specifying a
-         * width of 200, and a height of 100, and pass the `Canvas` returned to our method
-         * `drawSomething`. When `drawSomething` is done drawing to the `mPicture`
-         * canvas we call the `endRecording` method of `mPicture`. Finally we create a
-         * `PictureDrawable` from `mPicture` for our field `Drawable mDrawable`.
-         *
-         * Parameter: context `Context` to use to access resources, "this" when called from the
-         * `onCreate` method of the `Pictures` activity.
+         * The init block of our constructor. We enable our view to receive focus, and to receive
+         * focus in touch mode. We create a new instance of `Picture` for our `Picture` field
+         * `mPicture`, call its `beginRecording` method specifying a width of 200, and a height of
+         * 100, and pass the `Canvas` returned to our method `drawSomething`. When `drawSomething`
+         * is done drawing to the `mPicture` canvas we call the `endRecording` method of `mPicture`.
+         * Finally we create a `PictureDrawable` from `mPicture` for our `Drawable` field `mDrawable`.
          */
         init {
             isFocusable = true
