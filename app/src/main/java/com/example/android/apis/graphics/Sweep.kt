2f167cd760dc2778439f994ce1ae672949383e27
markgray <mark.t.gray@gmail.com>
2020-03-05 05:11:34 -0500

    Edited comments

--- app/src/main/java/com/example/android/apis/graphics/Sweep.kt
+++ app/src/main/java/com/example/android/apis/graphics/Sweep.kt
@@ -28,7 +28,8 @@ import android.view.View
 class Sweep : GraphicsActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * `onCreate`, then we set our content view to a new instance of [SampleView].
+     * `onCreate`, then we set [densityScale] to the logical density of the display. Finally we set
+     * our content view to a new instance of [SampleView].
      *
      * @param savedInstanceState we do not override `onSaveInstanceState` so do not use.
      */
@@ -71,17 +72,18 @@ class Sweep : GraphicsActivity() {
 
         /**
          * We implement this to do our drawing. First we make a local copy of [Paint] field [mPaint]
-         * for `val paint`, initialize `val x` to 160f, and `val y` to 100f. We set the color of the
-         * entire [Canvas] parameter [canvas] to WHITE. We set [Matrix] field [mMatrix] to rotate
-         * by field [mRotate] degrees around the point `(x,y)`, and set the local matrix of [Shader]
-         * field [mShader] to it, then set the shader of [Paint] field [mPaint] to [mShader]. We
-         * increment [mRotate] by 3 degrees and if the result is greater than or equal to 360 we set
-         * it to 0. We then invalidate the view so we will be called again sometime in the future.
+         * for `val paint`, initialize `val x` to 160f times [densityScale] (the logical density of
+         * the display), and `val y` to 100f times [densityScale]. We set the color of the entire
+         * [Canvas] parameter [canvas] to WHITE. We set [Matrix] field [mMatrix] to rotate by field
+         * [mRotate] degrees around the point `(x,y)`, and set the local matrix of [Shader] field
+         * [mShader] to it, then set the shader of [Paint] field [mPaint] to [mShader]. We increment
+         * [mRotate] by 3 degrees and if the result is greater than or equal to 360 we set it to 0.
+         * We then invalidate the view so we will be called again sometime in the future.
          *
          * If our [mDoTiming] flag is true, we set `var now` to the current time in milliseconds
          * since boot, loop drawing our circle 20 times, calculate how long this took and log the
          * result. If [mDoTiming] is false we simply use the [Paint] `paint` to draw a circle with a
-         * radius of 80 pixels around the point (x,y).
+         * radius of 80 times [densityScale] pixels around the point `(x,y)`.
          *
          * @param canvas the [Canvas] on which the background will be drawn
          */
@@ -111,14 +113,14 @@ class Sweep : GraphicsActivity() {
         }
 
         /**
-         * Called when a key down event has occurred. If the key is KEYCODE_D, we toggle the DITHER_FLAG
-         * of our `Paint mPaint`, invalidate the view and return true. If the key is KEYCODE_T,
-         * we toggle our `mDoTiming` flag, invalidate the view and return true. Otherwise we
-         * return whatever our super's implementation of `onKeyDown` returns.
+         * Called when a key down event has occurred. If the key is KEYCODE_D, we toggle the
+         * DITHER_FLAG of our [Paint] field [mPaint], invalidate the view and return true. If
+         * the key is KEYCODE_T, we toggle our [mDoTiming] flag, invalidate the view and return
+         * true. Otherwise we return whatever our super's implementation of `onKeyDown` returns.
          *
          * @param keyCode A key code that represents the button pressed.
-         * @param event   The KeyEvent object that defines the button action.
-         * @return true if we have consumed the `KeyEvent`.
+         * @param event   The [KeyEvent] object that defines the button action.
+         * @return true if we have consumed the [KeyEvent].
          */
         override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
             when (keyCode) {
@@ -145,10 +147,11 @@ class Sweep : GraphicsActivity() {
 
         /**
          * The init block of our constructor. First we enable our view to receive focus, and to
-         * receive focus in touch mode. We initialize `x` to 160 and `y` to 100 and use them to
-         * create a `SweepGradient` to initialize our `Shader` field `mShader` with the center at
-         * (x,y), using the array `COLORS` as the colors to be evenly distributed around the center.
-         * Finally we set `mShader` as the shader of `Paint mPaint`.
+         * receive focus in touch mode. We initialize `x` to 160 times `densityScale` and `y` to
+         * 100 times `densityScale` and use them to create a `SweepGradient` to initialize our
+         * `Shader` field `mShader` with the center at `(x,y)`, using the array `COLORS` as the
+         * colors to be evenly distributed around the center. Finally we set `mShader` as the
+         * shader of `Paint` field `mPaint`.
          */
         init {
             isFocusable = true
@@ -161,6 +164,10 @@ class Sweep : GraphicsActivity() {
     }
 
     companion object {
+        /**
+         * The logical density of the display, set in our [onCreate] method from the current display
+         * metrics `density` field, and used to scale DP values to pixels where needed.
+         */
         var densityScale = 1f
     }
 }
