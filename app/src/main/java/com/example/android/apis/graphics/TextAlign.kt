84b749fd2f91f0a5963537a7a04f85a30a0393e3
markgray <mark.t.gray@gmail.com>
2020-03-05 06:38:25 -0500

    Edited comments

--- app/src/main/java/com/example/android/apis/graphics/TextAlign.kt
+++ app/src/main/java/com/example/android/apis/graphics/TextAlign.kt
@@ -21,15 +21,15 @@ import android.os.Bundle
 import android.view.View
 
 /**
- * Shows how to position text drawn to a Canvas using Paint.setTextAlign, Canvas.drawPosText, and
- * along an arbitrary path using Canvas.drawTextOnPath
+ * Shows how to position text drawn to a Canvas using [Paint.setTextAlign], [Canvas.drawPosText],
+ * and along an arbitrary path using [Canvas.drawTextOnPath]
  */
 class TextAlign : GraphicsActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * `onCreate`, then we set our content view to a new instance of `SampleView`.
+     * `onCreate`, then we set our content view to a new instance of [SampleView].
      *
-     * @param savedInstanceState we do not override `onSaveInstanceState` so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +41,7 @@ class TextAlign : GraphicsActivity() {
      */
     private class SampleView(context: Context?) : View(context) {
         /**
-         * `Paint` we use to draw our text and some spacing lines with.
+         * [Paint] we use to draw our text and some spacing lines with.
          */
         private val mPaint: Paint
 
@@ -56,36 +56,34 @@ class TextAlign : GraphicsActivity() {
         private val mPos: FloatArray
 
         /**
-         * `Path` we use for our "Along a path" text sample.
+         * [Path] we use for our "Along a path" text sample.
          */
         private val mPath: Path
 
         /**
-         * `Paint` we use to draw the `Path` under our "Along a path" text sample.
+         * [Paint] we use to draw the [Path] under our "Along a path" text sample.
          */
         private val mPathPaint: Paint
 
         /**
          * Creates and returns an array of x, y coordinates for drawing each individual character in
-         * the parameter `String text`. First we allocate an entry for every character in
-         * `text` in `float[] widths`. Then we fill it with the widths of all of the
-         * characters in `text`, saving the number of code units in the text in `int n`.
-         * We allocate for `float[] pos` an array of `2*n` `float` objects.
+         * the [String] parameter [text]. First we allocate an entry for every character in [text]
+         * in [FloatArray] `val widths`. Then we fill it with the widths of all of the characters in
+         * [text] using the [Paint.getTextWidths] method of our [Paint] parameter [paint], saving
+         * the number of code units in the text in `val n`. We allocate an array of `2*n` [Float]
+         * objects for [FloatArray] `val pos`.
          *
-         *
-         * Then initializing `accumulatedX` to 0, we loop through all `n` values in
-         * `widths` setting two entries in `pos` for the coordinates of each character,
-         * the first value being `accumulatedX`, and the second our parameter `y`. We
-         * then add the entry in `widths` for this character to `accumulatedX` and loop
-         * back for the next character.
-         *
+         * Then initializing `var accumulatedX` to 0, we loop through all `n` values in `widths`
+         * setting two entries in `pos` for the coordinates of each character, the first value
+         * being `accumulatedX`, and the second our parameter [y]. We then add the entry in `widths`
+         * for this character to `accumulatedX` and loop back for the next character.
          *
          * Finally we return `pos` to the caller.
          *
-         * @param text  String whose characters we are to determine the position of.
+         * @param text  [String] whose characters we are to determine the position of.
          * @param y     y coordinate position for all the characters.
-         * @param paint `Paint` to use to get the text widths of the characters in `text`
-         * @return An array of x,y coordinates for the position of each character in `text`
+         * @param paint [Paint] to use to get the text widths of the characters in [text]
+         * @return An array of x,y coordinates for the position of each character in [text]
          * when is is drawn.
          */
         @Suppress("SameParameterValue")
@@ -105,55 +103,49 @@ class TextAlign : GraphicsActivity() {
         }
 
         /**
-         * We implement this to do our drawing. First we draw the entire `Canvas canvas` WHITE.
-         * We make a copy of the `Paint mPaint` pointer for `Paint p`, copy `mX` to
-         * `float x`, set `y` to 0, and make a copy of the `float[] mPos` pointer in
-         * `float[] pos`.
+         * We implement this to do our drawing. First we draw the entire [Canvas] parameter [canvas]
+         * WHITE. We make a copy of the [Paint] field [mPaint] for `val p`, copy the [Float] field
+         * [mX]  to `val x`, set `val y` to 0, and make a copy of the [FloatArray] field [mPos]
+         * pointer in `val pos`.
          *
-         *
-         * We set the color of `p` to red and use it to draw a line from (x,y) to (x,y+DY*3)
+         * We set the color of `p` to red and use it to draw a line from `(x,y)` to `(x,y+DY*3)`
          * (this is a line showing the center of the aligned text we are about to draw). We now set
-         * the color of `p` to BLACK, translate the `canvas` down `DY` pixels, set
-         * the text alignment of `p` to LEFT, and use it to draw the text TEXT_L ("Left") at
-         * location (x,y). We move the `canvas` down `DY` pixels, set the text alignment
-         * of `p` to CENTER, and use it to draw the text TEXT_C ("Center") at location (x,y).
-         * We move the `canvas` down `DY` pixels, set the text alignment of `p` to
-         * RIGHT, and use it to draw the text TEXT_R ("Right") at location (x,y).
+         * the color of `p` to BLACK, translate the [canvas] down [DY] pixels, set the text alignment
+         * of `p` to LEFT, and use it to draw the text TEXT_L ("Left") at location `(x,y)`. We move the
+         * [canvas] down [DY] pixels, set the text alignment of `p` to CENTER, and use it to draw the
+         * text TEXT_C ("Center") at location `(x,y)`. We move the [canvas] down [DY] pixels, set the
+         * text alignment of `p` to RIGHT, and use it to draw the text TEXT_R ("Right") at location
+         * `(x,y)`.
          *
-         *
-         * Now we move on to the positioned text, first moving the `Canvas canvas` to the
-         * location (100,DY*2), and setting the color of `p` to green. We loop through the
-         * coordinates in `pos` drawing a vertical line between one `DY` above to two
-         * `DY` below the (x,y) coordinates contained in `pos`.
-         *
+         * Now we move on to the positioned text, first moving the [Canvas] parameter [canvas] to
+         * the location `(100,DY*2)`, and setting the color of `p` to green. We loop through the
+         * coordinates in `pos` drawing a vertical line between one [DY] above to two [DY] below
+         * the `(x,y)` coordinates contained in `pos`.
          *
          * Then we set the color of `p` to BLACK, its text alignment to LEFT, and use the method
-         * `canvas.drawPosText` to draw the text `POSTEXT` ("Positioned"), using `pos`
-         * as the location of each character, and the `Paint p` as the `Paint`. We move the
-         * `Canvas canvas` down by `DY`, set the text alignment of `p` to to CENTER,
-         * and use the method `canvas.drawPosText` to draw the text `POSTEXT` ("Positioned"),
-         * using `pos` as the location of each character, and the `Paint p` as the `Paint`.
-         * Finally we move the `Canvas canvas` down by `DY`, set the text alignment of `p`
-         * to to RIGHT, and use the method `canvas.drawPosText` to draw the text `POSTEXT`
-         * ("Positioned"), using `pos` as the location of each character, and the `Paint p`
-         * as the `Paint`.
+         * `canvas.drawPosText` to draw the text [POSTEXT] ("Positioned"), using `pos` as the
+         * location of each character, and the [Paint] `p` as the [Paint]. We move [canvas] down
+         * by [DY], set the text alignment of `p` to to CENTER, and use the method `canvas.drawPosText`
+         * to draw the text [POSTEXT] ("Positioned"), using `pos` as the location of each character,
+         * and the [Paint] `p` as the [Paint]. Finally we move [canvas] down by [DY], set the text
+         * alignment of `p` to RIGHT, and use the method `canvas.drawPosText` to draw the text
+         * [POSTEXT] ("Positioned"), using `pos` as the location of each character, and the [Paint]
+         * `p` as the [Paint].
          *
+         * We move the `Canvas canvas` to (-100,DY*2) (relative to its last position of course), and
+         * draw our [Path] field [mPath] using the [Paint] field [mPathPaint]. We set the text
+         * alignment of `p` to LEFT, and use the method `canvas.drawTextOnPath` to draw the text
+         * [TEXTONPATH] ("Along a path") along the [Path] field [mPath] using `p` as the [Paint].
+         * We move [canvas] down by `DY*1.5` pixels, and draw our [Path] field [mPath] using the
+         * [Paint] field [mPathPaint]. We set the text alignment of `p` to CENTER, and use the
+         * method `canvas.drawTextOnPath` to draw the text [TEXTONPATH] ("Along a path") along the
+         * [Path] field [mPath] using `p` as the [Paint]. And finally we move [canvas] down by
+         * `DY*1.5` pixels, and draw  our [Path] field [mPath] using the [Paint] field [mPathPaint].
+         * We set the text alignment of `p` to RIGHT, and use the method `canvas.drawTextOnPath` to
+         * draw the text [TEXTONPATH] ("Along a path") along [Path] field [mPath] using `p` as the
+         * [Paint].
          *
-         * We move the `Canvas canvas` to (-100,DY*2) (relative to its last position of course),
-         * and draw our `Path mPath` using the `Paint mPathPaint`. We set the text alignment
-         * of `p` to LEFT, and use the method `canvas.drawTextOnPath` to draw the text
-         * TEXTONPATH ("Along a path") along the `Path mPath` using `Paint p` as the
-         * `Paint`. We move the `Canvas canvas` down by `DY*1.5` pixels, and draw
-         * our `Path mPath` using the `Paint mPathPaint`. We set the text alignment of
-         * `p` to CENTER, and use the method `canvas.drawTextOnPath` to draw the text
-         * TEXTONPATH ("Along a path") along the `Path mPath` using `Paint p` as the
-         * `Paint`. And finally we move the `Canvas canvas` down by `DY*1.5` pixels,
-         * and draw  our `Path mPath` using the `Paint mPathPaint`. We set the text alignment
-         * of `p` to RIGHT, and use the method `canvas.drawTextOnPath` to draw the text
-         * TEXTONPATH ("Along a path") along the `Path mPath` using `Paint p` as the
-         * `Paint`.
-         *
-         * @param canvas the canvas on which the background will be drawn
+         * @param canvas the [Canvas] on which the background will be drawn
          */
         override fun onDraw(canvas: Canvas) {
             canvas.drawColor(Color.WHITE)
@@ -212,10 +204,10 @@ class TextAlign : GraphicsActivity() {
         }
 
         /**
-         * This is called during layout when the size of this view has changed. If you were just added
-         * to the view hierarchy, you're called with the old values of 0. First we call through to our
-         * super's implementation of `onSizeChanged`, then we initialize our field `mX` to
-         * half of the new width of the view `int w`.
+         * This is called during layout when the size of this view has changed. If you were just
+         * added to the view hierarchy, you're called with the old values of 0. First we call
+         * through to our super's implementation of `onSizeChanged`, then we initialize our field
+         * [mX] to half of the new width of the view as given by [Int] parameter [w].
          *
          * @param w  Current width of this view.
          * @param h  Current height of this view.
@@ -259,10 +251,10 @@ class TextAlign : GraphicsActivity() {
             private const val TEXTONPATH = "Along a path"
 
             /**
-             * Fills its parameter `Path p` with a cubic bezier from (10,0) to (300,0) using the
+             * Fills its [Path] parameter [p] with a cubic bezier from (10,0) to (300,0) using the
              * control points (100,-50) and (200,50).
              *
-             * @param p `Path` we are to initialize.
+             * @param p [Path] we are to initialize.
              */
             private fun makePath(p: Path) {
                 p.moveTo(10f, 0f)
@@ -271,18 +263,14 @@ class TextAlign : GraphicsActivity() {
         }
 
         /**
-         * Our constructor. First we call our super's constructor, and then enable our view to receive
-         * focus. We initialize our field `Paint mPaint` with a new instance of `Paint`,
-         * set its anti alias flag to true, its text size to 30, and its typeface to SERIF. We initialize
-         * our field `float[] mPos` with the character positions that the characters in the
-         * string POSTEXT ("Positioned") when it is drawn. We allocate a new instance of `Path`
-         * for `Path mPath` and call our method `makePath` to fill `mPath` with a
-         * cubic bezier from (10,0) to (300,0). Then we allocate a new instance of `Paint` for
-         * `Paint mPathPaint`, set its anti alias flag to true, set its color to blue (with an
-         * alpha of 0x80), and set its style to STROKE.
-         *
-         * context `Content` to access resources, "this" in the `onCreate` method
-         * of `TextAlign`.
+         * The init block of our constructor. First we enable our view to receive focus. We initialize
+         * our `Paint` field `mPaint` with a new instance of `Paint`, set its anti alias flag to true,
+         * its text size to 30, and its typeface to SERIF. We initialize our `FloatArray` field `mPos`
+         * with the character positions that the characters in the string POSTEXT ("Positioned") are
+         * at when it is drawn. We allocate a new instance of `Path` for our field `mPath` and call
+         * our method `makePath` to fill `mPath` with a cubic bezier from (10,0) to (300,0). Then we
+         * allocate a new instance of `Paint` for our field `mPathPaint`, set its anti alias flag to
+         * true, set its color to blue (with an alpha of 0x80), and set its style to STROKE.
          */
         init {
             isFocusable = true
