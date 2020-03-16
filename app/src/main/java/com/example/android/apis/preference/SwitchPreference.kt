059c2e628df27f4a15f8e16dfeb60d7ea1dcf5d1
markgray <mark.t.gray@gmail.com>
2020-03-16 06:33:15 -0400

    Converted SwitchPreference.java to kotlin

--- app/src/main/java/com/example/android/apis/preference/SwitchPreference.kt
+++ app/src/main/java/com/example/android/apis/preference/SwitchPreference.kt
@@ -13,41 +13,45 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+@file:Suppress("DEPRECATION")
 
-package com.example.android.apis.preference;
+package com.example.android.apis.preference
 
-import com.example.android.apis.R;
-
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceManager;
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.preference.PreferenceActivity
+import android.preference.PreferenceManager
+import com.example.android.apis.R
 
 /**
  * Shows how to use android.preference.SwitchPreference
  */
 @SuppressLint("ExportedPreferenceActivity")
-@SuppressWarnings("deprecation")
-public class SwitchPreference extends PreferenceActivity {
+class SwitchPreference : PreferenceActivity() {
     /**
-     * Called when the {@code PreferenceActivity} is starting. First we call through to our super's
-     * implementation of {@code onCreate}. Next we set the default values for our preference file
+     * Called when the `PreferenceActivity` is starting. First we call through to our super's
+     * implementation of `onCreate`. Next we set the default values for our preference file
      * named "switch" by reading the default values from the XML preference file R.xml.default_values
      * using the values defined by each Preference item's android:defaultValue attribute (but only if
      * this is the first time we have been run). We set the name of the SharedPreferences file that
      * preferences we manage will use to "switch", and then inflate the XML resource R.xml.preference_switch
      * and add its preference hierarchy to the current preference hierarchy.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override `onSaveInstanceState` so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        PreferenceManager.setDefaultValues(this, "switch", MODE_PRIVATE, R.xml.default_values, false);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        PreferenceManager.setDefaultValues(
+                this,
+                "switch",
+                Context.MODE_PRIVATE,
+                R.xml.default_values,
+                false
+        )
 
         // Load the preferences from an XML resource
-        getPreferenceManager().setSharedPreferencesName("switch");
-        addPreferencesFromResource(R.xml.preference_switch);
+        preferenceManager.sharedPreferencesName = "switch"
+        addPreferencesFromResource(R.xml.preference_switch)
     }
 }
