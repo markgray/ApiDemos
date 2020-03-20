7924ae492fb6bd63d9e5a22d0d1d7fb454d04fd2
markgray <mark.t.gray@gmail.com>
2020-03-20 06:19:41 -0400

    Edited comments

--- app/src/main/java/com/example/android/apis/view/Animation2.kt
+++ app/src/main/java/com/example/android/apis/view/Animation2.kt
@@ -17,22 +17,21 @@ package com.example.android.apis.view
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.AdapterView
+import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.ViewFlipper
 import androidx.appcompat.app.AppCompatActivity
-import com.example.android.apis.R
 
 // Need the following import to get access to the app resources, since this
 // class is in a sub-package.
+import com.example.android.apis.R
+
 /**
- * Shows how to use the four different animations available for a ViewFlipper: "Push up", "Push left",
- * "Cross fade", and "Hyperspace". A ViewFlipper is a Simple ViewAnimator that will animate between
- * two or more views that have been added to it. Only one child is shown at a time. If requested, it
- * can automatically flip between each child at a regular interval.
+ * Shows how to use the four different animations available for a [ViewFlipper]: "Push up", "Push
+ * left", "Cross fade", and "Hyperspace". A [ViewFlipper] is a simple [ViewAnimator] that will
+ * animate between two or more views that have been added to it. Only one child is shown at a time.
+ * If requested, it can automatically flip between each child at a regular interval.
  */
 class Animation2 : AppCompatActivity(), OnItemSelectedListener {
     /**
@@ -41,7 +40,7 @@ class Animation2 : AppCompatActivity(), OnItemSelectedListener {
     private var mFlipper: ViewFlipper? = null
 
     /**
-     * Strings used for the `Adapter` used by the `Spinner` with ID R.id.spinner
+     * Strings used for the [Adapter] used by the [Spinner] with ID R.id.spinner
      */
     private val mStrings = arrayOf(
             "Push up", "Push left", "Cross fade", "Hyperspace"
@@ -50,22 +49,20 @@ class Animation2 : AppCompatActivity(), OnItemSelectedListener {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
      * `onCreate`, then we set our content view to our layout file R.layout.animation_2. We
-     * initialize our field `ViewFlipper mFlipper` by locating the `ViewFlipper` with ID
-     * R.id.flipper in our layout file, and instruct it to "start flipping" (starts a timer for it to
-     * cycle through its child views).
+     * initialize our [ViewFlipper] field [mFlipper] by locating the [ViewFlipper] with ID
+     * R.id.flipper in our layout file, and instruct it to "start flipping" (starts a timer for
+     * it to cycle through its child views).
      *
-     *
-     * Next we set `Spinner s` by locating the view with ID R.id.spinner, then we create
-     * `ArrayAdapter<String> adapter` from our field `String[] mStrings` using the
+     * Next we set [Spinner] `val s` by locating the view with ID R.id.spinner, and we create
+     * `ArrayAdapter<String>` `val adapter` from our array of [String]'s field [mStrings] using the
      * android.R.layout.simple_spinner_item as the resource ID for a layout file containing a
-     * TextView to use when instantiating views, and set android.R.layout.simple_spinner_dropdown_item
+     * [TextView] to use when instantiating views, and set android.R.layout.simple_spinner_dropdown_item
      * as the layout resource defining the drop down views.
      *
+     * We then set `adapter` as the [Adapter] for [Spinner] `s`, and set "this" as the
+     * [OnItemSelectedListener] for `s`.
      *
-     * We then set `adapter` as the adapter for `Spinner s`, and set "this" as the
-     * `OnItemSelectedListener` for `s`.
-     *
-     * @param savedInstanceState we do not override `onSaveInstanceState` so do not use
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use
      */
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,78 +78,67 @@ class Animation2 : AppCompatActivity(), OnItemSelectedListener {
     }
 
     /**
-     * Callback method to be invoked when an item in the `Spinner` with ID R.id.spinner has been
-     * selected. We switch base on the value of our parameter `position`:
+     * Callback method to be invoked when an item in the [Spinner] with ID R.id.spinner has been
+     * selected. We switch base on the value of our parameter [position]:
      *
-     *  *
-     * 0 - "Push up" we set the in animation of `ViewFlipper mFlipper` to the `Animation`
-     * loaded from R.anim.push_left_in, and its out animation to one loaded from R.anim.push_left_out
+     *  * 0 - "Push up" we set the "in" animation of [ViewFlipper] field [mFlipper] to the [Animation]
+     *  loaded from R.anim.push_left_in, and its "out" animation to one loaded from R.anim.push_left_out
      *
-     *  *
-     * 1 - "Push left" we set the in animation of `ViewFlipper mFlipper` to the `Animation`
-     * loaded from R.anim.push_up_in, and its out animation to one loaded from R.anim.push_up_out
+     *  * 1 - "Push left" we set the "in" animation of [ViewFlipper] field [mFlipper] to the [Animation]
+     *  loaded from R.anim.push_up_in, and its "out"" animation to one loaded from R.anim.push_up_out
      *
-     *  *
-     * 2 - "Cross fade" we set the in animation of `ViewFlipper mFlipper` to the `Animation`
-     * loaded from android.R.anim.fade_in, and its out animation to one loaded from android.R.anim.fade_out
+     *  * 2 - "Cross fade" we set the "in"" animation of [ViewFlipper] field [mFlipper] to the [Animation]
+     *  loaded from android.R.anim.fade_in, and its "out" animation to one loaded from
+     *  android.R.anim.fade_out
      *
-     *  *
-     * default - "Hyperspace" we set the in animation of `ViewFlipper mFlipper` to the `Animation`
-     * loaded from R.anim.hyperspace_in, and its out animation to one loaded from R.anim.hyperspace_out
+     *  * default - "Hyperspace" we set the "in" animation of [ViewFlipper] field [mFlipper] to the
+     *  [Animation] loaded from R.anim.hyperspace_in, and its "out" animation to one loaded from
+     *  R.anim.hyperspace_out
      *
      *
      * The eight different animation resource files contain XML elements for performing the animations:
      *
-     *  *
-     * R.anim.push_up_in - contains a `<translate>` for y from 100p to 0p with a duration
-     * of 300 milliseconds, and an `<alpha>` from 0.0 to 1.0 also with a duration of 300
-     * milliseconds.
+     *  * R.anim.push_up_in - contains a `<translate>` for y from 100p to 0p with a duration
+     *  of 300 milliseconds, and an `<alpha>` from 0.0 to 1.0 also with a duration of 300
+     *  milliseconds.
      *
-     *  *
-     * R.anim.push_up_out - contains a `<translate>` for y from 0p to -100p with a duration
-     * of 300 milliseconds, and an `<alpha>` from 1.0 to 0.0 also with a duration of 300
-     * milliseconds.
+     *  * R.anim.push_up_out - contains a `<translate>` for y from 0p to -100p with a duration
+     *  of 300 milliseconds, and an `<alpha>` from 1.0 to 0.0 also with a duration of 300
+     *  milliseconds.
      *
-     *  *
-     * R.anim.push_left_in - contains a `<translate>` for x from 100p to 0p with a duration
-     * of 300 milliseconds, and an `<alpha>` from 0.0 to 1.0 also with a duration of 300
-     * milliseconds.
+     *  * R.anim.push_left_in - contains a `<translate>` for x from 100p to 0p with a duration
+     *  of 300 milliseconds, and an `<alpha>` from 0.0 to 1.0 also with a duration of 300
+     *  milliseconds.
      *
-     *  *
-     * R.anim.push_left_out - contains a `<translate>` for x from 0p to -100p with a duration
-     * of 300 milliseconds, and an `<alpha>` from 1.0 to 0.0 also with a duration of 300
-     * milliseconds.
+     *  * R.anim.push_left_out - contains a `<translate>` for x from 0p to -100p with a duration
+     *  of 300 milliseconds, and an `<alpha>` from 1.0 to 0.0 also with a duration of 300
+     *  milliseconds.
      *
-     *  *
-     * android.R.anim.fade_in - contains an `<alpha>` from 0.0 to 1.0 using an interpolator
-     * of android:interpolator="@interpolator/decelerate_quad", and a duration of config_longAnimTime
-     * (500 milliseconds).
+     *  * android.R.anim.fade_in - contains an `<alpha>` from 0.0 to 1.0 using an interpolator
+     *  of android:interpolator="@interpolator/decelerate_quad", and a duration of config_longAnimTime
+     *  (500 milliseconds).
      *
-     *  *
-     * android.R.anim.fade_out - contains an `<alpha>` from 1.0 to 0.0 using an interpolator
-     * of android:interpolator="@interpolator/accelerate_quad", and a duration of config_mediumAnimTime
-     * (400 milliseconds).
+     *  * android.R.anim.fade_out - contains an `<alpha>` from 1.0 to 0.0 using an interpolator
+     *  of android:interpolator="@interpolator/accelerate_quad", and a duration of config_mediumAnimTime
+     *  (400 milliseconds).
      *
-     *  *
-     * R.anim.hyperspace_in - contains an `<alpha>` from 0.0 to 1.0, with a duration of 300
-     * milliseconds and an start offset of 1200 milliseconds
+     *  * R.anim.hyperspace_in - contains an `<alpha>` from 0.0 to 1.0, with a duration of 300
+     *  milliseconds and an start offset of 1200 milliseconds
      *
+     *  * R.anim.hyperspace_out - contains a `<scale>` that scales x from 1.0 to 1.4, y from
+     *  1.0 to 0.6, android:pivotX="50%", android:pivotY="50%", android:fillEnabled="true",
+     *  android:fillAfter="false" and a duration of 700 milliseconds. This is followed by a
+     *  `<set>` which contains a `<scale>` and a `<rotate>`, both with a start
+     *  offset of 700 milliseconds. The `<set>` uses accelerate_interpolator as its interpolator,
+     *  the `<scale>` scales x from 1.4 to 0.0, y from 0.6 to 0, android:pivotX="50%",
+     *  android:pivotY="50%", android:fillEnabled="true", android:fillBefore="false",
+     *  android:fillAfter="true" and a duration of 400 milliseconds, the `<rotate>` rotates
+     *  from 0 degrees to -45 degrees, with android:toYScale="0.0", android:pivotX="50%",
+     *  android:pivotY="50%", android:fillEnabled="true", android:fillBefore="false",
+     *  android:fillAfter="true" and a duration of 400 milliseconds.
      *
-     * R.anim.hyperspace_out - contains a `<scale>` that scales x from 1.0 to 1.4, y from
-     * 1.0 to 0.6, android:pivotX="50%", android:pivotY="50%", android:fillEnabled="true",
-     * android:fillAfter="false" and a duration of 700 milliseconds. This is followed by a
-     * `<set>` which contains a `<scale>` and a `<rotate>`, both with a start
-     * offset of 700 milliseconds. The `<set>` uses accelerate_interpolator as its interpolator,
-     * the `<scale>` scales x from 1.4 to 0.0, y from 0.6 to 0, android:pivotX="50%",
-     * android:pivotY="50%", android:fillEnabled="true", android:fillBefore="false", android:fillAfter="true"
-     * and a duration of 400 milliseconds, the `<rotate>` rotates from 0 degrees to -45 degrees,
-     * with android:toYScale="0.0", android:pivotX="50%", android:pivotY="50%", android:fillEnabled="true",
-     * android:fillBefore="false", android:fillAfter="true" and a duration of 400 milliseconds.
-     *
-     *
-     *
-     * @param parent   The AdapterView where the selection happened
-     * @param v        The view within the AdapterView that was clicked
+     * @param parent   The [AdapterView] where the selection happened
+     * @param v        The [View] within the AdapterView that was clicked
      * @param position The position of the view in the adapter
      * @param id       The row id of the item that is selected
      */
@@ -178,7 +164,7 @@ class Animation2 : AppCompatActivity(), OnItemSelectedListener {
     }
 
     /**
-     * Callback method to be invoked when the selection disappears from this `Spinner`. We ignore.
+     * Callback method to be invoked when the selection disappears from this [Spinner]. We ignore.
      *
      * @param parent The AdapterView that now contains no selected item.
      */
