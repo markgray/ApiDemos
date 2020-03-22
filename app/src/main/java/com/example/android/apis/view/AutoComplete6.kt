ebd64824e0a05b950c3b0d012f9b29b84a5cd9af
markgray <mark.t.gray@gmail.com>
2020-03-22 06:31:46 -0400

    Edited comments

--- app/src/main/java/com/example/android/apis/view/AutoComplete6.kt
+++ app/src/main/java/com/example/android/apis/view/AutoComplete6.kt
@@ -23,29 +23,32 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.android.apis.R
 
 /**
- * Shows how to use a MultiAutoCompleteTextView to show completion suggestions
+ * Shows how to use a [MultiAutoCompleteTextView] to show completion suggestions
  * for the substring of the text where the user is typing. Adds a comma at the
- * end of every selected suggestion and MultiAutoCompleteTextView.CommaTokenizer
- * uses that to separate the words for the MultiAutoCompleteTextView.
+ * end of every selected suggestion and [MultiAutoCompleteTextView.CommaTokenizer]
+ * uses that to separate the words for the [MultiAutoCompleteTextView].
  */
 class AutoComplete6 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
      * `onCreate`, then we set our content view to our layout file R.layout.autocomplete_6.
-     * We create `ArrayAdapter<String> adapter` using our array `COUNTRIES` as the data
+     * We create `ArrayAdapter<String>` `val adapter` using our array [COUNTRIES] as the data
      * and android.R.layout.simple_dropdown_item_1line as the resource ID for the layout file which
-     * contains a TextView to use when instantiating views. We set `MultiAutoCompleteTextView textView`
-     * by finding the view with ID R.id.edit, and set its adapter to `adapter`. Finally we set
-     * the Tokenizer of `textView` that will be used to determine the relevant range of the text
-     * where the user is typing to a new instance of `MultiAutoCompleteTextView.CommaTokenizer`.
+     * contains a `TextView` to use when instantiating views. We set [MultiAutoCompleteTextView]
+     * `val textView` by finding the view with ID R.id.edit, and set its adapter to `adapter`.
+     * Finally we set the `Tokenizer` of `textView` that will be used to determine the relevant
+     * range of the text where the user is typing to a new instance of [CommaTokenizer].
      *
-     * @param savedInstanceState we do not override `onSaveInstanceState` so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.autocomplete_6)
-        val adapter = ArrayAdapter(this,
-                android.R.layout.simple_dropdown_item_1line, COUNTRIES)
+        val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_dropdown_item_1line,
+                COUNTRIES
+        )
         val textView = findViewById<MultiAutoCompleteTextView>(R.id.edit)
         textView.setAdapter(adapter)
         textView.setTokenizer(CommaTokenizer())
@@ -58,41 +61,44 @@ class AutoComplete6 : AppCompatActivity() {
                 "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan",
                 "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
                 "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
-                "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory",
-                "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi",
-                "Cote d'Ivoire", "Cambodia", "Cameroon", "Canada", "Cape Verde",
-                "Cayman Islands", "Central African Republic", "Chad", "Chile", "China",
+                "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil",
+                "British Indian Ocean Territory", "British Virgin Islands", "Brunei", "Bulgaria",
+                "Burkina Faso", "Burundi", "Cote d'Ivoire", "Cambodia", "Cameroon", "Canada",
+                "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China",
                 "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo",
                 "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic",
-                "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
-                "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea",
-                "Estonia", "Ethiopia", "Faeroe Islands", "Falkland Islands", "Fiji", "Finland",
-                "Former Yugoslav Republic of Macedonia", "France", "French Guiana", "French Polynesia",
-                "French Southern Territories", "Gabon", "Georgia", "Germany", "Ghana", "Gibraltar",
-                "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau",
-                "Guyana", "Haiti", "Heard Island and McDonald Islands", "Honduras", "Hong Kong", "Hungary",
-                "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica",
-                "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos",
-                "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
-                "Macau", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands",
-                "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia", "Moldova",
+                "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica",
+                "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador",
+                "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Faeroe Islands",
+                "Falkland Islands", "Fiji", "Finland", "Former Yugoslav Republic of Macedonia",
+                "France", "French Guiana", "French Polynesia", "French Southern Territories",
+                "Gabon", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland",
+                "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana",
+                "Haiti", "Heard Island and McDonald Islands", "Honduras", "Hong Kong", "Hungary",
+                "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy",
+                "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait",
+                "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya",
+                "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Madagascar", "Malawi",
+                "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique",
+                "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia", "Moldova",
                 "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia",
-                "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand",
-                "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "North Korea", "Northern Marianas",
-                "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru",
-                "Philippines", "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar",
-                "Reunion", "Romania", "Russia", "Rwanda", "Sqo Tome and Principe", "Saint Helena",
-                "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon",
-                "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Saudi Arabia", "Senegal",
-                "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands",
-                "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "South Korea",
-                "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden",
-                "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "The Bahamas",
-                "The Gambia", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey",
-                "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Virgin Islands", "Uganda",
-                "Ukraine", "United Arab Emirates", "United Kingdom",
-                "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan",
-                "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Wallis and Futuna", "Western Sahara",
+                "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia",
+                "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island",
+                "North Korea", "Northern Marianas", "Norway", "Oman", "Pakistan", "Palau",
+                "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn Islands",
+                "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia",
+                "Rwanda", "Sqo Tome and Principe", "Saint Helena", "Saint Kitts and Nevis",
+                "Saint Lucia", "Saint Pierre and Miquelon", "Saint Vincent and the Grenadines",
+                "Samoa", "San Marino", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone",
+                "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa",
+                "South Georgia and the South Sandwich Islands", "South Korea", "Spain", "Sri Lanka",
+                "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden", "Switzerland",
+                "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "The Bahamas",
+                "The Gambia", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia",
+                "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Virgin Islands",
+                "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States",
+                "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu",
+                "Vatican City", "Venezuela", "Vietnam", "Wallis and Futuna", "Western Sahara",
                 "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"
         )
     }
