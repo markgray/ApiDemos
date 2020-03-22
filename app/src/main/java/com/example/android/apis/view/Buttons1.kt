e35132eb44736d526fb63cfe5ccad8b3f276f3ee
markgray <mark.t.gray@gmail.com>
2020-03-22 06:39:03 -0400

    Converted Buttons1.java to kotlin

--- app/src/main/java/com/example/android/apis/view/Buttons1.kt
+++ app/src/main/java/com/example/android/apis/view/Buttons1.kt
@@ -13,16 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 // Need the following import to get access to the app resources, since this
 // class is in a sub-package.
-import com.example.android.apis.R;
+import com.example.android.apis.R
+
 
 /**
  * A gallery of the different styles of buttons. Shows three different buttons,
@@ -30,16 +28,15 @@ import com.example.android.apis.R;
  * using style="?android:attr/buttonStyleSmall", and a ToggleButton. The small
  * button is same size as normal button on M.
  */
-public class Buttons1 extends AppCompatActivity {
+class Buttons1 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.buttons_1.
+     * `onCreate`, then we set our content view to our layout file R.layout.buttons_1.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.buttons_1);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.buttons_1)
     }
 }
