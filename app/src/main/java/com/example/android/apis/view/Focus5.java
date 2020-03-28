dbf30323a436d090bb0ef2384c4564234fa3fe9f
markgray <mark.t.gray@gmail.com>
2020-03-28 05:58:05 -0400

    Migrated Focus5.java to AppCompatActivity

--- app/src/main/java/com/example/android/apis/view/Focus5.java
+++ app/src/main/java/com/example/android/apis/view/Focus5.java
@@ -16,15 +16,17 @@
 
 package com.example.android.apis.view;
 
-import android.app.Activity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.android.apis.R;
 
 /**
  * Demonstrates using nextFocusForward in the layout/focus_5.xml to explicitly set sequential focus
  * order. Not for touch only.
  */
-public class Focus5 extends Activity {
+public class Focus5 extends AppCompatActivity {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
      * {@code onCreate}, then we set our content view to our layout file R.layout.focus_5.
