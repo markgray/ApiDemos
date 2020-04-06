0a9123dac82e991c699b47252469438e132d7c7d
markgray <mark.t.gray@gmail.com>
2020-04-06 06:58:57 -0400

    Converted Baseline3.java to kotlin

--- app/src/main/java/com/example/android/apis/view/Baseline3.kt
+++ app/src/main/java/com/example/android/apis/view/Baseline3.kt
@@ -13,30 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.android.apis.R;
-
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * Baseline alignment in LinearLayout with a center_vertical gravity. This sample shows that
  * using a center_vertical gravity disables baseline alignment.
  */
-public class Baseline3 extends AppCompatActivity {
+class Baseline3 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.baseline_3.
+     * `onCreate`, then we set our content view to our layout file R.layout.baseline_3.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.baseline_3);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.baseline_3)
     }
 }
