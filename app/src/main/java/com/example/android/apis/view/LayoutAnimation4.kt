5060a904ed06a9057c06b9687e7dc90ab97f0c16
markgray <mark.t.gray@gmail.com>
2020-04-06 05:39:01 -0400

    Edited comments

--- app/src/main/java/com/example/android/apis/view/LayoutAnimation4.kt
+++ app/src/main/java/com/example/android/apis/view/LayoutAnimation4.kt
@@ -36,10 +36,10 @@ class LayoutAnimation4 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
      * `onCreate`, then we set our content view to our layout file R.layout.layout_animation_4.
-     * Next we call our method `loadApps` to load `List<ResolveInfo> mApps` with data
+     * Next we call our method [loadApps] to load `List<ResolveInfo>` field [mApps] with data
      * from the `PackageManager` (all activities that can be performed for an intent with the
-     * action MAIN, and category LAUNCHER). We initialize our field `GridView mGrid` by finding the
-     * view with the ID R.id.grid, and set its adapter to a new instance of `AppsAdapter`.
+     * action MAIN, and category LAUNCHER). We initialize our [GridView] variable `grid` by finding
+     * the view with the ID R.id.grid, and set its adapter to a new instance of [AppsAdapter].
      *
      * @param savedInstanceState we do not override `onSaveInstanceState` so do not use.
      */
@@ -52,18 +52,18 @@ class LayoutAnimation4 : AppCompatActivity() {
     }
 
     /**
-     * List of `ResolveInfo` objects for all activities that can be executed for an intent
+     * List of [ResolveInfo] objects for all activities that can be executed for an intent
      * with the action MAIN, and category LAUNCHER loaded from the `PackageManager` by our
-     * method `loadApps`.
+     * method [loadApps].
      */
     private var mApps: List<ResolveInfo>? = null
 
     /**
-     * Loads `List<ResolveInfo> mApps` with a list of all activities that can be performed for
-     * an intent with the action MAIN, and category LAUNCHER loaded using the `PackageManager`.
-     * First we create `Intent mainIntent` with the action ACTION_MAIN, and add the category
+     * Loads `List<ResolveInfo>` field [mApps] with a list of all activities that can be performed
+     * for an intent with the action MAIN, and category LAUNCHER loaded using the `PackageManager`.
+     * First we create [Intent] `val mainIntent` with the action ACTION_MAIN, and add the category
      * CATEGORY_LAUNCHER. Then we retrieve a `PackageManager` instance and use it to retrieve
-     * all activities that can be performed for intent `mainIntent` to initialize `mApps`.
+     * all activities that can be performed for intent `mainIntent` to initialize [mApps].
      */
     private fun loadApps() {
         val mainIntent = Intent(Intent.ACTION_MAIN, null)
@@ -72,24 +72,23 @@ class LayoutAnimation4 : AppCompatActivity() {
     }
 
     /**
-     * Adapter that displays the icons in `List<ResolveInfo> mApps`.
+     * Adapter that displays the icons in `List<ResolveInfo>` field [mApps].
      */
     inner class AppsAdapter : BaseAdapter() {
         /**
          * Get a View that displays the data at the specified position in the data set. First we
-         * create a new instance for our variable `ImageView i`. We initialize our variable
-         * `ResolveInfo info` with the data in `mApps` at position `position`
-         * modulo the size of `mApps`, and set the content of `i` to a drawable of the
-         * icon associated with `info` that we retrieve by using that `ResolveInfo` to
-         * call back a `PackageManager` instance to load the icon from the application. We set
-         * the scale type of `i` to FIT_CENTER. We set our variable `int w` to 36 times
-         * the logical display density plus 0.5, and use it to set the layout parameters of `i`
-         * to `w` by `w`. Finally we return `i` to the caller.
+         * create a new instance for our [ImageView] variable `val i`. We initialize our [ResolveInfo]
+         * variable `val info` with the data in [mApps] at position [position] modulo the size of
+         * [mApps], and set the content of `i` to a drawable of the icon associated with `info` that
+         * we retrieve by using that [ResolveInfo] to call back a `PackageManager` instance to load
+         * the icon from the application. We set the scale type of `i` to FIT_CENTER. We set our
+         * variable `val w` to 36 times the logical display density plus 0.5, and use it to set the
+         * layout parameters of `i` to `w` by `w`. Finally we return `i` to the caller.
          *
          * @param position    The position of the item within the adapter's data set whose view we want.
-         * @param convertView The old view to reuse, if possible.
+         * @param convertView The old [View] to reuse, if possible.
          * @param parent      The parent that this view will eventually be attached to
-         * @return A View corresponding to the data at the specified position.
+         * @return A [View] corresponding to the data at the specified position.
          */
         override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
             val i = ImageView(this@LayoutAnimation4)
@@ -103,7 +102,7 @@ class LayoutAnimation4 : AppCompatActivity() {
 
         /**
          * How many items are in the data set represented by this Adapter. We return the minimum of
-         * the size of `mApps` and 32.
+         * the size of the [mApps] field and 32.
          *
          * @return Count of items.
          */
@@ -113,7 +112,7 @@ class LayoutAnimation4 : AppCompatActivity() {
 
         /**
          * Get the data item associated with the specified position in the data set. We return the
-         * data at position `position` modulo the size of `mApps`
+         * data at position [position] modulo the size of the [mApps] field.
          *
          * @param position Position of the item within the adapter's data set that we want.
          * @return The data at the specified position.
@@ -124,7 +123,7 @@ class LayoutAnimation4 : AppCompatActivity() {
 
         /**
          * Get the row id associated with the specified position in the list. Our row id is the same
-         * as our parameter `position` so we just return that to the caller.
+         * as our parameter [position] so we just return that to the caller.
          *
          * @param position The position of the item within the adapter's data set whose row id we want.
          * @return The id of the item at the specified position.
