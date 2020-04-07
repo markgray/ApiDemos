daa413d3ba205a7a2b66e2fb64641fb6331e64d2
markgray <mark.t.gray@gmail.com>
2020-04-07 05:45:48 -0400

    Converted Baseline6.java to kotlin

--- app/src/main/java/com/example/android/apis/view/Baseline6.kt
+++ app/src/main/java/com/example/android/apis/view/Baseline6.kt
@@ -13,29 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.os.Bundle;
-
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * Baseline alignment in RelativeLayout. Demonstrates the baseline alignment in a RelativeLayout
  * when one of the widgets is a multi-line EditText
  */
-public class Baseline6 extends Activity {
+class Baseline6 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.baseline_6.
+     * `onCreate`, then we set our content view to our layout file R.layout.baseline_6.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.baseline_6);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.baseline_6)
     }
 }
