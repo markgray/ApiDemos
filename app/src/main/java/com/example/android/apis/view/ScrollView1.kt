3ca517fa944698cb525ce8b039d0a0fa2ca55f94
markgray <mark.t.gray@gmail.com>
2020-04-08 07:03:09 -0400

    Converted ScrollView1.java to kotlin

--- app/src/main/java/com/example/android/apis/view/ScrollView1.kt
+++ app/src/main/java/com/example/android/apis/view/ScrollView1.kt
@@ -13,30 +13,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-// Need the following import to get access to the app resources, since this
-// class is in a sub-package.
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.os.Bundle;
-
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * Demonstrates wrapping a (short) layout in a ScrollView.
  */
-public class ScrollView1 extends Activity {
+class ScrollView1 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.scroll_view_1.
+     * `onCreate`, then we set our content view to our layout file R.layout.scroll_view_1.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.scroll_view_1);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scroll_view_1)
     }
 }
