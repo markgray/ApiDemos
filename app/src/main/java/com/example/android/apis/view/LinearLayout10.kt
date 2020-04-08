0ef6c89ec89e92ea997ebfbf6fdd77f20a033d17
markgray <mark.t.gray@gmail.com>
2020-04-08 06:52:20 -0400

    Converted LinearLayout10.java to kotlin

--- app/src/main/java/com/example/android/apis/view/LinearLayout10.kt
+++ app/src/main/java/com/example/android/apis/view/LinearLayout10.kt
@@ -13,16 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-// Need the following import to get access to the app resources, since this
-// class is in a sub-package.
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.os.Bundle;
-
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * Demonstrates the use of LinearLayout backgrounds to group labels,
@@ -30,16 +25,15 @@ import android.os.Bundle;
  * background to group related TextViews, EditTexts, and Buttons.
  * (EditText is hard to read because of background color -- please fix.)
  */
-public class LinearLayout10 extends Activity {
+class LinearLayout10 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.linear_layout_10.
+     * `onCreate`, then we set our content view to our layout file R.layout.linear_layout_10.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout_10);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout_10)
     }
 }
