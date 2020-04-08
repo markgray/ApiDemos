02f13f3ebcc25f1fdcef8987372f681c04052110
markgray <mark.t.gray@gmail.com>
2020-04-08 06:56:04 -0400

    Converted RelativeLayout1.java to kotlin

--- app/src/main/java/com/example/android/apis/view/RelativeLayout1.kt
+++ app/src/main/java/com/example/android/apis/view/RelativeLayout1.kt
@@ -13,32 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-// Need the following import to get access to the app resources, since this
-// class is in a sub-package.
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.os.Bundle;
-
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * A simple layout which demonstrates stretching a view to fill the space between two other views.
  * This occurs oddly enough because even though all three share the same default weight, the
  * android:layout_height="0dip" causes the center view to take the leftover space from the parent.
  */
-public class RelativeLayout1 extends Activity {
+class RelativeLayout1 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.relative_layout_1.
+     * `onCreate`, then we set our content view to our layout file R.layout.relative_layout_1.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.relative_layout_1);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.relative_layout_1)
     }
 }
