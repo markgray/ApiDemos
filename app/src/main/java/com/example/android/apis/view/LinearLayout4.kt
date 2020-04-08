c3b27b27530aa0c53b1b23fd2554d720236ea4c0
markgray <mark.t.gray@gmail.com>
2020-04-08 06:17:57 -0400

    Converted LinearLayout4.java to kotlin

--- app/src/main/java/com/example/android/apis/view/LinearLayout4.kt
+++ app/src/main/java/com/example/android/apis/view/LinearLayout4.kt
@@ -13,31 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-// Need the following import to get access to the app resources, since this
-// class is in a sub-package.
-
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * Demonstrates a horizontal linear layout with equally sized columns. This is due to
  * all children View's using the attribute android:layout_weight="1"
  */
-public class LinearLayout4 extends Activity {
+class LinearLayout4 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.linear_layout_4.
+     * `onCreate`, then we set our content view to our layout file R.layout.linear_layout_4.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout_4);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout_4)
     }
 }
