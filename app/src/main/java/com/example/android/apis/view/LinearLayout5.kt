8def0bebe18ce3dac5fda6639c3c0b6648b7f177
markgray <mark.t.gray@gmail.com>
2020-04-08 06:21:36 -0400

    Converted LinearLayout5.java to kotlin

--- app/src/main/java/com/example/android/apis/view/LinearLayout5.kt
+++ app/src/main/java/com/example/android/apis/view/LinearLayout5.kt
@@ -13,16 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-// Need the following import to get access to the app resources, since this
-// class is in a sub-package.
-
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * Demonstrates building a simple form with nested LinearLayouts. It uses
@@ -32,16 +27,15 @@ import android.os.Bundle;
  * LinearLayout's width is set to wrap_content. (If it was match_parent
  * it would not have any room to slide to the right.)
  */
-public class LinearLayout5 extends Activity {
+class LinearLayout5 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.linear_layout_5.
+     * `onCreate`, then we set our content view to our layout file R.layout.linear_layout_5.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout_5);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout_5)
     }
 }
