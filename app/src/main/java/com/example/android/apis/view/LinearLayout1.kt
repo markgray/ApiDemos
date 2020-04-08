df987fda2babc94cb41f04b7acfc66b3ddb7ec83
markgray <mark.t.gray@gmail.com>
2020-04-08 05:55:53 -0400

    Converted LinearLayout1.java to kotlin

--- app/src/main/java/com/example/android/apis/view/LinearLayout1.kt
+++ app/src/main/java/com/example/android/apis/view/LinearLayout1.kt
@@ -13,33 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-// Need the following import to get access to the app resources, since this
-// class is in a sub-package.
-
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.os.Bundle;
-
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * A simple linear layout where the height of the layout is the sum of its children.
  * The height of the layout is the sum of its children due to the attribute
  * android:layout_height="wrap_content
  */
-public class LinearLayout1 extends Activity {
+class LinearLayout1 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.linear_layout_1.
+     * `onCreate`, then we set our content view to our layout file R.layout.linear_layout_1.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout_1);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout_1)
     }
 }
