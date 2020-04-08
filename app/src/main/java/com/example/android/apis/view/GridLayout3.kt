967be87aecfdb778bbc72b1c4784c419d8456d90
markgray <mark.t.gray@gmail.com>
2020-04-08 05:48:05 -0400

    Edited comments

--- app/src/main/java/com/example/android/apis/view/GridLayout3.kt
+++ app/src/main/java/com/example/android/apis/view/GridLayout3.kt
@@ -30,7 +30,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
- * A form, showing use of the GridLayout API from java code. Here we demonstrate use of the
+ * A form, showing use of the [GridLayout] API from java code. Here we demonstrate use of the
  * row/column order preserved property which allows rows and or columns to pass over each other
  * when needed. The two buttons in the bottom right corner need to be separated from the other
  * UI elements. This can either be done by separating rows or separating columns - but we don't
@@ -41,10 +41,10 @@ import androidx.appcompat.app.AppCompatActivity
 class GridLayout3 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * `onCreate`, then we set our content view to the `GridLayout` built and configured
-     * by our method `create`.
+     * `onCreate`, then we set our content view to the [GridLayout] built and configured by our
+     * method [create].
      *
-     * @param savedInstanceState we do not override `onSaveInstanceState` so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,63 +53,60 @@ class GridLayout3 : AppCompatActivity() {
 
     companion object {
         /**
-         * Creates, builds, configures and returns a `GridLayout` to use for our UI. First we
-         * initialize our variable `GridLayout p` with a new instance, set it to use the default
-         * margins, and set its alignment mode to ALIGN_BOUNDS. We initialize `Configuration configuration`
-         * with the current configuration that is in effect for our `context`. If the `orientation`
-         * field of `configuration` is ORIENTATION_PORTRAIT we configure `p` to be at liberty to
-         * place the horizontal column boundaries in whatever order best fits its constraints. Otherwise
-         * we configure `p` to be at liberty to place the vertical row boundaries in whatever order
-         * best fits its constraints.
-         *
+         * Creates, builds, configures and returns a [GridLayout] to use for our UI. First we
+         * initialize our [GridLayout] variable `val p` with a new instance, set it to use the
+         * default margins, and set its alignment mode to ALIGN_BOUNDS. We initialize [Configuration]
+         * variable `val configuration` with the current configuration that is in effect for our
+         * [context]. If the `orientation` field of `configuration` is ORIENTATION_PORTRAIT we
+         * configure `p` to be at liberty to place the horizontal column boundaries in whatever
+         * order best fits its constraints. Otherwise we configure `p` to be at liberty to place
+         * the vertical row boundaries in whatever order best fits its constraints.
          *
          * Next we define the grid indices and alignment for the rows of `Grid` cells. We define
-         * `Spec titleRow` to start in row 0, `Spec introRow` to start row 1, `Spec emailRow`
-         * to start in row 2 with BASELINE alignment, `Spec passwordRow` to start row 3 with BASELINE
-         * alignment, `Spec button1Row` to start row 5, and `Spec button2Row` to start row 6.
-         *
+         * [GridLayout.Spec] variables `val titleRow` to start in row 0, `val introRow` to start
+         * row 1, `val emailRow` to start in row 2 with BASELINE alignment, `val passwordRow` to
+         * start row 3 with BASELINE alignment, `val button1Row` to start row 5, and `val button2Row`
+         * to start row 6.
          *
          * We define the grid indices, size, and alignment of the columns of cells as follows:
-         * `Spec centerInAllColumns` starts in column 0, with size 4 and CENTER alignment,
-         * `Spec leftAlignInAllColumns` starts in column 0, with size 4 and LEFT alignment,
-         * `Spec labelColumn` starts in column 0, with RIGHT alignment, `Spec fieldColumn`
-         * starts in column 1, with LEFT alignment, `Spec defineLastColumn` starts in column 3,
-         * `Spec fillLastColumn` starts in column 3 with FILL alignment (Indicates that a view
+         * [GridLayout.Spec] variables `val centerInAllColumns` starts in column 0, with size 4 and
+         * CENTER alignment, `val leftAlignInAllColumns` starts in column 0, with size 4 and LEFT
+         * alignment, `val labelColumn` starts in column 0, with RIGHT alignment, `val fieldColumn`
+         * starts in column 1, with LEFT alignment, `val defineLastColumn` starts in column 3,
+         * `val fillLastColumn` starts in column 3 with FILL alignment (Indicates that a view
          * should expanded to fit the boundaries of its cell group).
          *
-         *
-         * Now we create some views and use the above `Spec` objects to position them. We create
-         * `TextView c`, set its text size to 32, set its text to "Email setup" and add it to
-         * `p` using a new instance of `LayoutParams` which uses `titleRow` as the
-         * row spec, and `centerInAllColumns` as the column spec. We then create a new instance
-         * of `TextView c`, set its text size to 16, set its text to "You can configure email in
-         * a few simple steps:", and add it to `p` using a new instance of `LayoutParams`
-         * which uses `introRow` as the row spec, and `leftAlignInAllColumns` as the column
-         * spec. We then create a new instance of `TextView c`, set its text to "Email address:",
-         * and add it to `p` using a new instance of `LayoutParams` which uses `emailRow`
+         * Now we create some views and use the above [GridLayout.Spec] objects to position them.
+         * We create [TextView] `val c`, set its text size to 32, set its text to "Email setup" and
+         * add it to `p` using a new instance of [GridLayout.LayoutParams] which uses `titleRow` as
+         * the row spec, and `centerInAllColumns` as the column spec. We then create a new instance
+         * for [TextView] `val c`, set its text size to 16, set its text to "You can configure email
+         * in a few simple steps:", and add it to `p` using a new instance of [GridLayout.LayoutParams]
+         * which uses `introRow` as the row spec, and `leftAlignInAllColumns` as the column spec.
+         * We then create a new instance for [TextView] `val c`, set its text to "Email address:",
+         * and add it to `p` using a new instance of [GridLayout.LayoutParams] which uses `emailRow`
          * as the row spec, and `labelColumn` as the column spec. We create a new instance for
-         * `EditText c`, set its size to 10 ems, set its input type to TYPE_CLASS_TEXT or'ed with
-         * TYPE_TEXT_VARIATION_EMAIL_ADDRESS, and add it to `p` using a new instance of
-         * `LayoutParams` which uses `emailRow` as the row spec, and `fieldColumn` as
-         * the column spec. We create a new instance for `TextView c`, set its text to "Password:",
-         * and add it to `p` using a new instance of `LayoutParams` which uses `passwordRow`
-         * as the row spec, and `labelColumn` as the column spec. We create a new instance of
-         * `EditText` for `TextView c`, set its size to 8 ems, set its input type to TYPE_CLASS_TEXT
-         * or'ed with TYPE_TEXT_VARIATION_PASSWORD, and add it to `p` using a new instance of
-         * `LayoutParams` which uses `passwordRow` as the row spec, and `fieldColumn`
-         * as the column spec. We create a new instance for `Button c`, set its text to "Manual setup",
-         * and add it to `p` using a new instance of `LayoutParams` which uses `button1Row`
-         * as the row spec, and `defineLastColumn` as the column spec. We create a new instance for
-         * `Button c`, set its text to "Next", and add it to `p` using a new instance of
-         * `LayoutParams` which uses `button2Row` as the row spec, and `fillLastColumn`
-         * as the column spec.
-         *
+         * [EditText] `val c`, set its size to 10 ems, set its input type to TYPE_CLASS_TEXT or'ed
+         * with TYPE_TEXT_VARIATION_EMAIL_ADDRESS, and add it to `p` using a new instance of
+         * [GridLayout.LayoutParams] which uses `emailRow` as the row spec, and `fieldColumn` as
+         * the column spec. We create a new instance for [TextView] `val c`, set its text to
+         * "Password:", and add it to `p` using a new instance of [GridLayout.LayoutParams] which
+         * uses `passwordRow` as the row spec, and `labelColumn` as the column spec. We create a new
+         * instance of [EditText] for [TextView] `val c`, set its size to 8 ems, set its input type
+         * to TYPE_CLASS_TEXT or'ed with TYPE_TEXT_VARIATION_PASSWORD, and add it to `p` using a new
+         * instance of [GridLayout.LayoutParams] which uses `passwordRow` as the row spec, and
+         * `fieldColumn` as the column spec. We create a new instance for [Button] `val c`, set its
+         * text to "Manual setup", and add it to `p` using a new instance of [GridLayout.LayoutParams]
+         * which uses `button1Row` as the row spec, and `defineLastColumn` as the column spec. We
+         * create a new instance for [Button] `val c`, set its text to "Next", and add it to `p`
+         * using a new instance of [GridLayout.LayoutParams] which uses `button2Row` as the row spec,
+         * and `fillLastColumn` as the column spec.
          *
          * Finally we return `p` to the caller.
          *
-         * @param context `Context` to use to access resources, "this" when called from our
-         * `onCreate` override.
-         * @return a `GridLayout` containing our UI
+         * @param context [Context] to use to access resources, "this" when called from our
+         * [onCreate] override.
+         * @return a [GridLayout] containing our UI
          */
         fun create(context: Context): View {
             val p = GridLayout(context)
