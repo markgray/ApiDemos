4aafbecc1576ba937aa09033af9b7335e55542b2
markgray <mark.t.gray@gmail.com>
2020-04-10 06:17:48 -0400

    Converted TableLayout1.java to kotlin

--- app/src/main/java/com/example/android/apis/view/TableLayout1.kt
+++ app/src/main/java/com/example/android/apis/view/TableLayout1.kt
@@ -13,27 +13,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * Very simple example of TableLayout done entirely in xml, three rows, three columns of TextViews
  */
-public class TableLayout1 extends Activity {
+class TableLayout1 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.table_layout_1.
+     * `onCreate`, then we set our content view to our layout file R.layout.table_layout_1.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.table_layout_1);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_layout_1)
     }
 }
