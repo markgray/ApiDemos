c496389a125c81aa2a95904b27ac73df82d1aa25
markgray <mark.t.gray@gmail.com>
2020-04-10 06:36:38 -0400

    Converted TableLayout6.java to kotlin

--- app/src/main/java/com/example/android/apis/view/TableLayout6.kt
+++ app/src/main/java/com/example/android/apis/view/TableLayout6.kt
@@ -13,28 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * Uses android:stretchColumns="1" to stretch second column, and android:layout_column="1"
  * to skip column 0 for some TableRow's
  */
-public class TableLayout6 extends Activity {
+class TableLayout6 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.table_layout_6.
+     * `onCreate`, then we set our content view to our layout file R.layout.table_layout_6.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.table_layout_6);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_layout_6)
     }
 }
