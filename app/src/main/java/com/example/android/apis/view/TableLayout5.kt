2537e1bcf0a0faa400138435dc0a14f9c930ff09
markgray <mark.t.gray@gmail.com>
2020-04-10 06:32:57 -0400

    Converted TableLayout5.java to kotlin

--- app/src/main/java/com/example/android/apis/view/TableLayout5.kt
+++ app/src/main/java/com/example/android/apis/view/TableLayout5.kt
@@ -13,27 +13,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * Uses android:stretchColumns="1" and has View divider line children along with the TableRow's
  */
-public class TableLayout5 extends Activity {
+class TableLayout5 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.table_layout_5.
+     * `onCreate`, then we set our content view to our layout file R.layout.table_layout_5.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.table_layout_5);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_layout_5)
     }
 }
