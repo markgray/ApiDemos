0c2d20889049c83decd366a2ccf7a893011e7e2f
markgray <mark.t.gray@gmail.com>
2020-04-10 06:29:13 -0400

    Converted TableLayout4.java to kotlin

--- app/src/main/java/com/example/android/apis/view/TableLayout4.kt
+++ app/src/main/java/com/example/android/apis/view/TableLayout4.kt
@@ -13,28 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * Stretch some columns: second column uses android:gravity="right",
  * android:stretchColumns="1" (column numbers are zero based)
  */
-public class TableLayout4 extends Activity {
+class TableLayout4 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.table_layout_4.
+     * `onCreate`, then we set our content view to our layout file R.layout.table_layout_4.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.table_layout_4);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_layout_4)
     }
 }
