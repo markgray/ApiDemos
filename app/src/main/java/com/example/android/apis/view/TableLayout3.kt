2203f00bebbe4325bfd426191bfcdfa41a69d0c5
markgray <mark.t.gray@gmail.com>
2020-04-10 06:25:49 -0400

    Converted TableLayout3.java to kotlin

--- app/src/main/java/com/example/android/apis/view/TableLayout3.kt
+++ app/src/main/java/com/example/android/apis/view/TableLayout3.kt
@@ -13,13 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * Rows have different number of columns and content doesn't fit on screen:
@@ -28,16 +26,15 @@ import android.os.Bundle;
  * actually show what it intends to show. Not only that, but if the text is too long
  * it is wrapped and the row is increased in size to fit the entire text.
  */
-public class TableLayout3 extends Activity {
+class TableLayout3 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.table_layout_3.
+     * `onCreate`, then we set our content view to our layout file R.layout.table_layout_3.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.table_layout_3);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_layout_3)
     }
 }
