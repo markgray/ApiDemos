449bed8cfcec7203c45ef78c7f9306c58bba3bb9
markgray <mark.t.gray@gmail.com>
2020-04-10 07:08:42 -0400

    Converted TableLayout10.java to kotlin

--- app/src/main/java/com/example/android/apis/view/TableLayout10.kt
+++ app/src/main/java/com/example/android/apis/view/TableLayout10.kt
@@ -13,28 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-import com.example.android.apis.R;
-
-import android.app.Activity;
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * Uses xml only to construct a form, too long of an entry in the EditText boxes does a
  * weird thing -- it scrolls the buttons at the bottom off the screen!
  */
-public class TableLayout10 extends Activity {
+class TableLayout10 : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.table_layout_10.
+     * `onCreate`, then we set our content view to our layout file R.layout.table_layout_10.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.table_layout_10);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_layout_10)
     }
 }
