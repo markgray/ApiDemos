7e3817e1e78b4d5994f8980ac30738bbb959c898
markgray <mark.t.gray@gmail.com>
2020-04-14 06:03:11 -0400

    Converted List17.java to kotlin Does not use AppCompat

--- app/src/main/java/com/example/android/apis/view/List17.kt
+++ app/src/main/java/com/example/android/apis/view/List17.kt
@@ -13,16 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-import android.annotation.TargetApi;
-import android.app.ListActivity;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.annotation.TargetApi
+import android.app.ListActivity
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 /**
  * A list view where the last item the user clicked is placed in the "activated" state, causing its
@@ -30,43 +29,45 @@ import android.widget.ListView;
  * for showing a list item with a single line of text whose background changes when activated. Uses
  * getListView().setItemChecked in onCreate to start with first item activated.
  */
-public class List17 extends ListActivity {
+class List17 : ListActivity() {
     /**
      * Reference to the array that we use as our database.
      */
-    private String[] mStrings = Cheeses.sCheeseStrings;
+    private val mStrings = Cheeses.sCheeseStrings
 
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}. Then we set our list adapter to a new instance of {@code ArrayAdapter}
-     * constructed to display our array {@code mStrings} using the layout android.R.layout.simple_list_item_activated_1.
-     * We enable the text filter for our {@code ListView} (needs keyboard to use), set its choice mode
+     * `onCreate`. Then we set our list adapter to a new instance of [ArrayAdapter] constructed to
+     * display our array [mStrings] using the layout android.R.layout.simple_list_item_activated_1.
+     * We enable the text filter for our [ListView] (needs keyboard to use), set its choice mode
      * to CHOICE_MODE_SINGLE, and set the item at position 0 to be checked.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         // Use the built-in layout for showing a list item with a single
         // line of text whose background changes when activated.
-        setListAdapter(new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_activated_1, mStrings));
-        getListView().setTextFilterEnabled(true);
+        listAdapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_activated_1,
+                mStrings
+        )
+        listView.isTextFilterEnabled = true
 
         // Tell the list view to show one checked/activated item at a time.
-        getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        listView.choiceMode = ListView.CHOICE_MODE_SINGLE
 
         // Start with first item activated.
         // Make the newly clicked item the currently selected one.
-        getListView().setItemChecked(0, true);
+        listView.setItemChecked(0, true)
     }
 
     /**
-     * This method will be called when an item in the list is selected. We call the {@code setItemChecked}
-     * method of our {@code ListView} to set the item at {@code position} to checked state (automatically
+     * This method will be called when an item in the list is selected. We call the `setItemChecked`
+     * method of our [ListView] to set the item at [position] to checked state (automatically
      * un-checking the previously selected item).
      *
      * @param l        The ListView where the click happened
@@ -74,9 +75,8 @@ public class List17 extends ListActivity {
      * @param position The position of the view in the list
      * @param id       The row id of the item that was clicked
      */
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
+    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
         // Make the newly clicked item the currently selected one.
-        getListView().setItemChecked(position, true);
+        listView.setItemChecked(position, true)
     }
 }
