70eeece2766c72c44e61f0016a293ae3c43cf0ab
markgray <mark.t.gray@gmail.com>
2020-04-21 07:27:38 -0400

    Converted SystemUIModesOverlay.java to kotlin

--- app/src/main/java/com/example/android/apis/view/SystemUIModesOverlay.kt
+++ app/src/main/java/com/example/android/apis/view/SystemUIModesOverlay.kt
@@ -13,13 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-import android.annotation.TargetApi;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.Window;
+import android.annotation.TargetApi
+import android.os.Build
+import android.os.Bundle
+import android.view.Window
 
 /**
  * Version of demo that uses the action bar in overlay mode. Extends
@@ -27,21 +26,20 @@ import android.view.Window;
  * Window.FEATURE_ACTION_BAR_OVERLAY which overlays the action bar rather than
  * positioning the action bar in the space above the window content.
  */
-public class SystemUIModesOverlay extends SystemUIModes {
+class SystemUIModesOverlay : SystemUIModes() {
     /**
      * Called when the activity is starting. We fetch a reference to the current Window for this
      * activity and use it to request the feature FEATURE_ACTION_BAR_OVERLAY (requests an Action Bar
      * that overlays window content. Normally an Action Bar will sit in the space above window
      * content, but if this feature is requested along with FEATURE_ACTION_BAR it will be layered
      * over the window content itself), then we call through to our super's implementation of
-     * {@code onCreate}.
+     * `onCreate`.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
-        super.onCreate(savedInstanceState);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window.requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY)
+        super.onCreate(savedInstanceState)
     }
 }
