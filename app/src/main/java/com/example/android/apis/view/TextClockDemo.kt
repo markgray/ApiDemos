d3596b24065e9692f60e076f112059aa37f22a4f
markgray <mark.t.gray@gmail.com>
2020-04-25 06:53:15 -0400

    Converted TextClockDemo.java to kotlin and AppCompatActivity

--- app/src/main/java/com/example/android/apis/view/TextClockDemo.kt
+++ app/src/main/java/com/example/android/apis/view/TextClockDemo.kt
@@ -13,32 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.apis.view
 
-package com.example.android.apis.view;
-
-// Need the following import to get access to the app resources, since this
-// class is in a sub-package.
-
-import android.app.Activity;
-import android.os.Bundle;
-
-import com.example.android.apis.R;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.apis.R
 
 /**
  * Variants of TextClock: Default formats; Shows the date only; Uses styles
  * (different android:format12Hour, and android:format24Hour strings with
  * font and face defined in string xml); Shows seconds; Uses a fixed time zone.
  */
-public class TextClockDemo extends Activity {
+class TextClockDemo : AppCompatActivity() {
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * {@code onCreate}, then we set our content view to our layout file R.layout.textclock.
+     * `onCreate`, then we set our content view to our layout file R.layout.textclock.
      *
-     * @param savedInstanceState we do not override {@code onSaveInstanceState} so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.textclock);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.textclock)
     }
 }
