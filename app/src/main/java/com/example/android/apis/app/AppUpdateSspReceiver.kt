0fdcf5b9b93c08704dd783b408c6cdb04503d000
markgray <mark.t.gray@gmail.com>
2020-04-26 06:52:27 -0400

    Converted AppUpdateReceiver.java and AppUpdateSspReceiver.java to kotlin

---
+++ app/src/main/java/com/example/android/apis/app/AppUpdateSspReceiver.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.apis.app
+
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+/**
+ * Executed when a new version of any application is installed. It has an `action` intent filter
+ * for the action android:name="android.intent.action.PACKAGE_REPLACED"
+ */
+class AppUpdateSspReceiver : BroadcastReceiver() {
+    /**
+     * This method is called when the [BroadcastReceiver] is receiving an [Intent] broadcast for the
+     * action android:name="android.intent.action.PACKAGE_REPLACED". We just toast the fact that
+     * the app whose name is given in the `data` of our [Intent] parameter [intent] has been updated.
+     *
+     * @param context The Context in which the receiver is running.
+     * @param intent  The Intent being received.
+     */
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
+    override fun onReceive(context: Context, intent: Intent) {
+        val msg = "Ssp update received: " + intent.data
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
+    }
+}
