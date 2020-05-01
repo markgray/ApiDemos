34af9fd19144c120fcce170112403064b5edabf2
markgray <mark.t.gray@gmail.com>
2020-05-01 09:17:15 -0400

    Converted CheckableFrameLayout.java to kotlin (untested)

---
+++ app/src/main/java/com/example/android/apis/view/CheckableFrameLayout.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.apis.view
+
+import android.content.Context
+import android.graphics.drawable.ColorDrawable
+import android.util.AttributeSet
+import android.widget.Checkable
+import android.widget.FrameLayout
+
+class CheckableFrameLayout : FrameLayout, Checkable {
+    private var mChecked = false
+
+    constructor(context: Context?) : super(context!!)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs)
+
+    override fun setChecked(checked: Boolean) {
+        mChecked = checked
+        @Suppress("DEPRECATION")
+        setBackgroundDrawable(if (checked) ColorDrawable(-0xffff60) else null)
+    }
+
+    override fun isChecked(): Boolean {
+        return mChecked
+    }
+
+    override fun toggle() {
+        isChecked = !mChecked
+    }
+}
