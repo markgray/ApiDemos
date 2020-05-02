e048228564d69ae11e107690270723f3f87d6cd8
markgray <mark.t.gray@gmail.com>
2020-05-02 08:09:57 -0400

    Substituted individual imports for wild card imports.

--- app/src/main/java/com/example/android/apis/app/ForegroundService.kt
+++ app/src/main/java/com/example/android/apis/app/ForegroundService.kt
@@ -1,6 +1,10 @@
 package com.example.android.apis.app
 
-import android.app.*
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
@@ -10,7 +14,9 @@ import android.os.IBinder
 import android.os.PowerManager
 import android.os.PowerManager.WakeLock
 import android.util.Log
+
 import androidx.annotation.RequiresApi
+
 import com.example.android.apis.R
 
 @Suppress("MemberVisibilityCanBePrivate")
