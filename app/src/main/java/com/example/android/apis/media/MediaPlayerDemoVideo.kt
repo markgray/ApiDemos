1138f2ed73a01cdbe430924aca8e6c9d8f1df946
markgray <mark.t.gray@gmail.com>
2020-05-02 07:51:10 -0400

    Substituted individual imports for wild card imports.

--- app/src/main/java/com/example/android/apis/media/MediaPlayerDemoVideo.kt
+++ app/src/main/java/com/example/android/apis/media/MediaPlayerDemoVideo.kt
@@ -17,17 +17,22 @@ package com.example.android.apis.media
 
 import android.media.AudioManager
 import android.media.MediaPlayer
-import android.media.MediaPlayer.*
+import android.media.MediaPlayer.OnBufferingUpdateListener
+import android.media.MediaPlayer.OnCompletionListener
+import android.media.MediaPlayer.OnPreparedListener
+import android.media.MediaPlayer.OnVideoSizeChangedListener
 import android.os.Bundle
 import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
+
 import com.example.android.apis.R
 
 /**
- * Activity launched by `[MediaPlayerDemo]` to play a video file
+ * Activity launched by [MediaPlayerDemo] to play a video file
  */
 class MediaPlayerDemoVideo : AppCompatActivity(), OnBufferingUpdateListener, OnCompletionListener,
         OnPreparedListener, OnVideoSizeChangedListener, SurfaceHolder.Callback
