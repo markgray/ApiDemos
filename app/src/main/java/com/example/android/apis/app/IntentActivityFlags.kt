a2b67626cde397b4bee5f1756d996b35bc70bc0b
markgray <mark.t.gray@gmail.com>
2021-12-09 05:21:26 -0500

    Added PendingIntent.FLAG_IMMUTABLE to PendingIntent for SDK S and above in IntentActivityFlags.kt

--- app/src/main/java/com/example/android/apis/app/IntentActivityFlags.kt
+++ app/src/main/java/com/example/android/apis/app/IntentActivityFlags.kt
@@ -51,8 +51,16 @@ class IntentActivityFlags : AppCompatActivity() {
     private val mFlagActivityClearTaskPIListener = OnClickListener {
         val context = this@IntentActivityFlags
 
-        val pi = PendingIntent.getActivities(context, 0,
-                buildIntentsToViewsLists(), PendingIntent.FLAG_UPDATE_CURRENT)
+        val pi = PendingIntent.getActivities(
+            context,
+            0,
+            buildIntentsToViewsLists(),
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            } else {
+                PendingIntent.FLAG_UPDATE_CURRENT
+            }
+        )
 
         try {
             pi.send()
