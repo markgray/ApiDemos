e4ed7e6799e25e845b2e2da635a550261d5ced71
markgray <mark.t.gray@gmail.com>
2021-12-15 06:34:29 -0500

    Added TODO to RotationAnimation.kt

--- app/src/main/java/com/example/android/apis/app/RotationAnimation.kt
+++ app/src/main/java/com/example/android/apis/app/RotationAnimation.kt
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+@file:Suppress("DEPRECATION")
+// TODO: Replace deprecated use of LayoutParams.FLAG_FULLSCREEN with WindowInsetsController.hide
 
 package com.example.android.apis.app
 
