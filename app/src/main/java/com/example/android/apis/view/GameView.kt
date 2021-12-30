4b3a53534559763c57e794dbf4e9f3c7e3d77c34
markgray <mark.t.gray@gmail.com>
2021-12-30 06:18:25 -0500

    Added TODO in GameView.kt

--- app/src/main/java/com/example/android/apis/view/GameView.kt
+++ app/src/main/java/com/example/android/apis/view/GameView.kt
@@ -15,6 +15,8 @@
  */
 package com.example.android.apis.view
 
+// TODO: Update to use of VibratorManager
+
 import android.annotation.TargetApi
 import android.content.Context
 import android.graphics.Canvas
