5aab516924438896c95966d22a88671d9787034a
markgray <mark.t.gray@gmail.com>
2021-12-31 06:23:16 -0500

    Added TODO in LayoutAnimation2.kt

--- app/src/main/java/com/example/android/apis/view/LayoutAnimation2.kt
+++ app/src/main/java/com/example/android/apis/view/LayoutAnimation2.kt
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 package com.example.android.apis.view
+// TODO: comments need corections.
 
 import android.os.Bundle
 import android.view.animation.AlphaAnimation
