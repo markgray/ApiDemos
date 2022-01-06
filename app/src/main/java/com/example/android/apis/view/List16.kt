be77b6efcd640b68aef66651426a8897c9a1cec8
markgray <mark.t.gray@gmail.com>
2022-01-06 06:08:56 -0500

    Replaced use of deprecated ListActivity with a ListView in a layout file in List16.kt

--- app/src/main/java/com/example/android/apis/view/List16.kt
+++ app/src/main/java/com/example/android/apis/view/List16.kt
@@ -13,12 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:Suppress("DEPRECATION")
 
 package com.example.android.apis.view
 
 import android.annotation.TargetApi
-import android.app.ListActivity
 import android.os.Build
 import android.os.Bundle
 import android.view.ActionMode
@@ -28,33 +26,39 @@ import android.widget.AbsListView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.android.apis.R
 
 /**
  * This demo illustrates the use of CHOICE_MODE_MULTIPLE_MODAL, a.k.a. selection mode on ListView
  * coupled with the new simple_list_item_activated_1 which uses a highlighted border for selected
  * items.
- * TODO: Use ListFragment or RecyclerView instead of ListActivity
  */
-class List16 : ListActivity() {
+class List16 : AppCompatActivity() {
+    private lateinit var list: ListView
     /**
      * Called when the activity is starting. First we call through to our super's implementation of
-     * `onCreate`. We initialize our [ListView] variable `val lv` by fetching a reference to
-     * our [ListView], set its choice mode to `CHOICE_MODE_MULTIPLE_MODAL`, and set its
-     * `MultiChoiceModeListener` to a new instance of [ModeCallback]. Finally we set our
-     * list adapter to a new instance of [ArrayAdapter] constructed to display the array
-     * [Cheeses.sCheeseStrings] using the layout android.R.layout.simple_list_item_activated_1.
+     * `onCreate`, then we set our content view to our layout file [R.layout.list_16] and initialize
+     * our [ListView] field [list] by finding the view with ID [R.id.list]. We set the choice mode
+     * of [list] to `CHOICE_MODE_MULTIPLE_MODAL`, and set its `MultiChoiceModeListener` to a new
+     * instance of [ModeCallback]. Finally we set the list adapter of [list] to a new instance of
+     * [ArrayAdapter] constructed to display the array [Cheeses.sCheeseStrings] using the layout
+     * android.R.layout.simple_list_item_activated_1.
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val lv = listView
-        lv.choiceMode = ListView.CHOICE_MODE_MULTIPLE_MODAL
-        lv.setMultiChoiceModeListener(ModeCallback())
-        listAdapter = ArrayAdapter(this,
-                android.R.layout.simple_list_item_activated_1, Cheeses.sCheeseStrings)
+        setContentView(R.layout.list_16)
+        list = findViewById(R.id.list)
+        list.choiceMode = ListView.CHOICE_MODE_MULTIPLE_MODAL
+        list.setMultiChoiceModeListener(ModeCallback())
+        list.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_activated_1,
+            Cheeses.sCheeseStrings
+        )
     }
 
     /**
@@ -68,7 +72,7 @@ class List16 : ListActivity() {
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
-        actionBar!!.subtitle = "Long press to start selection"
+        supportActionBar!!.subtitle = "Long press to start selection"
     }
 
     /**
@@ -130,12 +134,12 @@ class List16 : ListActivity() {
         override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
             when (item.itemId) {
                 R.id.share -> {
-                    Toast.makeText(this@List16, "Shared " + listView.checkedItemCount +
-                            " items", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@List16, "Shared " + list.checkedItemCount +
+                        " items", Toast.LENGTH_SHORT).show()
                     mode.finish()
                 }
                 else -> Toast.makeText(this@List16, "Clicked " + item.title,
-                        Toast.LENGTH_SHORT).show()
+                    Toast.LENGTH_SHORT).show()
             }
             return true
         }
@@ -166,7 +170,7 @@ class List16 : ListActivity() {
          * @param checked  true if the item is now checked, false if the item is now unchecked.
          */
         override fun onItemCheckedStateChanged(mode: ActionMode, position: Int, id: Long, checked: Boolean) {
-            when (val checkedCount = listView.checkedItemCount) {
+            when (val checkedCount = list.checkedItemCount) {
                 0 -> mode.subtitle = null
                 1 -> mode.subtitle = "One item selected"
                 else -> mode.subtitle = "$checkedCount items selected"
