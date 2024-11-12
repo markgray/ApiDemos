c0307be5b95cfccc934811f9c50e86385b680c5f
markgray <mark.t.gray@gmail.com>
2024-11-12 04:55:26 -0500

    Updated AGP to 8.7.2 and updated gradle dependencies (Needs lots and lots of edgeToEdge work!)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,12 +39,12 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.4")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.mediarouter:mediarouter:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.multidex:multidex:2.0.1")
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 }
