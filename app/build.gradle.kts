a85c78350601af7b7544e5eb0110f679ad3efb54
Mark Gray <mark.t.gray@gmail.com>
2025-04-15 03:47:27 -0400

    Updated gradle dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,6 @@ dependencies {
     implementation("androidx.mediarouter:mediarouter:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.multidex:multidex:2.0.1")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 }
