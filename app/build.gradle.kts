fa44f72cb61067a2f9228d99ddb60ca90d37d3bf
markgray <mark.t.gray@gmail.com>
2025-09-09 08:50:47 -0400

    Updated gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("androidx.mediarouter:mediarouter:1.8.1")
+    implementation("androidx.print:print:1.1.0")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("androidx.core:core-ktx:1.17.0")
